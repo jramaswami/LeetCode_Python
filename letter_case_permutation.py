0cac4dcc965f89a438534964a63e2921d7b6b617
Joseph Ramaswami <jramaswami@gmail.com>
2021-02-16 10:49:33 -0500

    AC

--- letter_case_permutation.py
+++ letter_case_permutation.py
@@ -8,18 +8,21 @@ from itertools import combinations
 
 class Solution:
     def letterCasePermutation(self, S: str) -> List[str]:
-        solns = []
-        # Get the indices of the characters in S
-        letter_indices = [i for i, c in enumerate(S) if c.isalpha()]
-        # For each combo of indices in the powerset of the indices of the
-        # letters, capitalize the letters at the indicies.
-        for cap_count in range(len(letter_indices)+1):
-            for combo in combinations(letter_indices, cap_count):
-                S0 = [c for c in S]
-                for i in combo:
-                    S0[i] = S0[i].upper()
-                solns.append("".join(S0))
-        return solns
+        # Set of solutions
+        solns = set()
+        # For each bitmask in the range [0, 1 << len(S)) all the 1s are
+        # uppercase and all the 0s are lowercase.  This will get every 
+        # possibility and we use a set to reduce duplicates.
+        # O(2^N * N) but it is such a small N this is fast enough.
+        for mask in range(1 << len(S)):
+            S0 = list(S)
+            for bit in range(len(S)):
+                if (1 << bit) & mask:
+                    S0[bit] = S0[bit].upper()
+                else:
+                    S0[bit] = S0[bit].lower()
+            solns.add("".join(S0))
+        return list(solns)
 
 
 def test_1():
@@ -27,20 +30,24 @@ def test_1():
     T = sorted(["a1b2","a1B2","A1b2","A1B2"])
     assert sorted(Solution().letterCasePermutation(S)) == T
 
+
 def test_2():
     S = "3z4"
     T = sorted(["3z4","3Z4"])
     assert sorted(Solution().letterCasePermutation(S)) == T
+
 
 def test_3():
     S = "12345"
     T = sorted(["12345"])
     assert sorted(Solution().letterCasePermutation(S)) == T
 
+
 def test_4():
     S = "0"
     T = sorted(["0"])
     assert sorted(Solution().letterCasePermutation(S)) == T
+
 
 def test_5():
     S = "C"
