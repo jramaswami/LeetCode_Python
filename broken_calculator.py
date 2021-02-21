6feaf6cadb493b6a543aba2e4f2deaf92296462e
Joseph Ramaswami <jramaswami@gmail.com>
2021-02-21 15:37:14 -0500

    AC

--- broken_calculator.py
+++ broken_calculator.py
@@ -1,28 +1,21 @@
 """
 LeetCode :: Broken Calculator
 jramaswami
+
+REF: https://www.geeksforgeeks.org/convert-number-m-n-using-minimum-number-given-operations/
 """
-from collections import deque
 class Solution:
     def brokenCalc(self, X: int, Y: int) -> int:
-        visited = set()
-        queue = deque()
-        queue.append((X, 0))
-        visited.add(X)
-        while queue:
-            X0, ops = queue.popleft()
-            if X0 == Y:
-                return ops
-            if X0 < Y and X0 * 2 not in visited:
-                if X0 * 2 == Y:
-                    return ops + 1
-                queue.append((X0 * 2, ops + 1))
-                visited.add(X0 * 2)
-            if X0 > 0 and X0 - 1 not in visited:
-                if X0 - 1 == Y:
-                    return ops + 1
-                queue.append((X0 - 1, ops + 1))
-                visited.add(X0 - 1)
+        if X == Y:
+            return 0
+        elif X > Y:
+            return X - Y
+        else:
+            if Y % 2:
+                return 1 + self.brokenCalc(X, Y + 1)
+            else:
+                return 1 + self.brokenCalc(X, Y // 2)
+
 
 def test_1():
     assert Solution().brokenCalc(2, 3) == 2
