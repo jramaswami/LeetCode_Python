1f16ff138b91285abd1e3121555eeac49134e72a
Joseph Ramaswami <jramaswami@gmail.com>
2021-03-18 12:38:49 -0400

    AC

--- wiggle_subsequence.py
+++ wiggle_subsequence.py
@@ -8,24 +8,19 @@ from math import inf
 
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
-        def wiggle0(index, last_num, increasing, length):
-            if index >= len(nums):
-                return length
+        increasing_dp = [1 for _ in nums]
+        decreasing_dp = [1 for _ in nums]
+        for i, n in enumerate(nums):
+            for j, m in enumerate(nums[i+1:], start=i+1):
+                # If n < m, then m can be increasing from n.
+                if n < m:
+                    increasing_dp[j] = max(increasing_dp[j], 1 + decreasing_dp[i])
+                # If n > m then m can be decreasing from m.
+                if n > m:
+                    decreasing_dp[j] = max(decreasing_dp[j], 1 + increasing_dp[i])
 
-            soln = -inf
-            # If this in an increasing start and nums[index] is greater than
-            # the last number then we can add nums[index] to the sequence.
-            if increasing and nums[index] > last_num:
-                soln = max(soln, wiggle0(index + 1, nums[index], False, length + 1))
-            # Same for decreasing
-            if not increasing and nums[index] < last_num:
-                soln = max(soln, wiggle0(index + 1, nums[index], True, length + 1))
-            # We can also just skip nums[index]
-            soln = max(soln, wiggle0(index + 1, last_num, increasing, length))
+        return max(max(decreasing_dp), max(increasing_dp))
 
-            return soln
-
-        return max(wiggle0(0, -inf, True, 0), wiggle0(0, inf, False, 0))
 
 
 def test_1():
@@ -39,4 +34,4 @@ def test_2():
 def test_3():
     """TLE"""
     nums = [33,53,12,64,50,41,45,21,97,35,47,92,39,0,93,55,40,46,69,42,6,95,51,68,72,9,32,84,34,64,6,2,26,98,3,43,30,60,3,68,82,9,97,19,27,98,99,4,30,96,37,9,78,43,64,4,65,30,84,90,87,64,18,50,60,1,40,32,48,50,76,100,57,29,63,53,46,57,93,98,42,80,82,9,41,55,69,84,82,79,30,79,18,97,67,23,52,38,74,15]
-    assert Solution().wiggleMaxLength(nums) == 0
+    assert Solution().wiggleMaxLength(nums) == 67
