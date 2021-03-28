9345a760a327a803f5e8d236677875c34cd0a950
Joseph Ramaswami <jramaswami@gmail.com>
2021-03-28 11:25:08 -0400

    AC

--- reconstruct_original_digits_from_english.py
+++ reconstruct_original_digits_from_english.py
@@ -10,18 +10,16 @@ from math import inf
 class Solution:
     def originalDigits(self, s: str) -> str:
         ctr = Counter(s)
-        words = ["zero", "one", "two", "three", "four", "five", "six",
-                 "seven", "eight", "nine"]
-        digits = "0123456789"
+        words = ["zero", "two", "four", "six", "eight", "seven", "five", "three", "one", "nine"]
+        keyletters = ["z", "w", "u", "x", "g", "s", "v", "t", "o", "i"]
+        digits = ["0", "2", "4", "6", "8", "7", "5", "3", "1", "9"]
         soln = []
-        for word, digit in zip(words, digits):
-            min_freq = inf
-            for c in word:
-                min_freq = min(ctr[c], min_freq)
+        for word, keyletter, digit in zip(words, keyletters, digits):
+            min_freq = ctr[keyletter]
             for c in word:
                 ctr[c] -= min_freq
             soln.append(digit * min_freq)
-        return "".join(soln)
+        return "".join(sorted(soln))
 
 
 def test_1():
@@ -32,3 +30,20 @@ def test_2():
 
 def test_3():
     assert Solution().originalDigits("zeroonetwothreefourfivesixseveneightnine") == "0123456789"
+
+# z {'zero'}
+# e {'five', 'seven', 'nine', 'zero', 'one', 'eight', 'three'}
+# r {'four', 'three', 'zero'}
+# o {'one', 'four', 'two', 'zero'}
+# n {'one', 'seven', 'nine'}
+# t {'eight', 'three', 'two'}
+# w {'two'}
+# h {'eight', 'three'}
+# f {'four', 'five'}
+# u {'four'}
+# i {'eight', 'six', 'five', 'nine'}
+# v {'five', 'seven'}
+# s {'six', 'seven'}
+# x {'six'}
+# g {'eight'}
+
