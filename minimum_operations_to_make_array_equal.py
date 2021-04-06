8d72d39b55d0f0c9e8dbbb17e5b7ab5f2859dea9
Joseph Ramaswami <jramaswami@gmail.com>
2021-04-06 10:58:59 -0400

    AC. OEIS solution.

--- minimum_operations_to_make_array_equal.py
+++ minimum_operations_to_make_array_equal.py
@@ -2,16 +2,31 @@
 LeetCode :: April 2021 Challenge :: Minimum Operations to Make Array Equal
 jramaswami
 """
+import math
+
+
+def solve(n):
+    """Compute solution by simulating array."""
+    soln = 0
+    left = 1
+    right = (2 * (n - 1)) + 1
+    while left < right:
+        soln += ((right - left) // 2)
+        left += 2
+        right -= 2
+    return soln
+
+
+def solve0(n):
+    """
+    OEIS A002620
+    """
+    return int(math.floor(n / 2) * math.ceil(n / 2))
+
+
 class Solution:
     def minOperations(self, n: int) -> int:
-        soln = 0
-        left = 1
-        right = (2 * (n - 1)) + 1
-        while left < right:
-            soln += ((right - left) // 2)
-            left += 2
-            right -= 2
-        return soln
+        return solve0(n)
 
 
 def test_1():
