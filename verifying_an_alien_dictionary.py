92f0e41d3a417c42d7796d1fedb87b026544cdff
Joseph Ramaswami <jramaswami@gmail.com>
2021-04-09 08:07:53 -0400

    AC

---
+++ verifying_an_alien_dictionary.py
@@ -0,0 +1,28 @@
+"""
+LeetCode :: April 2021 Challenge :: Verifying an Alien Dictionary
+jramaswami
+"""
+from typing import *
+
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        encoding = dict((c, i) for i, c in enumerate(order))
+        encoded_words = [[encoding[c] for c in wd] for wd in words]
+        return encoded_words == sorted(encoded_words)
+
+
+def test_1():
+    words = ["hello","leetcode"]
+    order = "hlabcdefgijkmnopqrstuvwxyz"
+    assert Solution().isAlienSorted(words, order) == True
+
+def test_2():
+    words = ["word","world","row"]
+    order = "worldabcefghijkmnpqstuvxyz"
+    assert Solution().isAlienSorted(words, order) == False
+
+def test_3():
+    words = ["apple","app"]
+    order = "abcdefghijklmnopqrstuvwxyz"
+    assert Solution().isAlienSorted(words, order) == False
