52436858d9ce657e5404024b7e150aae0dac964d
Joseph Ramaswami <jramaswami@gmail.com>
2021-04-09 08:12:53 -0400

    AC. More efficient solution.

--- verifying_an_alien_dictionary.py
+++ verifying_an_alien_dictionary.py
@@ -9,7 +9,7 @@ class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
         encoding = dict((c, i) for i, c in enumerate(order))
         encoded_words = [[encoding[c] for c in wd] for wd in words]
-        return encoded_words == sorted(encoded_words)
+        return all(a <= b for a, b in zip(encoded_words[:-1], encoded_words[1:]))
 
 
 def test_1():
