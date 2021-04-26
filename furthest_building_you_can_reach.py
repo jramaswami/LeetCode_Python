64757f18d81b6da6458a24aca092be42cb0cf6f1
Joseph Ramaswami <jramaswami@gmail.com>
2021-04-26 10:13:50 -0400

    WA

---
+++ furthest_building_you_can_reach.py
@@ -0,0 +1,67 @@
+"""
+Leet Code :: April 2021 Challenge :: Furthest Building You Can Reach
+jramaswami
+"""
+from typing import *
+
+
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        print(f"{heights=}")
+        queue = [(bricks, ladders)]
+        new_queue = []
+        for i, _ in enumerate(heights[:-1]):
+            delta = heights[i+1] - heights[i]
+            print(f"@ building {i}: {heights[i]} -> {heights[i+1]} {delta=}")
+            if delta > 0:
+                for b, l in queue:
+                    advance = False
+                    if l > 0:
+                        new_queue.append((b, l - 1))
+                        advance = True
+                    if b >= delta:
+                        print(f"\t can use {delta} of {b} bricks; will have {b - delta} bricks left")
+                        new_queue.append((b - delta, l))
+                        print(f"\t{new_queue}")
+                        advance = True
+                print("switching queues")
+                queue, new_queue = new_queue, []
+                if not advance:
+                    return i
+            else:
+                print("can just jump down")
+            print(f"{i} -> {i+1}: {queue}")
+        return len(heights) - 1
+
+
+def test_1():
+    heights = [4,2,7,6,9,14,12]
+    bricks = 5
+    ladders = 1
+    expected = 4
+    assert Solution().furthestBuilding(heights, bricks, ladders) == expected
+
+
+def test_2():
+    heights = [4,12,2,7,3,18,20,3,19]
+    bricks = 10
+    ladders = 2
+    expected = 7
+    assert Solution().furthestBuilding(heights, bricks, ladders) == expected
+
+
+def test_3():
+    heights = [14,3,19,3]
+    bricks = 17
+    ladders = 0
+    expected = 3
+    assert Solution().furthestBuilding(heights, bricks, ladders) == expected
+
+
+def test_4():
+    """WA"""
+    heights = [1,5,1,2,3,4,10000]
+    bricks = 4
+    ladders = 1
+    expected = 5
+    assert Solution().furthestBuilding(heights, bricks, ladders) == expected
