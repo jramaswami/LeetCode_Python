5d2cf0454a16e76fdbc0c44baec9f3005ade5891
Joseph Ramaswami <jramaswami@gmail.com>
2021-05-07 10:42:00 -0400

    Different solution. Top-down without memoization.  TLE.

--- delete_operation_for_two_strings.py
+++ delete_operation_for_two_strings.py
@@ -3,6 +3,7 @@ Leet Code :: May 2021 Challenge :: Delete Operation for Two Strings
 jramaswami
 """
 from typing import *
+from math import inf
 
 
 class Solution:
@@ -12,25 +13,34 @@ class Solution:
         letters from each word to make it the longest common subsequence.
         """
 
-        def get(i, j, dp):
-            """Convenience method that returns 0 if you go out of bounds on dp."""
-            if i < 0:
+        def min_dist(index1, index2):
+            """
+            Return the minimum distance between word1[index:] and word2[index:]
+            """
+            if index1 == len(word1) and index2 == len(word2):
+                # We have reached the end
                 return 0
-            if j < 0:
-                return 0
-            return dp[i][j]
 
-        dp = [[0 for _ in word2] for _ in word1]
-        lcs = 0
-        for i, c1 in enumerate(word1):
-            for j, c2 in enumerate(word2):
-                if c1 == c2:
-                    dp[i][j] = get(i-1, j-1, dp) + 1
-                else:
-                    dp[i][j] = max(get(i-1, j, dp), get(i, j-1, dp))
-                lcs = max(dp[i][j], lcs)
+            if index1 == len(word1):
+                # We can only delete from word2
+                return 1 + min_dist(index1, index2 + 1)
 
-        return (len(word1) - lcs) + (len(word2) - lcs)
+            if index2 == len(word2):
+                # We can only delete from word1
+                return 1 + min_dist(index1 + 1, index2)
+
+            if word1[index1] == word2[index2]:
+                # We do not have to delete from either word.
+                return min_dist(index1 + 1, index2 + 1)
+
+            mn = inf
+            # Delete from word 1
+            mn = min(mn, 1 + min_dist(index1 + 1, index2))
+            # Delete from word2
+            mn = min(mn, 1 + min_dist(index1, index2+1))
+            return mn
+
+        return min_dist(0, 0)
 
 
 def test_1():
