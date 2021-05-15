2a8eeced6807eebfdd57097d0b45cc8f2f1a75a4
Joseph Ramaswami <jramaswami@gmail.com>
2021-05-15 12:53:53 -0400

    AC

--- single_number_ii.py
+++ single_number_ii.py
@@ -12,10 +12,19 @@ class Solution:
             mask = 1 << bit
             set_count = 0
             for n in nums:
-                if mask & n:
+                if mask & abs(n):
                     set_count += 1
             if set_count % 3:
                 soln |= mask
+
+        # Check sign.
+        neg_count = 0
+        for n in nums:
+            if n < 0:
+                neg_count += 1
+        if neg_count % 3:
+            soln = -soln
+
         return soln
         
 
