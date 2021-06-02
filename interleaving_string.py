216b5c02ef25ed9b11e7eefe34cdfbda438eadd9
Joseph Ramaswami <jramaswami@gmail.com>
2021-06-02 11:58:33 -0400

    TLE

--- interleaving_string.py
+++ interleaving_string.py
@@ -10,13 +10,13 @@ from typing import *
 def solve(i1, i2, s1, s2, s3):
     """Recursive solution."""
     # If both indexes are at the end, then we have interleaved the string.
-    if i1 >= len(s1) and i2 >= len(s2):
+    if i1 >= len(s1) and i2 >= len(s2) and i1 + i2 >= len(s3):
         return True
     
     soln = False
-    if i1 < len(s1) and s1[i1] == s3[i1 + i2]:
+    if i1 < len(s1) and i1 + i2 < len(s3) and s1[i1] == s3[i1 + i2]:
         soln = soln or solve(i1 + 1, i2, s1, s2, s3)
-    if i2 < len(s2) and s2[i2] == s3[i2 + i1]:
+    if i2 < len(s2) and i1 + i2 < len(s3) and s2[i2] == s3[i2 + i1]:
         soln = soln or solve(i1, i2 + 1, s1, s2, s3)
 
     return soln
@@ -55,3 +55,18 @@ def test_4():
     s3 = "a"
     assert Solution().isInterleave(s1, s2, s3) == False
 
+
+def test_5():
+    """WA"""
+    s1 = "a"
+    s2 = "b"
+    s3 = "a"
+    assert Solution().isInterleave(s1, s2, s3) == False
+
+
+def test_6():
+    """TLE"""
+    s1 = "bbbbbabbbbabaababaaaabbababbaaabbabbaaabaaaaababbbababbbbbabbbbababbabaabababbbaabababababbbaaababaa"
+    s2 = "babaaaabbababbbabbbbaabaabbaabbbbaabaaabaababaaaabaaabbaaabaaaabaabaabbbbbbbbbbbabaaabbababbabbabaab"
+    s3 = "babbbabbbaaabbababbbbababaabbabaabaaabbbbabbbaaabbbaaaaabbbbaabbaaabababbaaaaaabababbababaababbababbbababbbbaaaabaabbabbaaaaabbabbaaaabbbaabaaabaababaababbaaabbbbbabbbbaabbabaabbbbabaaabbababbabbabbab"
+    assert Solution().isInterleave(s1, s2, s3) == True #?
