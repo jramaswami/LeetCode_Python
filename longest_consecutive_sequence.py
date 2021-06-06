fee6b30153f3aee40f2d3a68092c3b9c54dd2159
Joseph Ramaswami <jramaswami@gmail.com>
2021-06-06 12:04:30 -0400

    AC.  A more elegant solution.

--- longest_consecutive_sequence.py
+++ longest_consecutive_sequence.py
@@ -5,71 +5,24 @@ jramaswami
 
 
 from typing import *
-from collections import namedtuple
-
-
-EndPoint = namedtuple('EndPoint', ['other', 'length'])
 
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        endpoints = dict()
+        nums0 = set(nums)
+        best = {}
+
+        def get_longest_sequence(n):
+            if n in best:
+                return best[n]
+            if n not in nums0:
+                return 0
+            best[n] = 1 + get_longest_sequence(n + 1)
+            return best[n]
+
         soln = 0
-        for n in set(nums):
-            before = n - 1 in endpoints
-            after = n + 1 in endpoints
-
-            if before and after:
-                length = 1 + endpoints[n - 1].length + endpoints[n + 1].length
-                soln = max(soln, length)
-
-                left_endpoint = EndPoint(endpoints[n + 1].other, length)
-                right_endpoint = EndPoint(endpoints[n - 1].other, length)
-                left_endpoint_key = right_endpoint.other
-                right_endpoint_key = left_endpoint.other
-
-                removals = [n + 1, n - 1, endpoints[n + 1].other, endpoints[n - 1].other]
-                for r in removals:
-                    endpoints.pop(r, None)
-
-                endpoints[left_endpoint_key] = left_endpoint
-                endpoints[right_endpoint_key] = right_endpoint
-            elif before:
-
-                length = 1 + endpoints[n - 1].length
-                soln = max(soln, length)
-
-                left_endpoint = EndPoint(endpoints[n - 1].other, length)
-                right_endpoint = EndPoint(n, length)
-                left_endpoint_key = right_endpoint.other
-                right_endpoint_key = left_endpoint.other
-
-                removals = [n - 1, endpoints[n - 1].other]
-                for r in removals:
-                    endpoints.pop(r, None)
-
-                endpoints[left_endpoint_key] = left_endpoint
-                endpoints[right_endpoint_key] = right_endpoint
-            elif after:
-                length = 1 + endpoints[n + 1].length
-                soln = max(soln, length)
-
-                left_endpoint = EndPoint(endpoints[n + 1].other, length)
-                right_endpoint = EndPoint(n, length)
-                left_endpoint_key = right_endpoint.other
-                right_endpoint_key = left_endpoint.other
-
-                removals = [n + 1, endpoints[n + 1].other]
-                for r in removals:
-                    endpoints.pop(r, None)
-
-                endpoints[left_endpoint_key] = left_endpoint
-                endpoints[right_endpoint_key] = right_endpoint
-            else:
-                length = 1
-                soln = max(soln, length)
-                endpoints[n] = EndPoint(n, length)
-
+        for n in nums0:
+            soln = max(soln, get_longest_sequence(n))
         return soln
 
 
