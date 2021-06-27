96e71237fb15514f3f1244acb011850c39db550e
Joseph Ramaswami <jramaswami@gmail.com>
2021-06-27 10:58:00 -0400

    AC

--- candy.py
+++ candy.py
@@ -6,17 +6,30 @@ jramaswami
 
 class Solution:
     def candy(self, ratings):
+        # Overall solution is O(n log n) time complexity with O(n) extra space.
+
+        # Sort by ratings, keeping the index with the rating.
+        # O(n log n)
         ratings0 = sorted((r, i) for i, r in enumerate(ratings))
         soln = [1 for _ in ratings]
+
+        # For each rating, increase the amount of candy to be one more
+        # than the largest candy of the ratings to the left and right
+        # that are less than the current rating.  Because we do them in
+        # order, we will only increase the candy if necessary.
+        # O(n)
         for _, i in ratings0:
             min_value = 1
-            if i >= 0:
+            if i > 0:
                 if ratings[i-1] < ratings[i]:
                     min_value = max(min_value, soln[i-1] + 1)
             if i + 1 < len(ratings):
                 if ratings[i+1] < ratings[i]:
                     min_value = max(min_value, soln[i+1] + 1)
             soln[i] = min_value
+
+        # Compute the total amount of candy.
+        # O(n)
         return sum(soln)
 
 
