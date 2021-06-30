0b05ef545fe458b93e544499a1e4b55f54160ad4
Joseph Ramaswami <jramaswami@gmail.com>
2021-06-30 09:33:53 -0400

    AC

---
+++ lowest_common_ancestor_of_a_binary_tree.py
@@ -0,0 +1,58 @@
+"""
+LeetCode :: June 2021 Challenge :: Lowest Common Ancestor of a Binary Tree
+jramaswami
+"""
+
+
+from collections import deque
+
+
+class Solution():
+    def lowestCommonAncestor(self, root, p, q):
+        # First, use a BFS to traverse tree and assign a level to each node
+        # and a parent for each node.  Dictionaries can be used for this
+        # because node values are unique.  We can also use this step to find
+        # the nodes for values p and q.  Each node is enqueued for an O(N)
+        # time complexity.  With level, parent, and queue we need O(N) extra
+        # space.
+        level = dict()
+        parent = dict()
+        queue = deque()
+        queue.append((root, 0))
+        parent[root] = None
+        p_node = None
+        q_node = None
+        while queue:
+            node, lvl = queue.popleft()
+            if node == p:
+                p_node = node
+            if node == q:
+                q_node = node
+            level[node] = lvl
+            if node.left is not None:
+                parent[node.left] = node
+                queue.append((node.left, lvl+1))
+            if node.right is not None:
+                parent[node.right] = node
+                queue.append((node.right, lvl+1))
+
+        # Finding the LCA will at most require traversing the entire
+        # length of the tree, for O(N) time complexity.
+
+        # Make sure that q_node is the node with the highest level.
+        if level[q_node] < level[p_node]:
+            p_node, q_node = q_node, p_node
+
+        # Move q_node up until it is at the same level as p_node.
+        while level[q_node] > level[p_node]:
+            q_node = parent[q_node]
+
+        # Now that the nodes are at the same level, move them both up
+        # until they are equal.
+        while q_node != p_node:
+            q_node = parent[q_node]
+            p_node = parent[p_node]
+
+
+        # Overall, this is a O(N) solution.
+        return q_node
