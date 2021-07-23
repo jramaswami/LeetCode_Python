d6ea6dc2b70e846e5b5ed8bebfbd6f7b5d4d3373
Joseph Ramaswami <jramaswami@gmail.com>
2021-07-22 22:50:23 -0400

    AC.  Again.

--- max_consecutive_ones.py
+++ max_consecutive_ones.py
@@ -2,24 +2,20 @@
 LeetCode :: 485. Max Consecutive Ones
 jramaswami
 """
-from typing import *
+
 
 class Solution:
-    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        run_length = 0
-        max_run_length = 0
-        for value in nums:
-            if value == 1:
-                run_length += 1
-            else:
-                max_run_length = max(max_run_length, run_length)
-                run_length = 0
-        max_run_length = max(max_run_length, run_length)
-        return max_run_length
+    def findMaxConsecutiveOnes(self, nums):
+        soln = curr_sum = 0
+        for i, n in enumerate(nums):
+            curr_sum = (curr_sum + n) * n
+            soln = max(soln, curr_sum)
+        return soln
 
 
 def test_1():
     assert Solution().findMaxConsecutiveOnes([1,1,0,1,1,1]) == 3
+
 
 def test_2():
     assert Solution().findMaxConsecutiveOnes([1,0,1,1,0,1]) == 2
