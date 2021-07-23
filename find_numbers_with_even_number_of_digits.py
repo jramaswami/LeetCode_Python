0778a17c6a5fed7bfbf2a6b5fdff25089f480db5
Joseph Ramaswami <jramaswami@gmail.com>
2021-07-22 23:06:18 -0400

    AC

--- find_numbers_with_even_number_of_digits.py
+++ find_numbers_with_even_number_of_digits.py
@@ -1,15 +1,18 @@
 """
 LeetCode :: Arrays Module :: Find Numbers with Even Number of Digits
 jramaswami
+
+Changing from log(n, 10) to log10(n) fixed the problem.  According to the
+Python docs, log10(x) is more accurate than log(x, 10)?!?
 """
 
 
-from math import log
+from math import log10
 
 
 class Solution:
     def findNumbers(self, nums):
-        return sum(int(log(n, 10) + 1) % 2 == 0 for n in nums)
+        return sum(int(log10(n) + 1) % 2 == 0 for n in nums)
 
 
 def test_1():
