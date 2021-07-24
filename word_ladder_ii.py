bc88e39e54fcd8cdc012047f59252a3008ed6b4a
Joseph Ramaswami <jramaswami@gmail.com>
2021-07-24 13:55:52 -0400

    WA

--- word_ladder_ii.py
+++ word_ladder_ii.py
@@ -4,97 +4,93 @@ jramaswami
 """
 
 
-from collections import defaultdict, deque
+from string import ascii_lowercase
+from collections import defaultdict
 from math import inf
 
 
-def adjacent(wd1, wd2):
+def neighbor_generator(word):
     """
-    Return True if wd1 and wd2 differ by only a single letter.
+    Generator for words that are one letter different than word.
     """
-    delta = 0
-    for a, b in zip(wd1, wd2):
-        if a != b:
-            delta += 1
-    return delta == 1
+    for i, c in enumerate(word):
+        for p in ascii_lowercase:
+            if p == c:
+                continue
+            yield word[:i] + p + word[i+1:]
 
 
-def build_graph(word_list):
+def build_graph(words):
     """
-    Build a graph of words that differ by 1 letter.
+    Build a graph from the words where an edge exists between a word
+    and any word in words that is different by a single letter.
     """
     adj = defaultdict(list)
-    for i, wd1 in enumerate(word_list):
-        for wd2 in word_list[i+1:]:
-            if adjacent(wd1, wd2):
-                adj[wd1].append(wd2)
-                adj[wd2].append(wd1)
+    for wd in words:
+        for wd0 in neighbor_generator(wd):
+            if wd0 in words:
+                adj[wd].append(wd0)
     return adj
 
 
 def bfs(begin_word, end_word, adj):
     """
-    BFS to determine the shortest path length from begin_word to end_word.
+    Use BFS to form a new graph where there is an adjacency list for each level
+    of the original graph, where a level is the distance from begin_word.
     """
-    distance = defaultdict(lambda: inf)
-    queue = deque()
-    distance[begin_word] = 0
-    visited = set()
-    queue.append(begin_word)
-    visited.add(begin_word)
+    parent = []
+    queue = set([begin_word])
+    level = 0
     while queue:
-        wd = queue.popleft()
-        for wd0 in adj[wd]:
-            if wd0 not in visited:
-                visited.add(wd0)
-                distance[wd0] = 1 + distance[wd]
-                queue.append(wd0)
-    return distance[end_word]
+        level_parent = defaultdict(set)
+        new_queue = set()
+        for wd in queue:
+            if wd == end_word:
+                # Stop here
+                new_queue = []
+                level_children = []
+                break
 
+            for wd0 in adj[wd]:
+                if parent and wd0 in parent[-1][wd]:
+                    continue
+                level_parent[wd0].add(wd)
+                new_queue.add(wd0)
 
+        queue = new_queue
+        level += 1
+        parent.append(level_parent)
 
-def dfs(begin_word, end_word, path_limit, adj):
-
-    soln = []
-    visited = defaultdict(int)
-
-    def dfs_visit(u, path, path_limit):
-        if len(path) > path_limit:
-            return
-
-        if u == end_word:
-            path.append(u)
-            soln.append(list(path))
-            path.pop()
-            return
-
-
-        visited[u] = 1
-        path.append(u)
-
-        for v in adj[u]:
-            if not visited[v]:
-                dfs_visit(v, path, path_limit)
-
-        path.pop()
-        visited[u] = 0
-
-    dfs_visit(begin_word, [], path_limit)
-    return soln
+    # The last entry in parent was from the level after cog was found,
+    # so we can get rid of it.
+    parent.pop()
+    return parent
 
 
 class Solution():
     def findLadders(self, begin_word, end_word, word_list):
-        # Make sure begin_word gets into the graph.
-        if begin_word not in word_list:
-            word_list.append(begin_word)
-        # Build the graph.
-        adj = build_graph(word_list)
-        # Use a BFS to find the shortest length path between begin_word
-        # and end_word.  It will be used to limit the DFS done later.
-        path_length = bfs(begin_word, end_word, adj)
-        # Use a DFS to find all the paths.
-        return dfs(begin_word, end_word, path_length, adj)
+        # Build a graph from the words.
+        word_list0 = set(word_list)
+        word_list0.add(begin_word)
+        adj = build_graph(word_list0)
+        # Build a leveled graph from the words.
+        level_adj = bfs(begin_word, end_word, adj)
+
+        # Build all the possible paths from end_word to begin_word using
+        # the leveled graph.
+        queue = [[end_word]]
+        new_queue = []
+        for level in reversed(level_adj):
+            for path in queue:
+                child = path[-1]
+                for parent in level[child]:
+                    path0 = list(path)
+                    path0.append(parent)
+                    new_queue.append(path0)
+            queue, new_queue = new_queue, []
+
+        # Reverse all the paths.
+        return [p[::-1] for p in queue]
 
 
 def test_1():
@@ -130,6 +126,16 @@ def test_4():
     begin_word = "cet"
     end_word = "ism"
     word_list = ["kid","tag","pup","ail","tun","woo","erg","luz","brr","gay","sip","kay","per","val","mes","ohs","now","boa","cet","pal","bar","die","war","hay","eco","pub","lob","rue","fry","lit","rex","jan","cot","bid","ali","pay","col","gum","ger","row","won","dan","rum","fad","tut","sag","yip","sui","ark","has","zip","fez","own","ump","dis","ads","max","jaw","out","btu","ana","gap","cry","led","abe","box","ore","pig","fie","toy","fat","cal","lie","noh","sew","ono","tam","flu","mgm","ply","awe","pry","tit","tie","yet","too","tax","jim","san","pan","map","ski","ova","wed","non","wac","nut","why","bye","lye","oct","old","fin","feb","chi","sap","owl","log","tod","dot","bow","fob","for","joe","ivy","fan","age","fax","hip","jib","mel","hus","sob","ifs","tab","ara","dab","jag","jar","arm","lot","tom","sax","tex","yum","pei","wen","wry","ire","irk","far","mew","wit","doe","gas","rte","ian","pot","ask","wag","hag","amy","nag","ron","soy","gin","don","tug","fay","vic","boo","nam","ave","buy","sop","but","orb","fen","paw","his","sub","bob","yea","oft","inn","rod","yam","pew","web","hod","hun","gyp","wei","wis","rob","gad","pie","mon","dog","bib","rub","ere","dig","era","cat","fox","bee","mod","day","apr","vie","nev","jam","pam","new","aye","ani","and","ibm","yap","can","pyx","tar","kin","fog","hum","pip","cup","dye","lyx","jog","nun","par","wan","fey","bus","oak","bad","ats","set","qom","vat","eat","pus","rev","axe","ion","six","ila","lao","mom","mas","pro","few","opt","poe","art","ash","oar","cap","lop","may","shy","rid","bat","sum","rim","fee","bmw","sky","maj","hue","thy","ava","rap","den","fla","auk","cox","ibo","hey","saw","vim","sec","ltd","you","its","tat","dew","eva","tog","ram","let","see","zit","maw","nix","ate","gig","rep","owe","ind","hog","eve","sam","zoo","any","dow","cod","bed","vet","ham","sis","hex","via","fir","nod","mao","aug","mum","hoe","bah","hal","keg","hew","zed","tow","gog","ass","dem","who","bet","gos","son","ear","spy","kit","boy","due","sen","oaf","mix","hep","fur","ada","bin","nil","mia","ewe","hit","fix","sad","rib","eye","hop","haw","wax","mid","tad","ken","wad","rye","pap","bog","gut","ito","woe","our","ado","sin","mad","ray","hon","roy","dip","hen","iva","lug","asp","hui","yak","bay","poi","yep","bun","try","lad","elm","nat","wyo","gym","dug","toe","dee","wig","sly","rip","geo","cog","pas","zen","odd","nan","lay","pod","fit","hem","joy","bum","rio","yon","dec","leg","put","sue","dim","pet","yaw","nub","bit","bur","sid","sun","oil","red","doc","moe","caw","eel","dix","cub","end","gem","off","yew","hug","pop","tub","sgt","lid","pun","ton","sol","din","yup","jab","pea","bug","gag","mil","jig","hub","low","did","tin","get","gte","sox","lei","mig","fig","lon","use","ban","flo","nov","jut","bag","mir","sty","lap","two","ins","con","ant","net","tux","ode","stu","mug","cad","nap","gun","fop","tot","sow","sal","sic","ted","wot","del","imp","cob","way","ann","tan","mci","job","wet","ism","err","him","all","pad","hah","hie","aim","ike","jed","ego","mac","baa","min","com","ill","was","cab","ago","ina","big","ilk","gal","tap","duh","ola","ran","lab","top","gob","hot","ora","tia","kip","han","met","hut","she","sac","fed","goo","tee","ell","not","act","gil","rut","ala","ape","rig","cid","god","duo","lin","aid","gel","awl","lag","elf","liz","ref","aha","fib","oho","tho","her","nor","ace","adz","fun","ned","coo","win","tao","coy","van","man","pit","guy","foe","hid","mai","sup","jay","hob","mow","jot","are","pol","arc","lax","aft","alb","len","air","pug","pox","vow","got","meg","zoe","amp","ale","bud","gee","pin","dun","pat","ten","mob"]
+    expected = [["cet","get","gee","gte","ate","ats","its","ito","ibo","ibm","ism"],["cet","cat","can","ian","inn","ins","its","ito","ibo","ibm","ism"],["cet","cot","con","ion","inn","ins","its","ito","ibo","ibm","ism"]]
+    result = Solution().findLadders(begin_word, end_word, word_list)
+    assert sorted(result) == sorted(expected)
+
+
+def test_5():
+    """WA"""
+    begin_word = "hot"
+    end_word = "dog"
+    word_list = ["hot","dog"]
     expected = []
     result = Solution().findLadders(begin_word, end_word, word_list)
     assert sorted(result) == sorted(expected)
