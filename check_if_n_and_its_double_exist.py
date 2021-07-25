19e5e5649690a86ac857e460dfacc567c1cbe5dc
Joseph Ramaswami <jramaswami@gmail.com>
2021-07-24 22:53:02 -0400

    WA

---
+++ check_if_n_and_its_double_exist.py
@@ -0,0 +1,40 @@
+"""
+LeetCode :: Array Module :: Check If N and Its Double Exist
+jramaswami
+"""
+
+class Solution:
+    def checkIfExist(self, arr):
+        # The module only speaks about linear searching.  The constraints say
+        # that len(arr) <= 500.  An O(N^2) algorithm should be fast enough.
+        for n in arr:
+            for m in arr:
+                if n * 2 == m:
+                    return True
+        return False
+
+
+def test_1():
+    arr = [10, 2, 5, 3]
+    assert Solution().checkIfExist(arr) == True
+
+
+def test_2():
+    arr = [7, 1, 14, 11]
+    assert Solution().checkIfExist(arr) == True
+
+
+def test_3():
+    arr = [3, 1, 7, 11]
+    assert Solution().checkIfExist(arr) == False
+
+
+def test_4():
+    arr = []
+    assert Solution().checkIfExist(arr) == False
+
+
+def test_5():
+    """WA: The 0 dupes.  Check the index!"""
+    arr = [-2,0,10,-19,4,6,-8]
+    assert Solution().checkIfExist(arr) == False
