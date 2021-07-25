a1c5acb0509f12e08f045984aaeb0681a5d60c19
Joseph Ramaswami <jramaswami@gmail.com>
2021-07-25 11:02:51 -0400

    AC with precomputing the valid numbers.

--- non_negative_integers_without_consecutive_ones.py
+++ non_negative_integers_without_consecutive_ones.py
@@ -5,34 +5,39 @@ jramaswami
 
 
 from collections import deque
+from bisect import bisect_right
+
+
+NUMBERS = [0, 1]
+
+
+def cache_numbers():
+    # Cache the numbers.
+    LIMIT = pow(10, 9)
+    queue = deque([2])   # start with 2 (b10)
+    while queue:
+        k = queue.popleft()
+        NUMBERS.append(k)
+        k0 = k << 1
+        k1 = 1 | (k << 1)
+        if k & 1:
+            # If k ends in a 1 then we must append a 0.
+            if k0 <= LIMIT:
+                queue.append(k0)
+        else:
+            # If k ends in a 0 then we can append a 0 or a 1.
+            if k0 <= LIMIT:
+                queue.append(k0)
+            if k1 <= LIMIT:
+                queue.append(k1)
+
+
+cache_numbers()
 
 
 class Solution:
     def findIntegers(self, n):
-        """BFS"""
-
-        # Base cases before building up numbers.
-        if n < 4:
-            return n + 1
-
-        soln = 2  # count 0, 1
-        queue = deque([2])   # start with 2 (b10)
-        while queue:
-            k = queue.popleft()
-            soln += 1
-            k0 = k << 1
-            k1 = 1 | (k << 1)
-            if k & 1:
-                # If k ends in a 1 then we must append a 0.
-                if k0 <= n:
-                    queue.append(k0)
-            else:
-                # If k ends in a 0 then we can append a 0 or a 1.
-                if k0 <= n:
-                    queue.append(k0)
-                if k1 <= n:
-                    queue.append(k1)
-        return soln
+        return bisect_right(NUMBERS, n)
 
 
 def test_1():
