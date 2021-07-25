7c18ffac52a5af05f1544bb47b4287d3fe765a08
Joseph Ramaswami <jramaswami@gmail.com>
2021-07-25 10:21:28 -0400

    WA

--- non_negative_integers_without_consecutive_ones.py
+++ non_negative_integers_without_consecutive_ones.py
@@ -4,21 +4,35 @@ jramaswami
 """
 
 
+from collections import deque
+
+
 class Solution:
     def findIntegers(self, n):
-        """Brute force."""
+        """BFS"""
 
-        def valid(k):
-            prev = 0
-            while k:
-                curr = k & 1
-                if prev & curr:
-                    return False
-                prev = curr
-                k = k >> 1
-            return True
+        # Base cases before building up numbers.
+        if n < 4:
+            return n + 1
 
-        return sum(valid(i) for i in range(n+1))
+        soln = 2  # count 0, 1
+        queue = deque([2])   # start with 2 (b10)
+        while queue:
+            k = queue.popleft()
+            soln += 1
+            k0 = k << 1
+            k1 = 1 | (k << 1)
+            if k & 1:
+                # If k ends in a 1 then we must append a 0.
+                if k0 <= n:
+                    queue.append(k0)
+            else:
+                # If k ends in a 0 then we can append a 0 or a 1.
+                if k0 <= n:
+                    queue.append(k0)
+                if k1 <= n:
+                    queue.append(k1)
+        return soln
 
 
 def test_1():
@@ -31,3 +45,17 @@ def test_2():
 
 def test_3():
     assert Solution().findIntegers(2) == 3
+
+
+def test_4():
+    assert Solution().findIntegers(2) == 3
+
+
+def test_5():
+    assert Solution().findIntegers(9865786) == 103682
+
+
+def test_6():
+    """WA"""
+    assert Solution().findIntegers(3) == 3
+
