8b07b9df62cdebee6e33eacb8bab7984fd8135f5
Joseph Ramaswami <jramaswami@gmail.com>
2021-10-03 13:23:29 -0400

    AC

--- largest_divisibile_subset.py
+++ largest_divisibile_subset.py
@@ -3,43 +3,46 @@ LeetCode :: 368. Largest Divisible Subset
 jramaswami
 """
 
+
 class Solution:
 
     def largestDivisibleSubset(self, nums):
         nums.sort()
 
-        # TODO: explore backwards.  The longest path ending at n is
-        # n added to the longest path ending at all numbers ending
-        # at m such that m divides n.
-
-        # Build graph and find starting nodes
-        in_degrees = [0 for _ in nums]
+        # Build graph where an edge (u, v) means nums[v] divides nums[u].
         adj = [[] for _ in nums]
         for i, n in enumerate(nums):
             for j, m in enumerate(nums[i+1:], start=i+1):
                 if m % n == 0:
-                    adj[i].append(j)
-                    in_degrees[j] += 1
+                    adj[j].append(i)
 
-        # To find longest path use dfs.
-        max_paths = [[] for _ in nums]
-        def dfs(node, path, root):
-            path.append(node)
-            if adj[node]:
-                for neighbor in adj[node]:
-                    dfs(neighbor, path, root)
+        # Top-down dynamic programming.
+        cache = [[] for n in nums]
+        has_cache = [False for n in nums]
+
+        def solve(u):
+            """Return the largest subset ending at nums[u]."""
+            if has_cache[u]:
+                return cache[u]
+
+            if adj[u]:
+                for v in adj[u]:
+                    v_path = solve(v)
+                    if len(v_path) + 1 > len(cache[u]):
+                        cache[u] = list(v_path)
+                        cache[u].append(u)
             else:
-                if len(path) > len(max_paths[root]):
-                    max_paths[root] = list(path)
-            path.pop()
+                cache[u] = [u]
 
-        print(in_degrees)
+            has_cache[u] = True
+            return cache[u]
+
         soln = []
-        for node, in_degree in enumerate(in_degrees):
-            if in_degree == 0:
-                dfs(node, [], node)
-                if len(max_paths[node]) > len(soln):
-                    soln = max_paths[node]
+        for off, n in enumerate(reversed(nums)):
+            u = len(nums) - off - 1
+            u_path = solve(u)
+            if len(soln) <= len(u_path):
+                soln = u_path
         return [nums[i] for i in soln]
 
 
@@ -55,7 +58,8 @@ def test_2():
     assert Solution().largestDivisibleSubset(nums) == expected
 
 
-"""TLE"""
-nums = [1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432,67108864,134217728,268435456,536870912,1073741824]
-expected = []
-assert Solution().largestDivisibleSubset(nums) == expected
+def test_3():
+    """TLE"""
+    nums = [1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432,67108864,134217728,268435456,536870912,1073741824]
+    expected = list(nums)   # nums is powers of two.
+    assert Solution().largestDivisibleSubset(nums) == expected
