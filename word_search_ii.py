690215e89a5766a2598fdf87d0cd0c0115d4f099
Joseph Ramaswami <jramaswami@gmail.com>
2021-10-09 13:25:25 -0400

    RTE

--- word_search_ii.py
+++ word_search_ii.py
@@ -4,12 +4,18 @@ jramaswami
 """
 
 
+import math
+
+
 class TrieNode:
 
-    def __init__(self):
+    def __init__(self, is_root=False):
         self.value = ""
         self.end_word = -1
         self.children = [None for _ in range(26)]
+        self.count = 1
+        if is_root:
+            self.count = math.inf
 
     def get_child(self, char):
         return self.children[ord(char) - ord('a')]
@@ -17,6 +23,17 @@ class TrieNode:
     def insert_child(self, char):
         self.children[ord(char) - ord('a')] = TrieNode()
 
+    def remove_child(self, char):
+        self.children[ord(char) - ord('a')] = None
+
+    def increment(self):
+        self.count += 1
+
+    def decrement(self):
+        self.count -= 1
+
+    def empty(self):
+        return self.count == 0
 
 
 class Solver:
@@ -32,15 +49,33 @@ class Solver:
         for index, word in enumerate(self.words):
             self._insert(index, word)
 
-
     def _insert(self, index, word):
         """Insert word."""
         curr = self.root
         for c in word:
             if not curr.get_child(c):
                 curr.insert_child(c)
+            else:
+                curr.increment()
             curr = curr.get_child(c)
+        curr.increment()
         curr.end_word = index
+
+    def _remove(self, word):
+        """Remove word from Trie."""
+        curr = self.root
+        for c in word:
+            curr.decrement()
+            curr = curr.get_child(c)
+        curr.decrement()
+        curr = self.root
+        following = None
+        for c in word:
+            following = curr.get_child(c)
+            if following.empty():
+                curr.remove_child(c)
+                return
+            curr = following
 
     def _inbounds(self, r, c):
         """Return True if (r, c) is inside the board."""
@@ -55,11 +90,11 @@ class Solver:
 
     def _search(self, r, c, curr_node):
         """DFS search of grid to find words using Trie."""
-
         if curr_node is None:
             return
 
-        if curr_node.end_word >= 0:
+        if curr_node.end_word >= 0 and not self.found[curr_node.end_word]:
+            self._remove(self.words[curr_node.end_word])
             self.found[curr_node.end_word] = True
 
         self.visited[r][c] = True
@@ -104,5 +139,13 @@ def test_3():
     """TLE"""
     board = [["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"]]
     words = ["lllllll","fffffff","ssss","s","rr","xxxx","ttt","eee","ppppppp","iiiiiiiii","xxxxxxxxxx","pppppp","xxxxxx","yy","jj","ccc","zzz","ffffffff","r","mmmmmmmmm","tttttttt","mm","ttttt","qqqqqqqqqq","z","aaaaaaaa","nnnnnnnnn","v","g","ddddddd","eeeeeeeee","aaaaaaa","ee","n","kkkkkkkkk","ff","qq","vvvvv","kkkk","e","nnn","ooo","kkkkk","o","ooooooo","jjj","lll","ssssssss","mmmm","qqqqq","gggggg","rrrrrrrrrr","iiii","bbbbbbbbb","aaaaaa","hhhh","qqq","zzzzzzzzz","xxxxxxxxx","ww","iiiiiii","pp","vvvvvvvvvv","eeeee","nnnnnnn","nnnnnn","nn","nnnnnnnn","wwwwwwww","vvvvvvvv","fffffffff","aaa","p","ddd","ppppppppp","fffff","aaaaaaaaa","oooooooo","jjjj","xxx","zz","hhhhh","uuuuu","f","ddddddddd","zzzzzz","cccccc","kkkkkk","bbbbbbbb","hhhhhhhhhh","uuuuuuu","cccccccccc","jjjjj","gg","ppp","ccccccccc","rrrrrr","c","cccccccc","yyyyy","uuuu","jjjjjjjj","bb","hhh","l","u","yyyyyy","vvv","mmm","ffffff","eeeeeee","qqqqqqq","zzzzzzzzzz","ggg","zzzzzzz","dddddddddd","jjjjjjj","bbbbb","ttttttt","dddddddd","wwwwwww","vvvvvv","iii","ttttttttt","ggggggg","xx","oooooo","cc","rrrr","qqqq","sssssss","oooo","lllllllll","ii","tttttttttt","uuuuuu","kkkkkkkk","wwwwwwwwww","pppppppppp","uuuuuuuu","yyyyyyy","cccc","ggggg","ddddd","llllllllll","tttt","pppppppp","rrrrrrr","nnnn","x","yyy","iiiiiiiiii","iiiiii","llll","nnnnnnnnnn","aaaaaaaaaa","eeeeeeeeee","m","uuu","rrrrrrrr","h","b","vvvvvvv","ll","vv","mmmmmmm","zzzzz","uu","ccccccc","xxxxxxx","ss","eeeeeeee","llllllll","eeee","y","ppppp","qqqqqq","mmmmmm","gggg","yyyyyyyyy","jjjjjj","rrrrr","a","bbbb","ssssss","sss","ooooo","ffffffffff","kkk","xxxxxxxx","wwwwwwwww","w","iiiiiiii","ffff","dddddd","bbbbbb","uuuuuuuuu","kkkkkkk","gggggggggg","qqqqqqqq","vvvvvvvvv","bbbbbbbbbb","nnnnn","tt","wwww","iiiii","hhhhhhh","zzzzzzzz","ssssssssss","j","fff","bbbbbbb","aaaa","mmmmmmmmmm","jjjjjjjjjj","sssss","yyyyyyyy","hh","q","rrrrrrrrr","mmmmmmmm","wwwww","www","rrr","lllll","uuuuuuuuuu","oo","jjjjjjjjj","dddd","pppp","hhhhhhhhh","kk","gggggggg","xxxxx","vvvv","d","qqqqqqqqq","dd","ggggggggg","t","yyyy","bbb","yyyyyyyyyy","tttttt","ccccc","aa","eeeeee","llllll","kkkkkkkkkk","sssssssss","i","hhhhhh","oooooooooo","wwwwww","ooooooooo","zzzz","k","hhhhhhhh","aaaaa","mmmmm"]
+    expected = ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+    assert sorted(Solution().findWords(board, words)) == sorted(expected)
+
+
+def test_4():
+    """RTE"""
+    board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]]
+    words = ["oath","pea","eat","rain","hklf", "hf"]
     expected = []
     assert sorted(Solution().findWords(board, words)) == sorted(expected)
