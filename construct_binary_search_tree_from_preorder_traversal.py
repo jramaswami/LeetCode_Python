e3cec347dea44ec2345ea10c710e2ceb82e34000
Joseph Ramaswami <jramaswami@gmail.com>
2021-10-13 15:06:43 -0400

    AC. A faster and more elegant solution.

--- construct_binary_search_tree_from_preorder_traversal.py
+++ construct_binary_search_tree_from_preorder_traversal.py
@@ -4,31 +4,21 @@ LeetCode :: October 2021 Challenge :: 1008. Construct Binary Search Tree from Pr
 from leetcode_bst import TreeNode
 
 
-from collections import namedtuple
-from math import inf
-
-
-SItem = namedtuple('SItem', ['node', 'subtree_max'])
-
-
 class Solution:
     def bstFromPreorder(self, preorder):
-        stack = []
-        root = TreeNode(preorder[0])
-        stack.append(SItem(root, inf))
 
-        for n in preorder[1:]:
-            # N can only go into a subtree where it does not exceed the
-            # subtree's maximum value.
-            while n > stack[-1].subtree_max:
-                stack.pop()
+        def solve(arr):
+            if len(arr) == 0:
+                return None
 
-            new_node = TreeNode(n)
-            if n < stack[-1].node.val:
-                stack[-1].node.left = new_node
-                stack.append(SItem(new_node, stack[-1].node.val))
-            else:
-                stack[-1].node.right = new_node
-                stack.append(SItem(new_node, stack[-1].subtree_max))
+            # The root of the subtree is the first item in arr.
+            root = TreeNode(arr[0])
 
-        return root
+            # All item less than root value go left.
+            root.left = solve([k for k in arr if k < root.val])
+            # All item more than root value go right.
+            root.right = solve([k for k in arr if k > root.val])
+
+            return root
+
+        return solve(preorder)
