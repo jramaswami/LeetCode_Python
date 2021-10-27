f69a5addc0450b770e68d1e97064c18266815c5c
Joseph Ramaswami <jramaswami@gmail.com>
2021-10-26 22:46:10 -0400

    AC. One pass, constant solution.

--- sort_colors.py
+++ sort_colors.py
@@ -9,15 +9,27 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # Two pass, constant space.
-        freqs = [0, 0, 0]
+        # One pass, constant extra space.
+        not_zero = 0
+        not_two = len(nums) - 1
 
-        for n in nums:
-            freqs[n] += 1
+        i = 0
+        while i < len(nums):
 
-        for i in range(0, freqs[0]):
-            nums[i] = 0
-        for i in range(freqs[0], freqs[0] + freqs[1]):
-            nums[i] = 1
-        for i in range(freqs[0] + freqs[1], len(nums)):
-            nums[i] = 2
+            # Update not_zero pointer.
+            while not_zero < len(nums) and nums[not_zero] == 0:
+                not_zero += 1
+
+            # Update not_two pointer.
+            while not_two >= 0 and nums[not_two] == 2:
+                not_two -= 1
+
+            if nums[i] == 0 and i > not_zero:
+                # An out of position zero, swap it with the leftmost not zero.
+                nums[not_zero], nums[i] = nums[i], nums[not_zero]
+            elif nums[i] == 2 and i < not_two:
+                # An out of position tow, swap it with the rightmost not two.
+                nums[not_two], nums[i] = nums[i], nums[not_two]
+            else:
+                # This number is in place, move to the next one.
+                i += 1
