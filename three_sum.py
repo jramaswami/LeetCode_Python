102b81f017ea6a1d32b64aa75bf6c6e9118edad6
Joseph Ramaswami <jramaswami@gmail.com>
2021-10-28 13:37:24 -0400

    AC

--- three_sum.py
+++ three_sum.py
@@ -9,32 +9,55 @@ import collections
 
 class Solution:
     def threeSum(self, nums):
+        solns = []
+        ctr = collections.Counter(nums)
+        nums0 = list(sorted(ctr))
+        for i, a in enumerate(nums0):
+            # You can use the same number twice if there is at least two of
+            # them.
+            offset = 0 if ctr[a] > 1 else 1
+            for k, b in enumerate(nums0[i+offset:], start=i+offset):
+                c = -(a + b)
+                if c < b:
+                    continue
+                if a == b == c:
+                    if ctr[a] >= 3:
+                        solns.append([a, b, c])
+                elif b == c:
+                    if ctr[b] >= 2:
+                        solns.append([a, b, c])
+                else:
+                    if ctr[c] > 0:
+                        solns.append([a, b, c])
+        return solns
 
-        cache = collections.defaultdict(list)
-
-        nums.sort()
-        zero_count = 0
-        for i, a in enumerate(nums):
-            if a == 0:
-                zero_count += 1
-            for j, b in enumerate(nums[i+1:], start=i+1):
-                cache[a + b].append((i, j))
 
 
-        solns = set()
-        if zero_count > 2:
-            solns.add((0,0,0))
 
-        for i, a in enumerate(nums):
-            if a == 0:
-                continue
-            for j, k in cache[-a]:
-                if i > j and i > k:
-                    solns.add((a, nums[j], nums[k]))
 
-        return [list(T) for T in solns]
 
 
 def test_1():
+    nums = [-1,0,1,2,-1,-4]
+    expected = [[-1,-1,2],[-1,0,1]]
+    result = Solution().threeSum(nums)
+    assert result == expected
+
+
+def test_2():
+    nums = []
+    expected = []
+    result = Solution().threeSum(nums)
+    assert result == expected
+
+
+def test_3():
+    nums = [0]
+    expected = []
+    result = Solution().threeSum(nums)
+    assert result == expected
+
+
+def test_4():
     nums = [0] * 3000
     assert Solution().threeSum(nums) == [[0, 0, 0]]
