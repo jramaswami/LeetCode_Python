a3c2488ec3d4297cf561c83618a2564d9b461876
Joseph Ramaswami <jramaswami@gmail.com>
2021-10-28 08:59:22 -0400

    Still TLE

--- three_sum.py
+++ three_sum.py
@@ -8,19 +8,33 @@ import collections
 
 
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums):
 
         cache = collections.defaultdict(list)
 
         nums.sort()
+        zero_count = 0
         for i, a in enumerate(nums):
+            if a == 0:
+                zero_count += 1
             for j, b in enumerate(nums[i+1:], start=i+1):
                 cache[a + b].append((i, j))
 
+
         solns = set()
+        if zero_count > 2:
+            solns.add((0,0,0))
+
         for i, a in enumerate(nums):
+            if a == 0:
+                continue
             for j, k in cache[-a]:
                 if i > j and i > k:
                     solns.add((a, nums[j], nums[k]))
 
-        return solns
+        return [list(T) for T in solns]
+
+
+def test_1():
+    nums = [0] * 3000
+    assert Solution().threeSum(nums) == [[0, 0, 0]]
