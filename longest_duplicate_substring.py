bf968732b08d74604c00b4553154e244e62d00ac
Joseph Ramaswami <jramaswami@gmail.com>
2021-10-30 10:15:12 -0400

    TLE, still.

--- longest_duplicate_substring.py
+++ longest_duplicate_substring.py
@@ -4,6 +4,9 @@ jramaswami
 """
 
 
+import collections
+
+
 class Solution:
     def longestDupSubstring(self, S):
 
@@ -17,18 +20,25 @@ class Solution:
                 j += 1
             return result
 
+        # Optimize so that when you start looking for repeated substrings,
+        # you only start on matching letters.
+        locations = collections.defaultdict(list)
+        for i, c in enumerate(S):
+            locations[c].append(i)
+
         soln_length = 0
         soln = ""
-        for i, _ in enumerate(S):
-            for j, _ in enumerate(S[i+1:], start=i+1):
-                # If we reach a point where the right substring is not
-                # large enough to beat our current winner, then stop.
-                if len(S) - j < soln_length:
-                    break
-                k = lds(i, j)
-                if k > soln_length:
-                    soln_length = k
-                    soln = S[i:i+k]
+        for c in locations:
+            for p, i in enumerate(locations[c]):
+                for _, j in enumerate(locations[c][p+1:], start=p+1):
+                    # If we reach a point where the right substring is not
+                    # large enough to beat our current winner, then stop.
+                    if len(S) - j < soln_length:
+                        break
+                    k = lds(i, j)
+                    if k > soln_length:
+                        soln_length = k
+                        soln = S[i:i+k]
         return soln
 
 
