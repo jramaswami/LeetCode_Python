280941b059d6a81e548d82c5d6dabaa562b13492
Joseph Ramaswami <jramaswami@gmail.com>
2021-10-30 12:10:52 -0400

    TLE, still.

--- longest_duplicate_substring.py
+++ longest_duplicate_substring.py
@@ -4,42 +4,25 @@ jramaswami
 """
 
 
-import collections
-
-
 class Solution:
     def longestDupSubstring(self, S):
+        # Construct suffix array.
+        suffix_array = [T[1] for T in (sorted((S[i:], i) for i, _ in enumerate(S)))]
 
-        def lds(i, j):
-            result = 0
-            while i < len(S) and j < len(S):
-                if S[i] != S[j]:
-                    return result
-                result += 1
-                i += 1
-                j += 1
-            return result
+        # Use suffix array to find longest duplicate substring.
+        start_index, end_index, soln_length = 0, -1, 0
+        for i, j in zip(suffix_array[:-1], suffix_array[1:]):
+            k, i0, j0 = 0, i, j
+            while i0 < len(S) and j0 < len(S) and S[i0] == S[j0]:
+                k += 1
+                i0 += 1
+                j0 += 1
+            if k > soln_length:
+                start_index, end_index, soln_length = i, i0, k
 
-        # Optimize so that when you start looking for repeated substrings,
-        # you only start on matching letters.
-        locations = collections.defaultdict(list)
-        for i, c in enumerate(S):
-            locations[c].append(i)
-
-        soln_length = 0
-        soln = ""
-        for c in locations:
-            for p, i in enumerate(locations[c]):
-                for _, j in enumerate(locations[c][p+1:], start=p+1):
-                    # If we reach a point where the right substring is not
-                    # large enough to beat our current winner, then stop.
-                    if len(S) - j < soln_length:
-                        break
-                    k = lds(i, j)
-                    if k > soln_length:
-                        soln_length = k
-                        soln = S[i:i+k]
-        return soln
+        if soln_length > 0:
+            return S[start_index:end_index]
+        return ""
 
 
 def test_1():
@@ -55,6 +38,7 @@ def test_2():
 
 
 def test_3():
+    """TLE"""
     S = "a" * pow(10, 4) * 3
     expected = "a" * ((pow(10, 4) * 3) - 1)
     result = Solution().longestDupSubstring(S)
