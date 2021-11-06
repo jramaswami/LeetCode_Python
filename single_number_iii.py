7c6fb0e430dc1927feac8e9e1f254b3677877e7b
Joseph Ramaswami <jramaswami@gmail.com>
2021-11-05 21:51:02 -0400

    AC. The more acceptable O(N) solution.

--- single_number_iii.py
+++ single_number_iii.py
@@ -3,15 +3,45 @@ LeetCode :: November 2021 Challenge :: 260. Single Number III
 """
 
 
+import functools
+import operator
+
+
 class Solution:
     def singleNumber(self, nums):
-        # Per Python Wiki set operations are O(1) amortized.
-        # So at most we will add each item once and remove
-        # all but two items once: 2 * O(N) = O(N).
-        visited = set()
-        for n in nums:
-            if n in visited:
-                visited.remove(n)
-            else:
-                visited.add(n)
-        return list(visited)
+
+        def lsb_mask(n):
+            mask = 1
+            while not n & mask:
+                print(mask)
+                mask = mask << 1
+                if mask > pow(2, 32):
+                    break
+            return mask
+
+        # XOR all the numbers giving X such that X = a ^ b.
+        X = functools.reduce(operator.xor, nums)
+        # Since a != b any set bit in X is unique to one of the numbers.
+        # Find that bit.
+        L = lsb_mask(X)
+        # If we XOR all the numbers with the unique bit set, the result
+        # will be a, since that bit is not set in b and all other numbers
+        # appear twice and will cancel out.
+        a = functools.reduce(operator.xor, [n for n in nums if n & L])
+        # Once we know a, we can get be by XORing X and a.
+        b = X ^ a
+        return [a, b]
+
+
+def test_1():
+    nums = [1,2,1,3,2,5]
+    expected = [3, 5]
+    assert set(Solution().singleNumber(nums)) == set(expected)
+
+
+def test_2():
+    nums = list(range(1, 100)) * 2
+    nums.extend([-1, -100])
+    expected = [-1, -100]
+    print(nums)
+    assert set(Solution().singleNumber(nums)) == set(expected)
