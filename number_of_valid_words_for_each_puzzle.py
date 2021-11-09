d32623d4dab031f448c62d880947af65593d8799
Joseph Ramaswami <jramaswami@gmail.com>
2021-11-09 14:01:41 -0500

    AC with Larry's help.

--- number_of_valid_words_for_each_puzzle.py
+++ number_of_valid_words_for_each_puzzle.py
@@ -1,28 +1,72 @@
 """
 LeetCode :: November 2021 Challenge :: 1178. Number of Valid Words for Each Puzzle
 jramaswami
+
+Thank You Larry!
 """
+
+
+import collections
 
 
 class Solution:
     def findNumOfValidWords(self, words, puzzles):
+
+        ORD_a = ord('a')
+
+        def letter_to_bit(c):
+            """Convert single letter into a bitmask."""
+            return 1 << (ord(c) - ORD_a)
+
         def word_to_bitmask(word):
+            """Convert the characters in word into a bitmask."""
             word_mask = 0
             for c in word:
-                word_mask |= 1 << (ord(c) - ord('a'))
+                word_mask |= letter_to_bit(c)
             return word_mask
 
-        def first_letter_valid(puzzle, word_mask):
-            puzzle_bit = 1 << (ord(puzzle[0]) - ord('a'))
-            return puzzle_bit & word_mask
+        def popcount(x):
+            """Return the number of bits set in x."""
+            bits = 0
+            while x:
+                if x & 1:
+                    bits += 1
+                x = x >> 1
+            return bits
 
-        word_letters = [word_to_bitmask(word) for word in words]
-        puzzle_letters = [word_to_bitmask(puzzle) for puzzle in puzzles]
+        # Turn words into bitmasks.  Since puzzles have seven letters,
+        # only keep track of bitmasks with less than 7 ones.
+        # In addition, any words that have the same mask can be combined.
+        word_masks = collections.defaultdict(int)
+        for word in words:
+            mask = word_to_bitmask(word)
+            if popcount(mask) <= 7:
+                word_masks[mask] += 1
+
+        # Each puzzle is only 7 characters long.  The first character is fixed
+        # giving us only 6 characters that can change.  The superset of 6
+        # items is only 2^6 or 64 possible combinations of letters.  Given the
+        # constraints where there are up to 10^5 words and 10^4 puzzles,
+        # comparing each word with each puzzle is O(10^9) where using all
+        # possible subsets of each puzzle is only O(10^4 * 64) or approximately
+        # O(7 * 10^5).
+        def puzzle_mask_generator(puzzle, index, prev_mask):
+            if index >= len(puzzle):
+                yield prev_mask
+            else:
+                # You can include the character at the given index.
+                yield from puzzle_mask_generator(
+                    puzzle, index + 1, prev_mask | letter_to_bit(puzzle[index])
+                )
+
+                # You can skip the character at the given index.
+                yield from puzzle_mask_generator(puzzle, index + 1, prev_mask)
+
+        # Compute solution.
         soln = [0 for _ in puzzles]
-        for i, word in enumerate(word_letters):
-            for j, puzzle in enumerate(puzzle_letters):
-                if first_letter_valid(puzzles[j], word) and word & puzzle == word:
-                    soln[j] += 1
+        for i, puzzle in enumerate(puzzles):
+            for puzzle_mask in puzzle_mask_generator(puzzle, 1, letter_to_bit(puzzle[0])):
+                soln[i] += word_masks[puzzle_mask]
         return soln
 
 
