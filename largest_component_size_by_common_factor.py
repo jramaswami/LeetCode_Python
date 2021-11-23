8488634a953770e0a9f42bf6daecc01d2bb7bc33
Joseph Ramaswami <jramaswami@gmail.com>
2021-11-23 15:16:12 -0500

    TLE

--- largest_component_size_by_common_factor.py
+++ largest_component_size_by_common_factor.py
@@ -40,18 +40,30 @@ class UnionFind:
 
 
 class Solution:
+    def __init__(self):
+        limit = pow(10,5) // 2
+        is_prime = [True for _ in range(limit + 1)]
+        is_prime[0] = is_prime[1] = False
+        for i in range(4, len(is_prime), 2):
+            is_prime[i] = False
+        p = 3
+        while p * p <= len(is_prime):
+            if is_prime[p]:
+                for i in range(p + p, len(is_prime), p):
+                    is_prime[i] = False
+            p += 2
+        self.PRIMES = [p for p, _ in enumerate(is_prime) if is_prime[p]]
+
     def largestComponentSize(self, nums):
         uf = UnionFind()
         for n in nums:
             uf.make_set(n)
 
-        for a, b in itertools.combinations(nums, 2):
-            if math.gcd(a, b) > 1:
-                print(a, b)
-                uf.union_set(a, b)
-
+        for P in self.PRIMES:
+            A = [n for n in nums if n % P == 0]
+            for k in A[1:]:
+                uf.union_set(A[0], k)
         return uf.max_size()
-
 
 
 def test_1():
@@ -63,4 +75,10 @@ def test_1():
 def test_2():
     nums = [2,3,6,7,4,12,21,39]
     expected = 8
+    assert Solution().largestComponentSize(nums) == expected
+
+
+def test_3():
+    nums = list(range(1, pow(10, 4)))
+    expected = 9438
     assert Solution().largestComponentSize(nums) == expected
