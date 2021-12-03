51e33e01739d85a1afa216dfb00175e0e3fea304
Joseph Ramaswami <jramaswami@gmail.com>
2021-12-02 22:09:31 -0500

    WA

---
+++ maximum_product_subarray.py
@@ -0,0 +1,50 @@
+"""
+LeetCode :: December 2021 Challenge :: 152. Maximum Product Subarray
+jramaswami
+"""
+
+
+class Solution:
+    def maxProduct(self, nums):
+        curr = 1
+        prefix = []
+        for n in nums:
+            curr *= n
+            prefix.append(curr)
+
+        soln = 0
+        for left, _ in enumerate(prefix):
+            for right, P in enumerate(prefix[left+1:], start=left+1):
+                if left == 0:
+                    soln = max(soln, P)
+                else:
+                    if prefix[left - 1] != 0:
+                        soln = max(soln, P // prefix[left - 1])
+        return soln
+
+
+
+def test_1():
+    nums = [2,3,-2,4]
+    expected = 6
+    assert Solution().maxProduct(nums) == expected
+
+
+def test_2():
+    nums = [2,0,-1]
+    expected = 0
+    assert Solution().maxProduct(nums) == expected
+
+
+def test_3():
+    nums = [-8, -6, -5, -10, 9, -9, 2, -2, 9, 0, 6, -4, 3, -2, -10, 10]
+    expected = 6998400
+    assert Solution().maxProduct(nums) == expected
+
+
+def test_4():
+    "WA"
+    nums = [-2]
+    expected = -2
+    assert Solution().maxProduct(nums) == expected
+
