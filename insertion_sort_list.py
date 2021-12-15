70efe7896566aee07665e0c99bca1e54168dea33
Joseph Ramaswami <jramaswami@gmail.com>
2021-12-14 22:08:31 -0500

    Still TLE.

--- insertion_sort_list.py
+++ insertion_sort_list.py
@@ -8,11 +8,11 @@ class Solution:
 
     def insertionSortList(self, head):
         sorted_list = None
-        unsorted_list = head
-        while unsorted_list:
+        curr_unsorted = head
+        while curr_unsorted:
             # Pop the first node off the unsorted list.
-            new_node = unsorted_list
-            unsorted_list = new_node.next
+            new_node = ListNode(curr_unsorted.val)
+            curr_unsorted = curr_unsorted.next
             # Put that item at the head of the sorted list.
             new_node.next = sorted_list
             sorted_list = new_node
