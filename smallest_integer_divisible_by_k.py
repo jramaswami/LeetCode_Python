07b36d0a006faf451304556bd5d066deb3f37a1b
Joseph Ramaswami <jramaswami@gmail.com>
2021-12-30 16:31:52 -0500

    AC

--- smallest_integer_divisible_by_k.py
+++ smallest_integer_divisible_by_k.py
@@ -1,37 +1,23 @@
 """
 LeetCode :: December 2021 Challenge :: 1015. Smallest Integer Divisible by K
 jramaswami
+
+Thank You Larry!
 """
 
 
 class Solution:
     def smallestRepunitDivByK(self, k: int) -> int:
+        # Numbers divisible by 2 or 5 will never have product with all ones.
+        if k % 2 == 0 or k % 5 == 0:
+            return -1
 
-        def find_digit(carry, k):
-            "Return digit that results in 1 in the ones place."
-            for d in range(10):
-                summ = carry + (k * d)
-                if summ % 10 == 1:
-                    return summ // 10, d
-            return carry, -1
-
-        def all_ones(number):
-            return all(c == '1' for c in str(number))
-
-
-        carry = 0
-        t = 0
-        m = 1
-        while not all_ones(k * t):
-            carry, d = find_digit(carry, k)
-            if d == -1:
-                return -1
-            t = t + (m * d)
-            m *= 10
-
-            # print(f"{k} * {t} = {k * t}")
-
-        return len(str(k * t))
+        r = 0
+        for soln in range(1, k+1):
+            r = (r * 10 + 1) % k
+            if r == 0:
+                return soln
+        return -1
 
 
 def test_1():
@@ -50,6 +36,6 @@ def test_3():
 
 
 def test_4():
-    "TLE"
+    "TLE: endless loop?"
     k = 19927
-    assert Solution().smallestRepunitDivByK(k) == 3
+    assert Solution().smallestRepunitDivByK(k) == 19926
