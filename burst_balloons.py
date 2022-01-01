42b2b69df47b6cc23cad85161fa5ae355f343810
Joseph Ramaswami <jramaswami@gmail.com>
2022-01-01 12:41:05 -0500

    Removed function class but still would be TLE.

--- burst_balloons.py
+++ burst_balloons.py
@@ -17,22 +17,7 @@ class Solution:
     def maxCoins(self, nums):
 
         # DP array holds State for given subarray.
-        dp = [[None for _ in nums] for _ in nums]
-
-        # Helper functions.
-        def get_coins(index):
-            "Return the number of coins you get for a given index."
-            if index < 0 or index >= len(nums):
-                return 1
-            return nums[index]
-
-        def get_burst_coins(left, right):
-            "Return the number of coinds you get for bursting subarray."
-            if left > right:
-                return 0
-            if dp[left][right] is None:
-                return 0
-            return dp[left][right].coins
+        dp = [[State(0, -1) for _ in nums] for _ in nums]
 
         # Dynamic programming to find solution.
         for length in range(1, len(nums)+1):
@@ -42,17 +27,31 @@ class Solution:
                 # burst last.
                 best_coins = 0
                 best_balloon = 0
+
+                # Get the values of the two coins to the far left and far
+                # right (the coins just outside the subarray).
+                far_left = far_right = 1
+                if 0 <= left - 1 < len(nums):
+                    far_left = nums[left - 1]
+                if 0 <= right + 1 < len(nums):
+                    far_right = nums[right + 1]
+
                 for last in range(left, right+1):
                     # Consider last balloon to burst at given index.
                     # Get the best from the left part of the subarray.
-                    left_coins = get_burst_coins(left, last-1)
+                    left_coins = 0
+                    if left <= last - 1:
+                        left_coins = dp[left][last-1].coins
                     # Get the best from the right part of the subarray.
-                    right_coins = get_burst_coins(last+1, right)
+                    right_coins = 0
+                    if last + 1 <= right:
+                        right_coins = dp[last+1][right].coins
                     # Now get the coins from bursting last balloon, which will
                     # be grouped with the balloons adjacent but outside the
                     # subarray because we have already burst all the other
                     # balloons in the subarray.
-                    last_coins = get_coins(left - 1) * get_coins(last) * get_coins(right + 1)
+
+                    last_coins = far_left * nums[last] * far_right
                     total_coins = left_coins + last_coins + right_coins
                     if total_coins > best_coins:
                         best_coins, best_balloon = total_coins, last
