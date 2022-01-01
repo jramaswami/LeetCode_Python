35888bad8f1906aa3317cb75032d414189c52b94
Joseph Ramaswami <jramaswami@gmail.com>
2022-01-01 13:01:31 -0500

    AC

--- burst_balloons.py
+++ burst_balloons.py
@@ -3,19 +3,14 @@ LeetCode :: January 2022 Challenge :: 312. Burst Balloons
 jramaswami
 
 
-REF: https://www.youtube.com/watch?v=IFNibRVgFBo
+Thank You Larry!
 """
-
-
-import collections
-
-
-State = collections.namedtuple('State', ['coins', 'last'])
 
 
 class Solution:
     def maxCoins(self, nums):
 
+        nums = [1] + nums + [1]
         # DP array holds State for given subarray.
         dp = [[0 for _ in nums] for _ in nums]
 
@@ -23,29 +18,11 @@ class Solution:
         for length in range(1, len(nums)+1):
             for left in range(len(nums) - length + 1):
                 right = left + length - 1
-                # Consider subarray [left:right+1], find the best balloon to
-                # burst last.
-
-                # Get the values of the two coins to the far left and far
-                # right (the coins just outside the subarray).
-                far_left = nums[left - 1] if 0 <= left - 1 else 1
-                far_right = nums[right + 1] if right + 1 < len(nums) else 1
-
-                for last in range(left, right+1):
-                    # Consider last balloon to burst at given index.
-                    # Get the best from the left part of the subarray.
-                    left_coins = left_coins = dp[left][last-1] if left <= last - 1 else 0
-                    # Get the best from the right part of the subarray.
-                    right_coins = right_coins = dp[last+1][right] if last + 1 <= right else 0
-                    # Now get the coins from bursting last balloon, which will
-                    # be grouped with the balloons adjacent but outside the
-                    # subarray because we have already burst all the other
-                    # balloons in the subarray.
-                    last_coins = far_left * nums[last] * far_right
-                    total_coins = left_coins + last_coins + right_coins
-                    # Record the best number of coins in the dp array.
-                    dp[left][right] = max(dp[left][right], total_coins)
-
+                for last in range(left+1, right):
+                    dp[left][right] = max(
+                            dp[left][right],
+                            dp[left][last] + dp[last][right] + (nums[left] * nums[last] * nums[right])
+                    )
         return dp[0][-1]
 
 
