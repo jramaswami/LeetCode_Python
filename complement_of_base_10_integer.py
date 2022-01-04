7c365690af9ef1e87721ca67e911819d20a28e3f
Joseph Ramaswami <jramaswami@gmail.com>
2022-01-04 09:37:09 -0500

    WA

---
+++ complement_of_base_10_integer.py
@@ -0,0 +1,32 @@
+"""
+LeetCode :: January 2022 Challenge :: 1009. Complement of Base 10 Integer
+jramaswami
+"""
+
+
+class Solution:
+    def bitwiseComplement(self, num: int) -> int:
+        soln = 0
+        bit = 0
+        while num:
+            if num & 1:
+                pass
+            else:
+                soln |= (1 << bit)
+            num = num >> 1
+            bit += 1
+        return soln
+
+
+def test_1():
+    assert Solution().bitwiseComplement(5) == 2
+
+
+def test_2():
+    assert Solution().bitwiseComplement(1) == 0
+
+
+def test_3():
+    "WA"
+    assert Solution().bitwiseComplement(0) == 1
+
