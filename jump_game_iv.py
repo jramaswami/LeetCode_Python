621a0ccd44e10afc49923b332f7dbd1a8c6a7c59
Joseph Ramaswami <jramaswami@gmail.com>
2022-01-14 22:18:46 -0500

    TLE.

--- jump_game_iv.py
+++ jump_game_iv.py
@@ -6,7 +6,6 @@ jramaswami
 
 import collections
 import heapq
-import itertools
 import math
 
 
@@ -37,12 +36,9 @@ class Solution:
             if dist[u] != d:
                 continue
 
-            if u == len(arr) - 1:
-                return dist[-1]
-
             # Can we jump directly there?
             if arr[u] == arr[-1]:
-                return dist[u] + 1
+                dist[-1] = min(dist[-1], d + 1)
 
             # Forward
             if u + 1 < len(arr) and d + 1 < dist[u + 1]:
@@ -61,6 +57,8 @@ class Solution:
                 if d + 2 < dist[v]:
                     heapq.heappush(queue, (d + 2, v))
                     dist[v] = d + 2
+
+        return dist[-1]
 
 
 
@@ -102,4 +100,3 @@ def test_6():
     arr = [51,64,-15,58,98,31,48,72,78,-63,92,-5,64,-64,51,-48,64,48,-76,-86,-5,-64,-86,-47,92,-41,58,72,31,78,-15,-76,72,-5,-97,98,78,-97,-41,-47,-86,-97,78,-97,58,-41,72,-41,72,-25,-76,51,-86,-65,78,-63,72,-15,48,-15,-63,-65,31,-41,95,51,-47,51,-41,-76,58,-81,-41,88,58,-81,88,88,-47,-48,72,-25,-86,-41,-86,-64,-15,-63]
     expected = 4
     assert Solution().minJumps(arr) == expected
-
