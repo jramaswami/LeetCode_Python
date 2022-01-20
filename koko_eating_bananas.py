8453c7cffa5b8378bbc438be83d4afd4d5852dac
Joseph Ramaswami <jramaswami@gmail.com>
2022-01-20 08:50:37 -0500

    TLE

---
+++ koko_eating_bananas.py
@@ -0,0 +1,59 @@
+"""
+LeetCode :: January 2022 Challenge :: 875. Koko Eating Bananas
+jramaswami
+"""
+
+
+class Solution:
+    def minEatingSpeed(self, piles, h):
+
+        def can_eat(k):
+            t = 0
+            for b in piles:
+                q, r = divmod(b, k)
+                if r:
+                    q += 1
+                t += q
+            return t <= h
+
+        # Binary search for the answer.
+        soln = max(piles)
+        lo = 1
+        hi = soln
+        while lo <= hi:
+            mid = lo + ((hi - lo) // 2)
+            if can_eat(mid):
+                hi -= 1
+                soln = min(soln, mid)
+            else:
+                lo += 1
+        return soln
+
+
+def test_1():
+    piles = [3,6,7,11]
+    h = 8
+    expected = 4
+    assert Solution().minEatingSpeed(piles, h) == expected
+
+
+def test_2():
+    piles = [30,11,23,4,20]
+    h = 5
+    expected = 30
+    assert Solution().minEatingSpeed(piles, h) == expected
+
+
+def test_3():
+    piles = [30,11,23,4,20]
+    h = 6
+    expected = 23
+    assert Solution().minEatingSpeed(piles, h) == expected
+
+
+def test_4():
+    "TLE"
+    piles = [312884470]
+    h = 312884469
+    expected = 23
+    assert Solution().minEatingSpeed(piles, h) == expected
