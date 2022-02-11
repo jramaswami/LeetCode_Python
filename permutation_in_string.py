4f4f0bb755cd90e7d9fc248394ac251cee4fd244
Joseph Ramaswami <jramaswami@gmail.com>
2022-02-10 21:12:53 -0500

    WA

---
+++ permutation_in_string.py
@@ -0,0 +1,48 @@
+"""
+LeetCode :: February 2022 Challenge :: 567. Permutation in String
+jramaswami
+"""
+
+
+import collections
+import string
+
+
+class Solution:
+
+    def checkInclusion(self, s1, s2):
+        # Initialize each to the same baseline of 1 for every letter.
+        target_ctr = collections.Counter(string.ascii_lowercase)
+        window_ctr = collections.Counter(string.ascii_lowercase)
+        # Update with target string and initial sliding window, respectively.
+        target_ctr.update(s1)
+        window_ctr.update(s2[:len(s1)])
+        # Use a sliding window to find if there is a permuation present.
+        for i, _ in enumerate(s2[len(s1):], start=len(s1)):
+            if target_ctr == window_ctr:
+                return True
+            window_ctr[s2[i-len(s1)]] -= 1
+            window_ctr[s2[i]] += 1
+        return False
+
+
+def test_1():
+    s1 = "ab"
+    s2 = "eidbaooo"
+    expected = True
+    assert Solution().checkInclusion(s1, s2) == expected
+
+
+def test_2():
+    s1 = "ab"
+    s2 = "eidboaoo"
+    expected = False
+    assert Solution().checkInclusion(s1, s2) == expected
+
+
+def test_3():
+    "WA"
+    s1 = "adc"
+    s2 = "dcda"
+    expected = True
+    assert Solution().checkInclusion(s1, s2) == expected
