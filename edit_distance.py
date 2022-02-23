681643b42b643498e510f915b9e4db240bb2293d
Joseph Ramaswami <jramaswami@gmail.com>
2022-02-23 11:29:38 -0500

    WA

--- edit_distance.py
+++ edit_distance.py
@@ -8,6 +8,12 @@ class Solution:
 
     def minDistance(self, word1, word2):
 
+        # Boundary cases
+        if word1 == "":
+            return len(word2)
+        if word2 == "":
+            return len(word1)
+
         # dp[word2][word1]
         dp = [[0 for _ in range(len(word1) + 1)] for _ in range(len(word2) + 1)]
 
@@ -53,4 +59,12 @@ def test_3():
     word1 = ""
     word2 = ""
     expected = 0
+    assert Solution().minDistance(word1, word2) == expected
+
+
+def test_4():
+    "WA"
+    word1 = "a"
+    word2 = "b"
+    expected = 1
     assert Solution().minDistance(word1, word2) == expected
