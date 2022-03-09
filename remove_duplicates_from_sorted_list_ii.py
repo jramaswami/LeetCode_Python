ec42378617326f9cf4835da000497f4f9752044b
Joseph Ramaswami <jramaswami@gmail.com>
2022-03-09 08:46:13 -0500

    WA

---
+++ remove_duplicates_from_sorted_list_ii.py
@@ -0,0 +1,51 @@
+"""
+LeetCode :: March 2022 Challenge :: 82. Remove Duplicates from Sorted List II
+jramaswami
+"""
+
+
+from leetcode_linked_lists import *
+
+
+class Solution:
+    def deleteDuplicates(self, head):
+        dummy = ListNode(-1000)
+
+        tail = dummy
+        curr = head
+        while curr:
+            candidate = curr
+            if candidate.next and candidate.val == candidate.next.val:
+                # Discard duplicate values
+                while curr and curr.val == candidate.val:
+                    curr = curr.next
+            else:
+                # Append nonduplicate value to new list.
+                tail.next = curr
+                tail = curr
+                curr = curr.next
+        return dummy.next
+
+
+def test_1():
+    head = [1,2,3,3,4,4,5]
+    expected = [1,2,5]
+    result = Solution().deleteDuplicates(make_list(head))
+    assert make_arr(result) == expected
+
+
+def test_2():
+    head = [1,1,1,2,3]
+    expected = [2,3]
+    result = Solution().deleteDuplicates(make_list(head))
+    assert make_arr(result) == expected
+
+
+def test_3():
+    "WA"
+    head = [1,2,2]
+    expected = [1]
+    result = Solution().deleteDuplicates(make_list(head))
+    assert make_arr(result) == expected
+
+
