ac607f4933e2e5a1dc1da03cd40cd404362f9a37
Joseph Ramaswami <jramaswami@gmail.com>
2022-03-19 10:31:32 -0400

    AC. Resolved for March 2020 Challenge.

--- maximum_frequency_stack.py
+++ maximum_frequency_stack.py
@@ -1,25 +1,28 @@
 """
-LeetCode :: Maximum Frequency Stack
+LeetCode :: March 2022 Challenge :: Maximum Frequency Stack
 jramaswami
 """
-from collections import defaultdict
+
+
+import collections
 import heapq
 
 
 class FreqStack:
 
     def __init__(self):
-        self.queue = []
-        self.freqs = defaultdict(int)
-        self.timer = -1
-        
-    def push(self, x: int) -> None:
+        # Stack is heap of (-freq, time, val)
+        self.stack = []
+        self.clock = 0
+        self.freqs = collections.defaultdict(int)
+
+    def push(self, x) -> None:
+        self.clock += 1
         self.freqs[x] += 1
-        heapq.heappush(self.queue, (-self.freqs[x], self.timer, x))
-        self.timer -=1
-        
+        heapq.heappush(self.stack, (-self.freqs[x], -self.clock, x))
+
     def pop(self) -> int:
-        _, _, x = heapq.heappop(self.queue)
+        _, _, x = heapq.heappop(self.stack)
         self.freqs[x] -= 1
         return x
 
@@ -46,7 +49,4 @@ def test_2():
     expected = [null,null,null,null,null,null,null,4,null,6,null,1,null,1,null,4,2,3,9,0,4]
     fs = FreqStack()
     for call, arg, exp in zip(calls[1:], args[1:], expected[1:]):
-        print(fs.freqs)
-        print(fs.queue)
-        print(call, arg, exp)
         assert getattr(fs, call)(*arg) == exp
