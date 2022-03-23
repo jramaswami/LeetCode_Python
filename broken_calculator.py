aa0487ddbefa4e53e47abcedb978c57b04c0b338
Joseph Ramaswami <jramaswami@gmail.com>
2022-03-22 22:04:06 -0400

    AC. Resolved for March 2022 Challenge.

--- broken_calculator.py
+++ broken_calculator.py
@@ -1,30 +1,37 @@
 """
-LeetCode :: Broken Calculator
+LeetCode :: March 2022 Challenge :: Broken Calculator
 jramaswami
-
-REF: https://www.geeksforgeeks.org/convert-number-m-n-using-minimum-number-given-operations/
 """
+
+
 class Solution:
-    def brokenCalc(self, X: int, Y: int) -> int:
+    def brokenCalc(self, X, Y):
+        # Boundary case.
         if X == Y:
             return 0
-        elif X > Y:
-            return X - Y
-        else:
+
+        soln = 0
+        while Y > X:
+            soln += 1
             if Y % 2:
-                return 1 + self.brokenCalc(X, Y + 1)
+                Y += 1
             else:
-                return 1 + self.brokenCalc(X, Y // 2)
+                Y //= 2
+
+        return soln + X - Y
 
 
 def test_1():
     assert Solution().brokenCalc(2, 3) == 2
 
+
 def test_2():
     assert Solution().brokenCalc(5, 8) == 2
 
+
 def test_3():
     assert Solution().brokenCalc(3, 10) == 3
+
 
 def test_4():
     assert Solution().brokenCalc(1024, 1) == 1023
