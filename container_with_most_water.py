e4d03d82a2748d49bd6dcd16d86a80b9a4a23343
Joseph Ramaswami <jramaswami@gmail.com>
2022-04-05 09:09:26 -0400

    TLE.  N^2 solution.

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -1,65 +1,18 @@
 """
-LeetCode :: Container With Most Water
+LeetCode :: April 2022 Challenge :: Container With Most Water
 jramaswami
 """
-from typing import *
-from math import inf
 
 
 class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        # Make an array of the largest item to the left, include index.
-        prefix_maxes = [-inf for _ in height]
-        curr_max = (-inf, 0)
-        for i, ht in enumerate(height):
-            if ht > curr_max[0]:
-                curr_max = (ht, i)
-            prefix_maxes[i] = curr_max
-
-        # Make an array of the largest item to the right, include index.
-        suffix_maxes = [-inf for _ in height]
-        curr_max = (-inf, 0)
-        for negi, ht in enumerate(reversed(height), start=-(len(height) - 1)):
-            if ht > curr_max[0]:
-                curr_max = (ht, -negi)
-            suffix_maxes[-negi] = curr_max
-
-        soln = -inf
-
-        # For each index and height
-        for i, ht in enumerate(height):
-            # Find the height farthest to the left that is greater than or
-            # equal to the current height.
-            low = 0
-            high = i - 1
-            left = inf
-            while low <= high:
-                mid = low + ((high - low) // 2)
-                if prefix_maxes[mid][0] >= ht:
-                    left = min(left, prefix_maxes[mid][1])
-                    high = mid - 1
-                else:
-                    low = mid + 1
-
-            # Find the height farthest to the right that is greater than or 
-            # equal to the current height.
-            low = i + 1
-            high = len(height) - 1
-            right = -inf
-            while low <= high:
-                mid = low + ((high - low) // 2)
-                if suffix_maxes[mid][0] >= ht:
-                    right = max(right, suffix_maxes[mid][1])
-                    low = mid + 1
-                else:
-                    high = mid - 1
-
-            # Determine the max area possible from this height.
-            if left != inf:
-                soln = max(soln, (i - left) * ht)
-            if right != -inf:
-                soln = max(soln, (right - i) * ht)
-
+    def maxArea(self, height):
+        soln = 0
+        left = [(0, height[0])]
+        for i, h in enumerate(height[1:], start=1):
+            for j, k in left:
+                soln = max(soln, min(h, k) * (i - j))
+            if h > left[0][1]:
+                left.append((i, h))
         return soln
 
 
