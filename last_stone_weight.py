ea49617ba6fb1869137cb22441362c326b3a8de7
Joseph Ramaswami <jramaswami@gmail.com>
2022-04-06 22:10:49 -0400

    AC

---
+++ last_stone_weight.py
@@ -0,0 +1,35 @@
+"""
+LeetCode :: April 2022 Challenge :: 1046. Last Stone Weight
+jramaswami
+"""
+
+
+import heapq
+
+
+class Solution:
+
+    def lastStoneWeight(self, stones):
+        heap = [-s for s in stones]
+        heapq.heapify(heap)
+        while len(heap) > 1:
+            y = -heapq.heappop(heap)
+            x = -heapq.heappop(heap)
+            z = y - x
+            if z:
+                heapq.heappush(heap, -z)
+        if heap:
+            return -heapq.heappop(heap)
+        return 0
+
+
+def test_1():
+    stones = [2,7,4,1,8,1]
+    expected = 1
+    assert Solution().lastStoneWeight(stones) == expected
+
+
+def test_2():
+    stones = [1]
+    expected = 1
+    assert Solution().lastStoneWeight(stones) == expected
