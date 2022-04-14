1f3afefb0ca772c80bad9f270a53b4ae95c14072
Joseph Ramaswami <jramaswami@gmail.com>
2022-04-14 08:22:57 -0400

    AC

---
+++ search_in_a_binary_tree.py
@@ -0,0 +1,16 @@
+"""
+LeetCode :: April 2022 Challenge :: 700. Search in a Binary Search Tree
+jramaswami
+"""
+
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root is None:
+            return None
+        elif val == root.val:
+            return root
+        elif val < root.val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
