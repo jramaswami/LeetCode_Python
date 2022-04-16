83a459b11470681a9e348460c734fa6522844a0c
Joseph Ramaswami <jramaswami@gmail.com>
2022-04-16 10:23:49 -0400

    AC. Resolved for April 2022 Challenge.

--- convert_bst_to_greater_tree.py
+++ convert_bst_to_greater_tree.py
@@ -1,55 +1,30 @@
 """
-LeetCode :: Convert BST to Greater Tree
+LeetCode :: April 2022 Challenge :: 538. Convert BST to Greater Tree
 jramaswami
 """
-from typing import *
 
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-    def __repr__(self):
-        return f"TreeNode(val={self.val} left={self.left}, right={self.right})"
-
-    def __eq__(self, other):
-        if isinstance(other, TreeNode):
-            return self.val == other.val and self.right == other.right and self.left == other.left
-        return False
-
-
-def solve(node, acc):
-    if node is None:
-        return 0
-
-    prev_val = node.val
-
-    # Right
-    sum_right = solve(node.right, acc)
-
-    # Node
-    node.val = prev_val + sum_right + acc
-
-    # Left
-    sum_left = solve(node.left, node.val)
-
-    return sum_left + sum_right + prev_val
-    
-    
 class Solution:
-    def convertBST(self, root: TreeNode) -> TreeNode:
-        solve(root, 0)
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def sum_right(node):
+            if node is None:
+                return 0
+
+            right_subtree_sum = sum_right(node.right)
+            left_subtree_sum = sum_right(node.left)
+
+            node.val += right_subtree_sum
+            return left_subtree_sum + node.val
+
+        def push_left(node, acc):
+            if node is None:
+                return
+
+            node.val += acc
+            push_left(node.left, node.val)
+            push_left(node.right, acc)
+
+
+        sum_right(root)
+        push_left(root, 0)
         return root
-
-
-def test_1():
-    root = TreeNode(4, TreeNode(1, TreeNode(0, None, None), TreeNode(2, None, TreeNode(3, None, None))),
-                       TreeNode(6, TreeNode(5, None, None), TreeNode(7, None, TreeNode(8, None, None))))
-    expected = TreeNode(30, TreeNode(36, TreeNode(36, None, None), TreeNode(35, None, TreeNode(33, None, None))),
-                       TreeNode(21, TreeNode(26, None, None), TreeNode(15, None, TreeNode(8, None, None))))
-    result = Solution().convertBST(root)
-    print(result)
-    print(expected)
-    assert result == expected
