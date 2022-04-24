7ec62414643852f144be543a0fda9f5f762779f6
Joseph Ramaswami <jramaswami@gmail.com>
2022-04-24 11:46:11 -0400

    AC. Resolved for April 2022 Challenge.

--- design_underground_system.py
+++ design_underground_system.py
@@ -1,41 +1,41 @@
 """
-LeetCode :: March 2021 Challenge :: Design Underground System
+LeetCode :: April 2022 Challenge :: Design Underground System
 jramaswami
 """
-from collections import defaultdict
+
+
+import collections
 
 
 class UndergroundSystem:
 
     def __init__(self):
-        self.travelers = dict()
-        self.travel_times = defaultdict(int)
-        self.travel_freqs = defaultdict(int)
-        
-    def checkIn(self, custid: int, stationName: str, t: int) -> None:
-        self.travelers[custid] = (stationName, t)
-        
-    def checkOut(self, custid: int, stationName: str, t: int) -> None:
-        stationName0, checkInTime = self.travelers[custid]
-        del self.travelers[custid]
-        if stationName0 > stationName:
-            stationName0, stationName = stationName, stationName0
-        key = (stationName0, stationName)
-        self.travel_times[key] += t - checkInTime
-        self.travel_freqs[key] += 1
-        
-    def getAverageTime(self, startStation: str, endStation: str) -> float:
-        if startStation > endStation:
-            startStation, endStation = endStation, startStation
-        key = (startStation, endStation)
-        result = self.travel_times[key] / self.travel_freqs[key]
-        return result
+        self.travel_time = collections.defaultdict(int)
+        self.travel_freq = collections.defaultdict(int)
+        self.curr_travelers = dict()
+
+    def checkIn(self, cust_id: int, station_name: str, checkin_t: int) -> None:
+        self.curr_travelers[cust_id] = (station_name, checkin_t)
+
+    def checkOut(self, cust_id: int, station_name: str, checkout_t: int) -> None:
+        prev_station, checkin_t = self.curr_travelers[cust_id]
+        key = (prev_station, station_name)
+        self.travel_freq[key] += 1
+        self.travel_time[key] += checkout_t - checkin_t
+        self.curr_travelers[cust_id] = None
+
+    def getAverageTime(self, start_station: str, end_station: str) -> float:
+        key = (start_station, end_station)
+        return self.travel_time[key] / self.travel_freq[key]
 
 
 #
 # Testing
 #
+
+
 null = None
+
 
 def test_1():
     ugs = UndergroundSystem()
@@ -48,6 +48,7 @@ def test_1():
             assert (abs(e - result) < 0.0001)
         else:
             assert result == e
+
 
 def test_2():
     ugs = UndergroundSystem()
