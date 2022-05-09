cb8efbde96ad0f89ee9a789ffd0e02b33a9af766
Joseph Ramaswami <jramaswami@gmail.com>
2022-05-09 10:18:51 -0400

    AC. Resolved for May 2022 Challenge.

--- letter_combinations_of_a_phone_number.py
+++ letter_combinations_of_a_phone_number.py
@@ -1,36 +1,18 @@
 """
-LeetCode :: April 2021 Challenge :: Letter Combinations of a Phone Number
+LeetCode :: May 2022 Challenge :: 17. Letter Combinations of a Phone Number
 jramaswami
 """
-from typing import *
+
+
+import itertools
+
+
+LETTER_MAP = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
 
 
 class Solution:
+
     def letterCombinations(self, digits: str) -> List[str]:
-        if digits == '':
+        if not digits:
             return []
-
-        letters = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno',
-                   '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
-
-        curr_soln = [tuple()]
-        next_soln = []
-        for digit in digits:
-            for letter in letters[digit]:
-                for soln in curr_soln:
-                    soln0 = list(soln)
-                    soln0.append(letter)
-                    next_soln.append(tuple(soln0))
-            curr_soln, next_soln = next_soln, []
-        
-        return sorted("".join(t) for t in curr_soln)
-                    
-
-def test_1():
-    assert Solution().letterCombinations("23") == ["ad","ae","af","bd","be","bf","cd","ce","cf"]
-
-def test_2():
-    assert Solution().letterCombinations("") == []
-
-def test_3():
-    assert Solution().letterCombinations("2") == ["a","b","c"]
+        return ["".join(p) for p in itertools.product(*[LETTER_MAP[int(i)] for i in digits])]
