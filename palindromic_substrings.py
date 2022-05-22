9ddf437f42176c427772de06d5d49f89008676ee
Joseph Ramaswami <jramaswami@gmail.com>
2022-05-22 07:16:43 -0400

    AC. Resolved for May 2020 Challenge.

--- palindromic_substrings.py
+++ palindromic_substrings.py
@@ -1,35 +1,40 @@
 """
-LeetCode :: March 2021 Challenge :: Palindromic Substrings
+LeetCode :: May 2020 Challenge :: Palindromic Substrings
 jramaswami
 """
+
+
+import collections
+
+
+QItem = collections.namedtuple('QItem', ['left', 'right'])
+
+
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        dp = [[False for _ in enumerate(s)] for _ in enumerate(s)]
-        count = 0
-        for offset in range(0, len(s)):
-            for start_index, _ in enumerate(s):
-                end_index = start_index + offset
-                if end_index >= len(s):
-                    break
-                if start_index == end_index:
-                    dp[start_index][end_index] = True
-                elif end_index - start_index == 1:
-                    dp[start_index][end_index] = s[start_index] == s[end_index]
-                else:
-                    dp[start_index][end_index] = (s[start_index] == s[end_index]
-                            and dp[start_index + 1][end_index - 1]
-                    )
+        # All one character strings are palindromes.
+        queue = collections.deque(QItem(i, i) for i, _ in enumerate(s))
+        # Collect two character palindromes.
+        queue.extend(QItem(i, i+1) for i, _ in enumerate(s[:-1]) if s[i] == s[i+1])
+        soln = 0
+        while queue:
+            item = queue.popleft()
+            soln += 1
+            if item.left - 1 >= 0 and item.right + 1 < len(s):
+                # If we can extend the item ...
+                if s[item.left - 1] == s[item.right + 1]:
+                    # ... and the extensions are equal, add to queue.
+                    queue.append(QItem(item.left - 1, item.right + 1))
+        return soln
 
-                if dp[start_index][end_index]:
-                    count += 1
-        return count
-        
 
 def test_1():
     assert Solution().countSubstrings("abc") == 3
 
+
 def test_2():
     assert Solution().countSubstrings("aaa") == 6
+
 
 def test_3():
     s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
