8262aa18128fa4ad60cce495242c9ec6f74f3bd3
Joseph Ramaswami <jramaswami@gmail.com>
2022-05-26 16:08:24 -0400

    AC. Resolved for May 2022 Challenge.

--- number_of_1_bits.py
+++ number_of_1_bits.py
@@ -1,9 +1,11 @@
 """
-LeetCode :: 191. Number of 1 Bits
+LeetCode :: May 2022 Challenge :: 191. Number of 1 Bits
 jramaswami
 """
+
+
 class Solution:
-    def hammingWeight(self, n: int) -> int:
+    def hammingWeight(self, n):
         soln = 0
         for i in range(33):
             mask = 1 << i
