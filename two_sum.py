e4f29721a52589a03bf59e8c0ac482c0d9deb0bd
Joseph Ramaswami <jramaswami@gmail.com>
2022-05-31 18:33:15 -0400

    Resolved for Grind 75.

--- two_sum.py
+++ two_sum.py
@@ -1,24 +1,17 @@
 """
-LeetCode :: August 2021 Challenge :: Two Sum
+LeetCode :: Grind 75 :: Two Sum
 """
-
-
-import collections
 
 
 class Solution:
     def twoSum(self, nums, target):
-        nums0 = collections.defaultdict(list)
+        prevs = dict()
         for i, n in enumerate(nums):
-            nums0[n].append(i)
+            d = target - n
+            if d in prevs:
+                return [min(i, prevs[d]), max(i, prevs[d])]
+            prevs[n] = i
 
-        for n in nums0:
-            m = target - n
-            if m == n:
-                if len(nums0[n]) > 1:
-                    return nums0[n][:2]
-            elif m in nums0:
-                return [nums0[n][0], nums0[m][0]]
 
 
 def test_1():
@@ -40,4 +33,3 @@ def test_3():
     target = 6
     expected = [0, 1]
     assert Solution().twoSum(nums, target) == expected
-
