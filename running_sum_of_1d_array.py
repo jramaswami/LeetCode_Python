6b8041d19751b864efd3a2e875f326de43c30610
Joseph Ramaswami <jramaswami@gmail.com>
2022-05-31 22:05:40 -0400

    AC. Resolved for June 2022 Challenge.

--- running_sum_of_1d_array.py
+++ running_sum_of_1d_array.py
@@ -1,10 +1,30 @@
 """
-Leet Code :: May 2021 Challenge :: Running Sum of 1d Array
+Leet Code :: June 2022 Challenge :: Running Sum of 1d Array
 jramaswami
 """
-from itertools import accumulate
+
+
+import itertools
 
 
 class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        return list(accumulate(nums))
+    def runningSum(self, nums):
+        return list(itertools.accumulate(nums))
+
+
+def test_1():
+    nums = [1,2,3,4]
+    expected = [1,3,6,10]
+    assert Solution().runningSum(nums) == expected
+
+
+def test_2():
+    nums = [1,1,1,1,1]
+    expected = [1,2,3,4,5]
+    assert Solution().runningSum(nums) == expected
+
+
+def test_3():
+    nums = [3,1,2,10,1]
+    expected = [3,4,6,16,17]
+    assert Solution().runningSum(nums) == expected
