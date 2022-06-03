59a55ed84747846ae24abdfe97bb6fbe7b438ca8
Joseph Ramaswami <jramaswami@gmail.com>
2022-06-03 12:58:53 -0400

    AC. Resolved for June 2022 Challenge.

--- range_sum_query_2d_immutable.py
+++ range_sum_query_2d_immutable.py
@@ -1,18 +1,35 @@
 """
-Leet Code :: May 2021 Challenge :: Range Sum Query 2D - Immutable
+Leet Code :: June 2022 Challenge :: Range Sum Query 2D - Immutable
 jramaswami
 """
+
+
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
         self.matrix = matrix
+        self.prefix = [[0 for _ in row] for row in matrix]
+        for r, row in enumerate(matrix):
+            for c, _ in enumerate(row):
+                self.prefix[r][c] = (
+                    self.matrix[r][c] +
+                    self._get_prefix(r-1, c) +
+                    self._get_prefix(r, c-1) -
+                    self._get_prefix(r-1, c-1)
+                )
+
+    def _get_prefix(self, r, c):
+        if r < 0 or c < 0:
+            return 0
+        return self.prefix[r][c]
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        soln = 0
-        for row in self.matrix[row1:row2+1]:
-            soln += sum(row[col1:col2+1])
-        return soln       
-
+        return (
+            self._get_prefix(row2, col2) -
+            self._get_prefix(row1-1, col2) -
+            self._get_prefix(row2, col1-1) +
+            self._get_prefix(row1-1, col1-1)
+        )
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
