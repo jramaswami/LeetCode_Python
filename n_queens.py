7abcd8b32885a7e2cdc909dac114ac41fc5a4fc4
Joseph Ramaswami <jramaswami@gmail.com>
2022-06-04 09:36:51 -0400

    AC. Resolved for June 2022 Challenge.

--- n_queens.py
+++ n_queens.py
@@ -1,57 +1,65 @@
 """
-Leet Code :: May 2021 Challenge :: N-Queens
+Leet Code :: June 2022 Challenge :: N-Queens
 jramaswami
 """
 
 
-from typing import *
-from collections import namedtuple
-from itertools import combinations
+import collections
 
 
-Queen = namedtuple('Queen', ['rank', 'file'])
+class Solver:
+    def __init__(self, dim):
+        self.dim = dim
+        self.rows = [-1 for _ in range(dim)]
+        self.neg_diag = collections.defaultdict(bool)
+        self.pos_diag = collections.defaultdict(bool)
+        self.solns = []
 
-
-def make_board(queens, n):
-    """Return a string representation of the board."""
-    board = [['.' for _ in range(n)] for _ in range(n)]
-    for queen in queens:
-        board[queen.rank][queen.file] = 'Q'
-    return ["".join(row) for row in board]
-
-
-def peaceful(queens):
-    """Return True if no queen is under threat."""
-    # Base case: only 1 queen is always safe.
-    if len(queens) <= 1:
+    def _peaceful(self, row, col):
+        # I can place a queen at (row, col) if:
+        # (1) That row is not already taken.
+        if self.rows[row] != -1:
+            return False
+        # (2) The neg_diag is not occupied.
+        if self.neg_diag[row - col] == True:
+            return False
+        # (3) The pos_diag is not occupied.
+        if self.pos_diag[row + col] == True:
+            return False
         return True
-    for q1, q2 in combinations(queens, 2):
-        # Make sure there are no more than 1 queen rank and file.
-        if q1.rank == q2.rank or q1.file == q2.file:
-            return False
-        # Check diagonal attacks
-        if abs(q1.rank - q2.rank) == abs(q1.file - q2.file):
-            return False
-    return True
 
+    def _render_board(self):
+        board = [["." for _ in range(self.dim)] for _ in range(self.dim)]
+        for row, col in enumerate(self.rows):
+            board[row][col] = "Q"
+        return ["".join(row) for row in board]
 
-def solve(queens, n, acc):
-    if len(queens) == n:
-        acc.append(list(queens))
-        return
+    def _solve(self, col):
+        # Base case
+        if col >= self.dim:
+            # Add board
+            self.solns.append(self._render_board())
+            return
 
-    for chess_rank in range(n):
-        queens.append(Queen(chess_rank, len(queens)))
-        if peaceful(queens):
-            solve(queens, n, acc)
-        queens.pop()
+        for row in range(self.dim):
+            if self._peaceful(row, col):
+                self.rows[row] = col
+                self.neg_diag[row - col] = True
+                self.pos_diag[row + col] = True
+                self._solve(col+1)
+                self.rows[row] = -1
+                self.neg_diag[row - col] = False
+                self.pos_diag[row + col] = False
+
+    def solve(self):
+        self._solve(0)
+        return self.solns
 
 
 class Solution:
-    def solveNQueens(self, n: int) -> List[List[str]]:
-        acc = []
-        solve([], n, acc)
-        return [make_board(t, n) for t in acc]
+    def solveNQueens(self, n):
+        solver = Solver(n)
+        return solver.solve()
 
 
 def test_1():
