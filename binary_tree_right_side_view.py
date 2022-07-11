a16a8931199969bc6c9b2f77aac3e40d8d1aca08
Joseph Ramaswami <jramaswami@gmail.com>
2022-07-10 22:01:20 -0400

    AC. Resolved for July 2022 Challenge.

--- binary_tree_right_side_view.py
+++ binary_tree_right_side_view.py
@@ -1,23 +1,50 @@
 """
-LeetCode :: Binary Tree Right Side View
+LeetCode :: July 2022 Challenge Binary Tree Right Side View
 jramaswami
 """
-def solve(node, level, acc):
-    if node is None:
-        return
-    
-    # Preorder: node, left, right
-    if level >= len(acc):
-        acc.append(node.val)
-    else:
-        acc[level] = node.val
-    
-    solve(node.left, level + 1, acc)
-    solve(node.right, level + 1, acc)
-    
-    
+
+
 class Solution:
-    def rightSideView(self, root: TreeNode) -> List[int]:
-        acc = []
-        solve(root, 0, acc)
-        return acc
+    def rightSideView(self, root):
+
+        soln = []
+
+        def reverse_inorder(node, level):
+            if node is None:
+                return
+
+            while level >= len(soln):
+                soln.append(None)
+            reverse_inorder(node.right, level+1)
+            if soln[level] is None:
+                soln[level] = node.val
+            reverse_inorder(node.left, level+1)
+
+        reverse_inorder(root, 0)
+        return soln
+
+
+#
+# Testing
+#
+
+
+from leetcode_trees import *
+
+
+def test_1():
+    root = make_tree([1,2,3,null,5,null,4])
+    expected = [1, 3, 4]
+    assert Solution().rightSideView(root) == expected
+
+
+def test_2():
+    root = make_tree([1,null,3])
+    expected = [1,3]
+    assert Solution().rightSideView(root) == expected
+
+
+def test_3():
+    root = None
+    expected = []
+    assert Solution().rightSideView(root) == expected
