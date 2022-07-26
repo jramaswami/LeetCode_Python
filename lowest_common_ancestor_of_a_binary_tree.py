f2d7a734daf606130921802fbaeacf108e5ff3c5
Joseph Ramaswami <jramaswami@gmail.com>
2022-07-26 13:05:42 -0400

    Resolved for July 2022 Challenge.

--- lowest_common_ancestor_of_a_binary_tree.py
+++ lowest_common_ancestor_of_a_binary_tree.py
@@ -4,47 +4,27 @@ jramaswami
 """
 
 
-from collections import deque
-
-
 class Solution():
     def lowestCommonAncestor(self, root, p_node, q_node):
-        # First, use a BFS to traverse tree and assign a level to each node
-        # and a parent for each node.  Dictionaries can be used for this
-        # because node values are unique.  Each node is enqueued for an O(N)
-        # time complexity.  With level, parent, and queue we need O(N) extra
-        # space.
-        level = dict()
-        parent = dict()
-        queue = deque()
-        queue.append((root, 0))
-        parent[root] = None
-        while queue:
-            node, lvl = queue.popleft()
-            level[node] = lvl
-            if node.left is not None:
-                parent[node.left] = node
-                queue.append((node.left, lvl+1))
-            if node.right is not None:
-                parent[node.right] = node
-                queue.append((node.right, lvl+1))
 
-        # Finding the LCA will at most require traversing the entire
-        # length of the tree, for O(N) time complexity.
+        def dfs(node, parent, level):
+            if node is None:
+                return
+            node.level = level
+            node.parent = parent
+            dfs(node.left, node, level + 1)
+            dfs(node.right, node, level + 1)
 
-        # Make sure that q_node is the node with the highest level.
-        if level[q_node] < level[p_node]:
-            p_node, q_node = q_node, p_node
+        dfs(root, None, 0)
 
-        # Move q_node up until it is at the same level as p_node.
-        while level[q_node] > level[p_node]:
-            q_node = parent[q_node]
+        while p_node.level > q_node.level:
+            p_node = p_node.parent
 
-        # Now that the nodes are at the same level, move them both up
-        # until they are equal.
+        while q_node.level > p_node.level:
+            q_node = q_node.parent
+
         while q_node != p_node:
-            q_node = parent[q_node]
-            p_node = parent[p_node]
+            p_node = p_node.parent
+            q_node = q_node.parent
 
-        # Overall, this is a O(N) solution.
-        return q_node
+        return p_node
