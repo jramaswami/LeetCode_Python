8047e3114720332747b7bb44fefd39490e7cd4df
Joseph Ramaswami <jramaswami@gmail.com>
2022-07-26 22:35:39 -0400

    AC

--- flatten_binary_tree_to_linked_list.py
+++ flatten_binary_tree_to_linked_list.py
@@ -1,32 +1,39 @@
 """
-Leet Code :: May 2021 Challenge :: Flatten Binary Tree to Linked List
+Leet Code :: July 2022 Challenge :: Flatten Binary Tree to Linked List
 jramaswami
 """
+
+
 from typing import *
-
-
-def flatten(node, preorder):
-    """Flatten nodes into an array in preorder traversal."""
-    if node is None:
-        return
-    preorder.append(node)
-    flatten(node.left, preorder)
-    flatten(node.right, preorder)
-    node.left = None
-    node.right = None
+from leetcode_trees import *
 
 
 class Solution:
-    def flatten(self, root: TreeNode) -> None:
+    def flatten(self, root: Optional[TreeNode]) -> None:
         """
         Do not return anything, modify root in-place instead.
         """
-        # Flatten the array into preorder traversal.
-        preorder = []
-        flatten(root, preorder[1:])
-        # Reconnect all the nodes leaning right.
-        node = root
-        for right in preorder:
-            node.right = right
-            node = right
-        return root
+        # Boundary case.
+        if root is None:
+            return None
+
+        # Create a list of nodes inorder.
+        inorder = []
+        def traverse(node):
+            if node is None:
+                return
+
+            inorder.append(node)
+            traverse(node.left)
+            traverse(node.right)
+
+        traverse(root)
+
+        print(inorder)
+
+        # Fix the nodes in the list.
+        for a, b in zip(inorder[:-1], inorder[1:]):
+            a.left, a.right = None, None
+            b.left, b.right = None, None
+            a.right = b
+        return inorder[0]
