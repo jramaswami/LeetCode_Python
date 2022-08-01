c0a3ac0db3dafa3be2e9e64e1714eb1241e5e6e8
Joseph Ramaswami <jramaswami@gmail.com>
2022-08-01 06:42:27 -0400

    AC. Resolved for August 2022 Challenge.

--- unique_paths.py
+++ unique_paths.py
@@ -1,18 +1,31 @@
 """
-LeetCode :: November 2021 Challenge :: 62. Unique Paths
+LeetCode :: July 2022 Challenge :: 62. Unique Paths
 jramaswami
 """
 
 
-import math
-
-
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        dp[0][0] = 1
+        for r, row in enumerate(dp):
+            for c, _ in enumerate(row):
+                if c + 1 < n:
+                    dp[r][c+1] += dp[r][c]
+                if r + 1 < m:
+                    dp[r+1][c] += dp[r][c]
+        return dp[-1][-1]
 
-        def nCk(n, k):
-            return math.factorial(n) // (math.factorial(k) * math.factorial(n - k))
 
-        m -= 1
-        n -= 1
-        return nCk(m + n, m)
+def test_1():
+    m = 3
+    n = 7
+    expected = 28
+    assert Solution().uniquePaths(m, n) == expected
+
+
+def test_2():
+    m = 3
+    n = 2
+    expected = 3
+    assert Solution().uniquePaths(m, n) == expected
