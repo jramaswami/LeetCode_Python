a7999b2448ac906b732795ab2730c30b13b2596d
Joseph Ramaswami <jramaswami@gmail.com>
2022-08-10 11:24:07 -0400

    AC

--- utf_8_validation.py
+++ utf_8_validation.py
@@ -18,11 +18,11 @@ class Solution:
             for b in range(7, 3, -1):
                 if (1 << b) & k:
                     bs += 1
+                else:
+                    break
             return bs
 
         def validate_char(char):
-            if len(char) == 1:
-                return True
             # For n-bytes chars the first byte must have
             # n 1-bits followed by a 0-bit
             # The rest of the bits must start with 10
@@ -35,8 +35,11 @@ class Solution:
             elif len(char) == 2:
                 mask = int("11100000", 2)
                 expected = int("11000000", 2)
+            elif len(char) == 1:
+                mask = int("10000000", 2)
+                expected = int("00000000", 2)
             if mask & char[0] != expected:
-                # print(f"Header 1: {len(A)} {char[0]:08b}")
+                # print(f"Header 1: {len(char)} {char[0]:08b}")
                 return False
             mask = int("11000000", 2)
             expected = int("10000000", 2)
@@ -51,6 +54,9 @@ class Solution:
         while i < len(data):
             b = expected_bytes(data[i])
             chars.append(data[i:i+b])
+            # Make sure we got enough bytes.
+            if len(chars[-1]) != b:
+                return False
             i += b
         return all(validate_char(c) for c in chars)
 
@@ -71,5 +77,20 @@ def test_2():
 def test_3():
     "WA"
     data = [235]
+    expected = False
+    assert Solution().validUtf8(data) == expected
+
+
+def test_4():
+    "WA"
+    data = [194,155,231,184,185,246,176,131,161,222,174,227,162,134,241,154,168,185,218,178,229,187,139,246,178,187,139,204,146,225,148,179,245,139,172,134,193,156,233,131,154,240,166,188,190,216,150,230,145,144,240,167,140,163,221,190,238,168,139,241,154,159,164,199,170,224,173,140,244,182,143,134,206,181,227,172,141,241,146,159,170,202,134,230,142,163,244,172,140,191]
+    expected = True
+    assert Solution().validUtf8(data) == expected
+
+
+def test_5():
+    "WA"
+    data = [145]
+    print(f"{145:08b}")
     expected = False
     assert Solution().validUtf8(data) == expected
