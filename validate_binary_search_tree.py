35fb0a28fc59583e521f7c226f7a15c53793ff78
Joseph Ramaswami <jramaswami@gmail.com>
2022-08-10 20:28:18 -0400

    WA

--- validate_binary_search_tree.py
+++ validate_binary_search_tree.py
@@ -2,55 +2,70 @@
 LeetCode :: Validate Binary Search Tree
 jramaswami
 """
-from leetcode_bst import *
-from typing import *
-from math import inf
 
 
-def solve0(node, less_than, more_than):
-    if node is None:
-        return True
-
-    if less_than <= node.val:
-        return False
-
-    if more_than >= node.val:
-        return False
-
-    return (solve0(node.left, node.val, more_than) and
-            solve0(node.right, less_than, node.val))
+import math
 
 
 class Solution:
-    def isValidBST(self, root: TreeNode) -> bool:
-        # Invariant in binary search tree is that for a given node,
-        # it is larger than all items in its left subtree and less than
-        # all the items in its right subtree.
-        return solve0(root, inf, -inf)
+    def isValidBST(self, root):
+
+        def traverse(node):
+            # The left subtree of a node contains only nodes with keys less
+            # than the node's key.
+            nmin, nmax, nvalid = node.val, node.val, True
+            if node.left:
+                lmin, lmax, lvalid = traverse(node.left)
+                if lmax >= node.val:
+                    nvalid = False
+                nmin = min(nmin, lmin)
+                nmax = max(nmax, lmax)
+
+            # The right subtree of a node contains only nodes with keys
+            # greater than the node's key.
+            if node.right:
+                rmin, rmax, rvalid = traverse(node.right)
+                if rmin <= node.val:
+                    nvalid = False
+                nmin = min(nmin, rmin)
+                nmax = max(nmax, rmax)
+
+            return nmin, nmax, nvalid
+
+        _, _, soln = traverse(root)
+        return soln
 
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 
+#
 # Testing
+#
+
+
+from leetcode_bst import *
+
 
 def test_1():
     root = make_tree([2,1,3])
     assert Solution().isValidBST(root) == True
 
+
 def test_2():
     root = make_tree([5,1,4,null,null,3,6])
     assert Solution().isValidBST(root) == False
+
 
 def test_3():
     root = make_tree([1,1])
     assert Solution().isValidBST(root) == False
 
+
 def test_4():
     root = make_tree([5,4,6,null,null,3,7])
     assert Solution().isValidBST(root) == False
 
+
+def test_5():
+    "WA"
+    root = make_tree([32,26,47,19,null,null,56,null,27])
+    assert Solution().isValidBST(root) == False
