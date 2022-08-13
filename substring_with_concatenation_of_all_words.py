b1fb07e97e087df6003609a85485cff97ee0ad92
Joseph Ramaswami <jramaswami@gmail.com>
2022-08-13 10:57:14 -0400

    WA

--- substring_with_concatenation_of_all_words.py
+++ substring_with_concatenation_of_all_words.py
@@ -5,31 +5,41 @@ jramaswami
 
 
 from typing import *
+import collections
 
 
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         k = len(words[0])
-        words0 = set(words)
+        words0 = collections.Counter(words)
         intervals = []
         for i, _ in enumerate(s):
-            if i + k >= len(s):
+            if i + k > len(s):
                 break
             if s[i:i+k] in words0:
                 intervals.append((i, i+k))
 
+        print(intervals)
         soln = []
         for i, (a, b) in enumerate(intervals):
-            curr = set([s[a:b]])
+            print(f"{a=} {b=} {s[a:b]=}")
+            curr = collections.defaultdict(int)
+            curr[s[a:b]] += 1
             prev_stop = b
+            words_found = 1
             for j, (x, y) in enumerate(intervals[i+1:]):
+                print(f"{x=} {y=} {s[x:y]=} {curr=}")
                 if prev_stop != x:
+                    print('gap')
                     break
                 t = s[x:y]
-                if t in curr:
+                if curr[t] == words0[t]:
+                    print(f"too many {t}")
                     break
-                curr.add(t)
-                if len(curr) == len(words):
+                curr[t] +=1
+                words_found += 1
+                print(f"{words_found=} {len(words)=}")
+                if words_found == len(words):
                     soln.append(a)
                     break
                 prev_stop = y
@@ -64,3 +74,11 @@ def test_4():
     words = ["word","good","best","good"]
     expected = [8]
     assert Solution().findSubstring(s, words) == expected
+
+def test_5():
+    "WA"
+    s = "a"
+    words = ["a"]
+    expected = [0]
+    assert Solution().findSubstring(s, words) == expected
+
