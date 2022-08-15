d1e91695d00e443baad5acd30902cd7ecaeaade3
Joseph Ramaswami <jramaswami@gmail.com>
2022-08-15 12:06:16 -0400

    AC. Resolved for August 2022 Challenge.

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -1,41 +1,40 @@
 """
-LeetCode :: Roman to Integer
+LeetCode :: August 2022 Challenge :: Roman to Integer
 jramaswami
 """
+
+
 class Solution:
     def romanToInt(self, s: str) -> int:
-        value_of = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 
-                    'C': 100, 'D': 500, 'M': 1000
-        }
-
+        symbols = (
+            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+            (10, "X"), (9, "IX"), (5, "V",), (4, "IV"), (1, "I")
+        )
         soln = 0
         i = 0
-        while i < len(s):
-            if i + 1 < len(s):
-                if value_of[s[i]] < value_of[s[i+1]]:
-                    # E.g. IV
-                    soln += (value_of[s[i+1]] - value_of[s[i]])
-                    i += 2
-                else:
-                    soln += value_of[s[i]]
-                    i += 1
-            else:
-                soln += value_of[s[i]]
-                i += 1
+        for v, t in symbols:
+            while s[i:i+len(t)] == t:
+                soln += v
+                i += len(t)
         return soln
 
 
 def test_1():
     assert Solution().romanToInt("III") == 3
 
+
 def test_2():
     assert Solution().romanToInt("IV") == 4
+
 
 def test_3():
     assert Solution().romanToInt("IX") == 9
 
+
 def test_4():
     assert Solution().romanToInt("LVIII") == 58
+
 
 def test_5():
     assert Solution().romanToInt("MCMXCIV") == 1994
