2150fddf8206c32b04f4396885bc867a14945f6d
Joseph Ramaswami <jramaswami@gmail.com>
2022-08-21 09:48:16 -0400

    WA

--- stamping_the_sequence.py
+++ stamping_the_sequence.py
@@ -1,83 +1,69 @@
 """
-LeetCode :: March 2021 Challenge :: Stamping the Sequence
+LeetCode :: August Challenge :: Stamping the Sequence
 jramaswami
+
+REF: https://www.youtube.com/watch?v=e3SR3NpebEU
 """
+
+
 from typing import *
-
-
-def left_match(target_index, target, stamp):
-    """Can you match left to right?"""
-    ok = False
-    for i, _ in enumerate(stamp):
-        if target_index + i >= len(target):
-            return False
-
-        if target[target_index + i] == '?':
-            continue
-        else:
-            ok = True
-        if stamp[i] != target[target_index + i]:
-            return False
-    return ok and True
-
-
-def right_match(target_index, target, stamp):
-    """Can you match right to left?"""
-    ok = False
-    for off, _ in enumerate(stamp):
-        if target_index - off < 0:
-            return False
-        if target[target_index - off] == '?':
-            continue
-        else:
-            ok = True
-        if stamp[len(stamp) - off - 1] != target[target_index - off]:
-            return False
-    return ok and True
-
-
-def question_from_left(target_index, target, stamp):
-    """Replace chars with questions marks left to right."""
-    replacements = 0
-    for i, _ in enumerate(stamp):
-        if target[target_index + i] != "?":
-            target[target_index + i] = "?"
-            replacements += 1
-    return replacements
-
-
-def question_from_right(target_index, target, stamp):
-    """Replace chars with questions marks right to left."""
-    replacements = 0
-    for i, _ in enumerate(stamp):
-        if target[target_index - i] != "?":
-            target[target_index - i] = "?"
-            replacements += 1
-    return replacements
 
 
 class Solution:
     def movesToStamp(self, stamp: str, target: str) -> List[int]:
-        target0 = list(target)
+        target = list(target)
+        def matches(i):
+            """
+            Return True if you can stamp starting at i and get a match.
+            Exclude a match that is all "?"s
+            """
+            found_nonq = False
+            for j, b in enumerate(target[i:], start=i):
+                if j - i >= len(stamp):
+                    break
+                a = stamp[j-i]
+                if b != "?" and a != b:
+                    return False
+                if b != "?":
+                    found_nonq = True
+            return found_nonq
+
+        def find_first_stamp():
+            "Return the index of the first matching stamp."
+            print(f"find_first_stamp() {stamp=} {''.join(target)}")
+            for i, _ in enumerate(target[:-len(stamp)+1]):
+                print(i, "".join(target[i:]))
+                if matches(i):
+                    return i
+            return -1
+
+        def do_stamp(i):
+            "Stamp starting at i."
+            for j, _ in enumerate(stamp):
+                if i + j >= len(target):
+                    break
+                target[i + j] = '?'
+
         soln = []
-        questions = 0
-        while questions < len(target0):
-            found = False
-            for target_index, _ in enumerate(target0):
-                if left_match(target_index, target0, stamp):
-                    found = True
-                    soln.append(target_index)
-                    questions += question_from_left(target_index, target0, stamp)
-                elif right_match(target_index, target0, stamp):
-                    found = True
-                    soln.append(target_index - len(stamp) + 1)
-                    questions += question_from_right(target_index, target0, stamp)
-            if not found:
-                return []
+        x = 0
+        while any(t != "?" for t in target):
+            # Find first match of stamp.
+            i = find_first_stamp()
+            print("".join(target), i)
+            if i < 0:
+                soln = []
+                break
+            soln.append(i)
+            do_stamp(i)
+            x += 1
+            if x > 10 * len(target):
+                soln = []
+                break
+        print("".join(target), stamp)
         return soln[::-1]
 
 
-# 
+#
 # TESTING
 #
 def verify(stamp_indices, target, stamp):
@@ -115,4 +101,22 @@ def test_4():
     stamp = "zbs"
     target = "zbzbsbszbssbzbszbsss"
     result = Solution().movesToStamp(stamp, target)
+    print(result)
     assert verify(result, target, stamp)
+
+
+def test_5():
+    "WA"
+    stamp = "bedaefaeddccbce"
+    target = "bebedabebbedaefaeddccbced"
+    result = Solution().movesToStamp(stamp, target)
+    # assert verify(result, target, stamp)
+    assert result == []
+
+def test_6():
+    "WA"
+    stamp = "v"
+    target = "v"
+    result = Solution().movesToStamp(stamp, target)
+    # assert verify(result, target, stamp)
+    assert result == [0]
