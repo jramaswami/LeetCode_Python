9c49d979dbf717f92e4cbc90ff28e58cd55de95f
Joseph Ramaswami <jramaswami@gmail.com>
2022-08-23 21:37:36 -0400

    AC. Resolved for August 2022 Challenge.

--- power_of_three.py
+++ power_of_three.py
@@ -4,14 +4,25 @@ jramaswami
 """
 
 
-import math
-
-
 class Solution:
+    def __init__(self):
+        # Cache the largest power of three more than the limit of 2^32.
+        # This number will be divisible evenly by any other power of three
+        # less thatn 2^32.
+        self.T = 1
+        L = pow(2, 32)
+        while self.T < L:
+            self.T *= 3
+
     def isPowerOfThree(self, n: int) -> bool:
-        return n > 0 and math.log(n, 3).is_integer()
+        return n > 0 and self.T % n == 0
 
 
 def test_1():
     "WA"
     assert Solution().isPowerOfThree(243) == True
+
+
+def test_2():
+    "WA"
+    assert Solution().isPowerOfThree(45) == False
