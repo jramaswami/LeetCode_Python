52287951bdf1ed5834248a08fc95849fc3efc4e8
Joseph Ramaswami <jramaswami@gmail.com>
2022-08-23 22:10:01 -0400

    TLE

--- serialize_and_deserialize_binary_tree.py
+++ serialize_and_deserialize_binary_tree.py
@@ -11,20 +11,21 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        H = []
+        NULL = "1111111111"
+        H = [NULL]
         def traverse(node, i):
             if node is None:
                 return
 
             while len(H) <= i:
-                H.append(None)
+                H.append(NULL)
 
-            H[i] = node.val
+            H[i] = f"{node.val:010b}")
             traverse(node.left, i*2)
             traverse(node.right, (i*2)+1)
 
         traverse(root, 1)
-        return str(H)
+        return "".join(H)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -32,17 +33,16 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        if data == "[]":
+        NULL = "1111111111"
+        if data == [NULL]:
             return None
-        H = [t.strip() for t in data[1:-1].split(",")]
-
         def traverse(i):
             if i >= len(H):
                 return None
-            if H[i] == "None":
+            if data[i*10:(i+1)*10] == NULL:
                 return None
             left_child = traverse(i * 2)
             right_child = traverse((i * 2) + 1)
-            return TreeNode(int(H[i]), left_child, right_child)
+            return TreeNode(int(data[i*10:(i+1)*10], 2), left_child, right_child)
 
         return traverse(1)
