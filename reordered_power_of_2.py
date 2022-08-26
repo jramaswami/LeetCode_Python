0dc79bac21b55c7f5282f19cd557cdf67ae47aff
Joseph Ramaswami <jramaswami@gmail.com>
2022-08-25 23:11:32 -0400

    AC. Resolved for August 2022 Challenge.

--- reordered_power_of_2.py
+++ reordered_power_of_2.py
@@ -1,35 +1,51 @@
 """
-LeetCode :: March 2021 Challenge :: Reordered Power of 2
+LeetCode :: August 2022 Challenge :: Reordered Power of 2
 jramaswami
 """
+
+
+def compute_digit_freqs(n):
+    "Return frequency of each digit."
+    freqs = [0 for _ in range(10)]
+    while n:
+        n, r = divmod(n, 10)
+        freqs[r] += 1
+    return tuple(freqs)
+
+
 class Solution:
     def __init__(self):
-        # Initialize cache of powers of 2 by the number of digits.
-        self.digits = [set() for _ in range(10)]
-        p = 1
-        while p <= pow(10, 9):
-            s = str(p)
-            index = len(s)
-            key = "".join(sorted(s))
-            self.digits[index].add(key)
-            p *= 2
+        "Cache the frequency of digits in each power of 2."
+        limit = pow(10, 9)
+        n = 1
+        self.cache = set()
+        while n <= limit:
+            self.cache.add(compute_digit_freqs(n))
+            n *= 2
 
     def reorderedPowerOf2(self, N: int) -> bool:
-        key = "".join(sorted(str(N)))
-        return key in self.digits[len(key)]
+        "Return True if you can reorder the digits of N into a power of 2."
+        if compute_digit_freqs(N) in self.cache:
+            return True
+        return False
+
 
 
 def test_1():
     assert Solution().reorderedPowerOf2(1) == True
 
+
 def test_2():
     assert Solution().reorderedPowerOf2(10) == False
+
 
 def test_3():
     assert Solution().reorderedPowerOf2(16) == True
 
+
 def test_4():
     assert Solution().reorderedPowerOf2(24) == False
+
 
 def test_5():
     assert Solution().reorderedPowerOf2(46) == True
