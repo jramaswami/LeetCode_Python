d883ac35553bb8e98b53259226e323412be13300
Joseph Ramaswami <jramaswami@gmail.com>
2022-09-05 10:03:11 -0400

    AC. Resolved for September 2022 Challenge.

--- n_ary_tree_level_order_traversal.py
+++ n_ary_tree_level_order_traversal.py
@@ -1,27 +1,23 @@
 """
-LeetCode :: August 2021 Challenge :: N-ary Tree Level Order Traversal
+LeetCode :: September 2022 Challenge :: 429. N-ary Tree Level Order Traversal
 jramaswami
-
-# Definition for a Node.
-class Node:
-    def __init__(self, val=None, children=None):
-        self.val = val
-        self.children = children
 """
 
 
 class Solution:
-    def levelOrder(self, root):
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
         soln = []
-        if root:
-            queue = [root]
-            new_queue = []
-            while queue:
-                # Copy current level to solution.
-                soln.append([node.val for node in queue if node is not None])
-                # BFS next level.
-                for node in queue:
-                    for child in node.children:
-                        new_queue.append(child)
-                queue, new_queue = new_queue, []
+
+        def traverse(node, level):
+            if node is None:
+                return
+
+            while len(soln) <= level:
+                soln.append([])
+
+            soln[level].append(node.val)
+            for child in node.children:
+                traverse(child, level+1)
+
+        traverse(root, 0)
         return soln
