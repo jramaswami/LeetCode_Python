271d69561b48f2966f461a6903d10e488d22fba9
Joseph Ramaswami <jramaswami@gmail.com>
2022-09-06 06:48:17 -0400

    AC. Resolved for September 2022 Challenge.

--- binary_tree_pruning.py
+++ binary_tree_pruning.py
@@ -1,21 +1,21 @@
 """
-LeetCode :: July 2021 Challenge :: Binary Tree Pruning
+LeetCode :: September 2022 Challenge :: 814. Binary Tree Pruning
 jramaswami
 """
 
 
 class Solution:
-    def pruneTree(self, root):
+    def pruneTree(self, root: TreeNode) -> TreeNode:
 
-        def prune(node):
+        def traverse(node):
             if node is None:
-                return None
+                return False, None
 
-            node.left = prune(node.left)
-            node.right = prune(node.right)
-            if node.left or node.right or node.val:
-                return node
-            else:
-                return None
+            left_result, left_node = traverse(node.left)
+            right_result, right_node = traverse(node.right)
 
-        return prune(root)
+            if node.val == 1 or left_result or right_result:
+                return True, TreeNode(node.val, left_node, right_node)
+            return False, None
+
+        return traverse(root)[1]
