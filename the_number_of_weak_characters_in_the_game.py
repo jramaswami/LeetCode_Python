5f015e7e04afc4b284b0af490e50808be9be63d8
Joseph Ramaswami <jramaswami@gmail.com>
2022-09-08 20:28:17 -0400

    WA

--- the_number_of_weak_characters_in_the_game.py
+++ the_number_of_weak_characters_in_the_game.py
@@ -5,6 +5,7 @@ jramaswami
 
 
 import math
+import collections
 from typing import *
 
 
@@ -12,17 +13,22 @@ class Solution:
     def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
         # [attack, defense]
         # Sort properties by attack in descending order.
-        properties.sort(reverse=True)
+        characters_by_attack = collections.defaultdict(list)
+        for a, d in properties:
+            characters_by_attack[a].append(d)
         max_defense = -math.inf
         soln = 0
-        for a, d in properties:
-            # Every character to my left (already seen) has an attack score higher than me.
-            # Is there one that also has a defense score higher than me?
-            if max_defense > d:
-                # If there is then I am a weak character.
-                soln += 1
-            # Keep track of the highest defense score we have seen.
-            max_defense = max(d, max_defense)
+        for a in sorted(characters_by_attack, reverse=True):
+            # Every character already seen has an attack score greater
+            # than this attack score.
+            # For each character with this attack score, is there a previous
+            # character with higher defense score too?
+            for d in characters_by_attack[a]:
+                if max_defense > d:
+                    # If there is then (a,d) is a weak character.
+                    soln += 1
+            # Keep track of the highest attack and defense scores we have seen.
+            max_defense = max(d, max(characters_by_attack[a]))
         return soln
 
 
@@ -48,4 +54,18 @@ def test_4():
     "WA"
     properties = [[1,1],[2,1],[2,2],[1,2]]
     expected = 1
+    assert Solution().numberOfWeakCharacters(properties) == expected
+
+
+def test_5():
+    "WA"
+    properties = [[7,9],[10,7],[6,9],[10,4],[7,5],[7,10]]
+    expected = 2
+    assert Solution().numberOfWeakCharacters(properties) == expected
+
+
+def test_6():
+    "WA"
+    properties = [[10,1],[5,1],[7,10],[4,1],[5,9],[6,9],[7,2],[1,10]]
+    expected = 4
     assert Solution().numberOfWeakCharacters(properties) == expected
