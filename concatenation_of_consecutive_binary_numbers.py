7c0f14bc7952a6455d00f81318c45d7b47418624
Joseph Ramaswami <jramaswami@gmail.com>
2022-09-23 12:26:55 -0400

    AC. Solved for September 2022 Challenge with Larry's help.

--- concatenation_of_consecutive_binary_numbers.py
+++ concatenation_of_consecutive_binary_numbers.py
@@ -1,39 +1,25 @@
 """
 LeetCode :: September 2022 Challenge :: 1680. Concatenation of Consecutive Binary Numbers
 jramaswami
+
+Thank You Larry!
 """
-
-
-import math
-import collections
-
-
-CacheItem = collections.namedtuple('CacheItem', ['value', 'bit_length'])
 
 
 class Solution:
 
-    def __init__(self):
-        # Cache results.
-        pows2 = [CacheItem(pow(2, i), i) for i in range(19)][::-1]
-        MOD = pow(10, 9) + 7
-        self.cache = [CacheItem(0, 0)]
-        limit = pow(10,5)
-        for x in range(1, limit+1):
-            while x >= pows2[-1].value:
-                pows2.pop()
-            # Take previous value and shift it by the current values
-            # number of pits.
-            p = pow(2, pows2[-1].bit_length, MOD)
-            t = (self.cache[-1].value * p) % MOD
-            # Add current value.
-            y = (x + t) % MOD
-            # Figure out how many bits we no have.
-            l = pows2[-1].bit_length + self.cache[-1].bit_length
-            self.cache.append(CacheItem(y, l))
-
     def concatenatedBinary(self, n: int) -> int:
-        return self.cache[n].value
+        MOD = pow(10, 9) + 7
+        soln = 0
+        shift = 0
+        for x in range(1, n+1):
+            if (x & (x-1)) == 0:
+                shift += 1
+            soln <<= shift
+            soln |= x
+            soln %= MOD
+        return soln
+
 
 
 def test_1():
