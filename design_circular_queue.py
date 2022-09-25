221bea72491368551d8f8ee984583e75853931fc
Joseph Ramaswami <jramaswami@gmail.com>
2022-09-25 16:14:25 -0400

    AC. Resubmitted for September 2022 Challenge.

--- design_circular_queue.py
+++ design_circular_queue.py
@@ -1,19 +1,22 @@
 """
-LeetCode :: April 2021 Challenge :: Design Circular Queue
+LeetCode :: September 2022 Challenge :: Design Circular Queue
+jramaswami
 """
+
+
 class MyCircularQueue:
 
     def __init__(self, k: int):
         self.capacity = k
         self.size = 0
         self.arr = [None for _ in range(k)]
-        self.front = 0 
+        self.front = 0
         self.back = k - 1
 
     def enQueue(self, value: int) -> bool:
         if self.size == self.capacity:
             return False
-        
+
         self.back = (self.back + 1) % self.capacity
         self.arr[self.back] = value
         self.size += 1
@@ -37,7 +40,7 @@ class MyCircularQueue:
 
     def isEmpty(self) -> bool:
         return self.size == 0
-        
+
     def isFull(self) -> bool:
         return self.size == self.capacity
 
@@ -52,7 +55,7 @@ def test_1():
     methods = ["MyCircularQueue", "enQueue", "enQueue", "enQueue", "enQueue", "Rear", "isFull", "deQueue", "enQueue", "Rear"]
     arguments = [[3], [1], [2], [3], [4], [], [], [], [4], []]
     expected = [None, True, True, True, False, 3, True, True, True, 4]
-    
+
     Q = MyCircularQueue(*arguments[0])
     for meth, args, exp in zip(methods[1:], arguments[1:], expected[1:]):
         result = getattr(Q, meth)(*args)
