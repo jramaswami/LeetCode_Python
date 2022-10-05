b99d2adfabeef14873410ece84059265e439df5e
Joseph Ramaswami <jramaswami@gmail.com>
2022-10-05 16:15:36 -0400

    AC. Resolved for October 2022 Challenge.

--- add_one_row_to_tree.py
+++ add_one_row_to_tree.py
@@ -1,29 +1,28 @@
 """
-LeetCode :: March 2021 Challenge :: Add One Row To Tree
+LeetCode :: September 2022 Challenge :: Add One Row To Tree
 jramaswami
 """
-def solve0(node, node_level, target_value, target_level):
-    """Recursive solution to the problem."""
-    if node is None:
-        return None
-
-    if node_level == target_level - 1:
-        left_node = node.left
-        right_node = node.right
-
-        new_left_node = TreeNode(target_value, left_node, None)
-        new_right_node = TreeNode(target_value, None, right_node)
-
-        return TreeNode(node.val, new_left_node, new_right_node)
-
-    return TreeNode(node.val, solve0(node.left, node_level + 1, target_value, target_level),
-                              solve0(node.right, node_level + 1, target_value, target_level))
 
 
 class Solution:
-    def addOneRow(self, root: TreeNode, v: int, d: int) -> TreeNode:
-        # Base case, insert at root.
-        if d == 1:
-            return TreeNode(v, root, None)
-        return solve0(root, 1, v, d)
+    def addOneRow(self, root, val, depth):
 
+        def rec(node, level):
+            if node is None:
+                return None
+
+            if level == depth - 1:
+                # Here is where we replace
+                new_left = TreeNode(val, node.left)
+                new_right = TreeNode(val, node.right)
+                node.left = new_left
+                node.right = new_right
+                return node
+            else:
+                node.left = rec(node.left, level + 1)
+                node.right = rec(node.right, level + 1)
+                return node
+
+        if depth == 1:
+            return TreeNode(val, root, None)
+        return rec(root, 1)
