4c317d1bc4507257984b6c7411ee3b99b9a165e5
Joseph Ramaswami <jramaswami@gmail.com>
2022-10-08 17:19:04 -0400

    TLE?

--- three_sum_closest.py
+++ three_sum_closest.py
@@ -1,11 +1,11 @@
 """
-LeetCode :: July 2021 Challenge :: 3Sum Closest
+LeetCode :: October 2022 Challenge :: 3Sum Closest
 jramaswami
 """
 
 
-import bisect
 import math
+import bisect
 
 
 class Solution:
@@ -18,11 +18,11 @@ class Solution:
                 k = bisect.bisect_right(nums, d)
                 if k > 0 and k - 1 != i and k - 1 != j:
                     t = a + b + nums[k-1]
-                    if abs(target - t) < abs(soln - target):
+                    if abs(target - t) < abs(target - soln):
                         soln = t
                 if k < len(nums) and k != i and k != j:
                     t = a + b + nums[k]
-                    if abs(target - t) < abs(soln - target):
+                    if abs(target - t) < abs(target - soln):
                         soln = t
         return soln
 
@@ -31,3 +31,9 @@ def test_1():
     nums = [-1,2,1,-4]
     target = 1
     assert Solution().threeSumClosest(nums, target) == 2
+
+
+def test_2():
+    nums = [0, 0, 0]
+    target = 0
+    assert Solution().threeSumClosest(nums, target) == 0
