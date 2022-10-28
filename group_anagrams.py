19ffa77e6c519365ccca1a2d6b02a2ba9ae57798
Joseph Ramaswami <jramaswami@gmail.com>
2022-10-28 08:04:46 -0400

    AC. Resolved for October 2022 Challenge.

--- group_anagrams.py
+++ group_anagrams.py
@@ -1,25 +1,45 @@
 """
-LeetCode :: August 2021 Challenge :: Group Anagrams
+LeetCode :: October 2022 Challenge :: Group Anagrams
 jramaswami
 """
 
 
-import collections
-
-
 class Solution:
     def groupAnagrams(self, strs):
-        # Easiest but not the fastest way.
-        groups = collections.defaultdict(list)
-        for s in strs:
-            k = "".join(sorted(s))
-            groups[k].append(s)
-        return list(groups.values())
+        words = [("".join(sorted(s)), s) for s in strs]
+        words.sort()
+        soln = []
+        curr_group = words[0][0]
+        curr_list = []
+        for an, wd in words:
+            if an == curr_group:
+                curr_list.append(wd)
+            else:
+                soln.append(curr_list)
+                curr_list = [wd]
+                curr_group = an
+        soln.append(curr_list)
+        return soln
 
+
+#
+# TESTING
+#
+
+
+def lists_same(A, B):
+    # Sort all internal lists.
+    for a in A:
+        a.sort()
+    for b in B:
+        b.sort()
+    for  a, b in zip(sorted(A), sorted(B)):
+        if a != b:
+            return False
+    return True
 
 def test_1():
     strs = ["eat","tea","tan","ate","nat","bat"]
     expected = [["bat"],["nat","tan"],["ate","eat","tea"]]
     result = Solution().groupAnagrams(strs)
-    print(result)
-    assert result == expected
+    assert lists_same(expected, result)
