8306704b46e2b256cdf315f7427457f68818787d
Joseph Ramaswami <jramaswami@gmail.com>
2022-11-14 21:33:05 -0500

    AC. Resolved in Python for November 2022 Challenge.

--- count_complete_tree_nodes.py
+++ count_complete_tree_nodes.py
@@ -1,53 +1,48 @@
 """
-LeetCode :: October 2021 Challenge :: 222. Count Complete Tree Nodes
+LeetCode :: 222. Count Complete Tree Nodes
+November 2022 Challenge
 jramaswami
-
-Given that a complete tree has up to 2h nodes.
-
 """
 
-
-import collections
-
-
 class Solution:
+
     def countNodes(self, root: Optional[TreeNode]) -> int:
-        # Corner case: null tree.
         if root is None:
             return 0
 
-        # Get the height of the tree.  Since the last level
-        # is filled from the left, just go left until there
-        # are no more nodes.  This is done in O(H) timer where
-        # H is the height of the tree.
+        if root.left is None:
+            return 1
+
+        if root.right is None:
+            return 2
+
+        ht = 0
         node = root
-        height = 0
         while node:
+            ht += 1
             node = node.left
-            height += 1
 
-        # Compute the total number of possible nodes.
-        node_count = pow(2, height) - 1
+        # There are ht - 1 links to the bottom.
+        # This translates into ht - 1 bits.
+        lo = 0
+        hi = pow(2, ht - 1) - 1
+        result = -1
+        while lo <= hi:
+            mid = lo + ((hi - lo) // 2)
+            node = root
+            for bit in range(ht - 2, -1, -1):
+                mask = 1 << bit
+                if mid & mask:
+                    node = node.right
+                else:
+                    node = node.left
+            if node is None:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+                result = max(result, mid)
 
-        # Use BFS to find the null nodes at the bottom.
-        # Do not process the bottom row.
-        queue = collections.deque()
-        queue.append((root, 1))
-        while queue:
-            node, ht = queue.popleft()
-
-            # Corner case: tree with height of one.
-            if ht == height:
-                break
-
-            if node.left is None:
-                node_count -= 1
-            if node.right is None:
-                node_count -= 1
-
-            if ht + 1 < height and node.left:
-                queue.append((node.left, ht + 1))
-            if ht + 1 < height and node.right:
-                queue.append((node.right, ht + 1))
-
-        return node_count
+        # There are result + 1 nodes on the bottom row.
+        bottom_nodes = result + 1
+        top_nodes = pow(2, ht - 1) - 1
+        return bottom_nodes + top_nodes
