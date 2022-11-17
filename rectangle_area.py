1952abe5eb48a6cc6164275b7f325c68e67b0c2b
Joseph Ramaswami <jramaswami@gmail.com>
2022-11-16 20:46:50 -0500

    AC. Solved in Python for November 2022 Challenge.

--- rectangle_area.py
+++ rectangle_area.py
@@ -6,6 +6,17 @@ jramaswami
 
 
 def intersection_length(left1, right1, left2, right2):
+    "Return length of intersection of two lines."
+    #######
+      ###
+    if left1 <= left2 <= right1 and left1 <= right2 <= right1:
+        return right2 - left2
+
+      ###
+    #######
+    if left2 <= left1 <= right2 and left2 <= right1 <= right2:
+        return right1 - left1
+
     ######
     ######
     if (left1 == left2 and right1 == right2):
@@ -21,16 +32,6 @@ def intersection_length(left1, right1, left2, right2):
     if left2 <= left1 <= right2:
         return right2 - left1
 
-    #######
-      ###
-    if left1 <= left2 <= right1 and left1 <= right2 <= right1:
-        return right2 - left2
-
-      ###
-    #######
-    if left2 <= leftl <= right2 and left2 <= right1 <= right2:
-        return right1 - left1
-
     # Not overlapping
     return 0
 
@@ -39,6 +40,7 @@ class Solution:
     def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
         rect1 = (ax2 - ax1) * (ay2 - ay1)
         rect2 = (bx2 - bx1) * (by2 - by1)
+        print(intersection_length(ax1, ax2, bx1, bx2), intersection_length(ay1, ay2, by1, by2))
         intersect = intersection_length(ax1, ax2, bx1, bx2) * intersection_length(ay1, ay2, by1, by2)
         return rect1 + rect2 - intersect
 
