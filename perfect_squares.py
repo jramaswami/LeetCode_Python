2c4589478861d10650d2717779c03132da60a212
Joseph Ramaswami <jramaswami@gmail.com>
2022-11-21 21:15:46 -0500

    TLE

--- perfect_squares.py
+++ perfect_squares.py
@@ -3,24 +3,30 @@ LeetCode :: October 2021 Challenge :: 279. Perfect Squares
 jramaswami
 """
 
+import math
+
 
 class Solution:
     def numSquares(self, n):
-        queue = [0]
-        new_queue = []
-        soln = 1
-        while True:
-            for j in queue:
-                for k in range(1, n+1):
-                    m = j + (k * k)
-                    if m > n:
-                        break
+        # DP[sum] = minimum number of squares required
+        perfect_sums = [math.inf for _ in range(n+1)]
 
-                    if m == n:
-                        return soln
-                    new_queue.append(m)
-            queue, new_queue = new_queue, []
-            soln += 1
+        # Generate perfect squares
+        k = 1
+        perfect_squares = []
+        while k * k <= n:
+            perfect_squares.append(k * k)
+            perfect_sums[k * k] = 1
+            k += 1
+
+        # Perform dp
+        for i in range(1, n+1):
+            if perfect_sums[i] < math.inf:
+                for k in perfect_squares:
+                    if k + i > n:
+                        break
+                    perfect_sums[i + k] = min(perfect_sums[i + k], perfect_sums[i] + 1)
+        return perfect_sums[n]
 
 
 def test_1():
@@ -44,3 +50,9 @@ def test_4():
 def test_5():
     """TLE"""
     assert Solution().numSquares(1) == 1
+
+
+def test_6():
+    "TLE"
+    n = 3258
+    assert Solution().numSquares(n) == 2
