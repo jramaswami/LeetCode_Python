92172bc507f33da59546c76c1a2144b857ed2052
Joseph Ramaswami <jramaswami@gmail.com>
2022-12-01 21:00:41 -0500

    WA

---
+++ determine_if_two_strings_are_close.py
@@ -0,0 +1,45 @@
+"""
+LeetCode :: 1657. Determine if Two Strings Are Close
+December 2022 Challenge
+jramaswami
+"""
+
+
+import collections
+
+
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        freqs1 = collections.Counter(word1)
+        freqs2 = collections.Counter(word2)
+        return set(freqs1.values()) == set(freqs2.values())
+
+
+
+def test_1():
+    word1 = "abc"
+    word2 = "bca"
+    expected = True
+    assert Solution().closeStrings(word1, word2) == expected
+
+
+def test_2():
+    word1 = "a"
+    word2 = "aa"
+    expected = False
+    assert Solution().closeStrings(word1, word2) == expected
+
+
+def test_3():
+    word1 = "cabbba"
+    word2 = "abbccc"
+    expected = True
+    assert Solution().closeStrings(word1, word2) == expected
+
+
+def test_4():
+    "WA"
+    word1 = "uau"
+    word2 = "ssx"
+    expected = False
+    assert Solution().closeStrings(word1, word2) == expected
