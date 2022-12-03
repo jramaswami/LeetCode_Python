0e9d652f0cdec2440f5a786e9f7d29f49b736774
Joseph Ramaswami <jramaswami@gmail.com>
2022-12-03 10:42:08 -0500

    AC. Resubmitted Python solution for December 2022 Challenge.

--- sort_characters_by_frequency.py
+++ sort_characters_by_frequency.py
@@ -1,5 +1,7 @@
 """
-LeetCode :: October 2021 Challenge :: 451. Sort Characters By Frequency
+LeetCode
+451. Sort Characters By Frequency
+December 2022 Challenge
 jramaswami
 """
 
@@ -9,5 +11,24 @@ import collections
 
 class Solution:
     def frequencySort(self, s: str) -> str:
-        ctr = Counter(s)
+        ctr = collections.Counter(s)
         return "".join(c * f for c, f in sorted(ctr.items(), key=lambda t: (-t[1], t[0])))
+
+
+
+def test_1():
+    s = "tree"
+    expected = "eert"
+    assert Solution().frequencySort(s) == expected
+
+
+def test_2():
+    s = "cccaaa"
+    expected = "aaaccc"
+    assert Solution().frequencySort(s) == expected
+
+
+def test_3():
+    s = "Aabb"
+    expected = "bbAa"
+    assert Solution().frequencySort(s) == expected
