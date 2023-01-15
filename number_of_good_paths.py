d88d88ce6451779f6dbf6702bdbd8b88a885b93e
Joseph Ramaswami <jramaswami@gmail.com>
2023-01-15 15:02:01 -0500

    Solved (with help from Larry) in Python for January 2023 Challenge.

--- number_of_good_paths.py
+++ number_of_good_paths.py
@@ -10,6 +10,30 @@ from typing import *
 import collections
 
 
+class UnionFind:
+
+    def __init__(self, N):
+        self.parent = list(range(N))
+        self.size = [1 for _ in self.parent]
+
+    def find(self, u):
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+
+    def union(self, a, b):
+        a = self.find(a)
+        b = self.find(b)
+        if a != b:
+            if self.size[a] < self.size[b]:
+                a, b = b, a
+            self.parent[b] = a
+            self.size[a] += self.size[b]
+
+    def connected(self, a, b):
+        return self.find(a) == self.find(b)
+
+
 class Solution:
     def numberOfGoodPaths(self, vals: List[int], edges: List[List[int]]) -> int:
 
@@ -19,16 +43,31 @@ class Solution:
             graph[u].append(v)
             graph[v].append(u)
 
-        soln = 0
-        for root, max_val in enumerate(vals):
-            queue = collections.deque([(root, -1)])
-            while queue:
-                u, p = queue.popleft()
-                if vals[u] == vals[root] and u <= root:
-                    soln += 1
+        nodes_by_value = collections.defaultdict(list)
+        for u, val in enumerate(vals):
+            nodes_by_value[val].append(u)
+
+        soln = len(vals)
+        uf = UnionFind(len(vals))
+
+        # Process nodes in order by value
+        for val in sorted(nodes_by_value):
+            # Process all the nodes at this value.
+            for u in nodes_by_value[val]:
+                # Connect u to any of its adjacent nodes if they have a value
+                # less than or equal to the value of u.
                 for v in graph[u]:
-                    if v != p and vals[v] <= max_val:
-                        queue.append((v, u))
+                    if vals[v] <= val:
+                        uf.union(u, v)
+
+            # For each node of this value, how many other nodes *of this value*
+            # can the current node connect to.
+            count_by_component = collections.defaultdict(int)
+            for u in nodes_by_value[val]:
+                p = uf.find(u)
+                soln += count_by_component[p]
+                count_by_component[p] += 1
+
         return soln
 
 
