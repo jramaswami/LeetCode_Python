68b31b2e6e69735409275fbe52dca60fd1341367
Joseph Ramaswami <jramaswami@gmail.com>
2023-01-16 07:50:15 -0500

    AC. Solved in Python for January 2023 Challenge.

--- insert_interval.py
+++ insert_interval.py
@@ -26,26 +26,34 @@ class Solution:
         insertedInterval = newInterval
         for x in intervals:
             if state == BEFORE:
-                if overlaps(x, newInterval):
+                if overlaps(x, insertedInterval):
                     # When we start overlapping, discard x and change state.
                     state = DURING
                     # Update interval to insert.
                     insertedInterval = merge(x, insertedInterval)
+                elif insertedInterval[1] < x[0]:
+                    # When we have cleared the inserted interval.
+                    soln.append(insertedInterval)
+                    soln.append(x)
+                    state = AFTER
                 else:
                     soln.append(x)
             elif state == DURING:
-                if not overlaps(x, newInterval):
-                    # When we stop overlapping, insert interval,
-                    # add x and change state.
+                if not overlaps(x, insertedInterval) or insertedInterval[1] < x[0]:
+                    # When we stop overlapping or cleared inserted interval,
+                    # insert interval, add x, and change state.
                     soln.append(insertedInterval)
                     soln.append(x)
-                    state == AFTER
+                    state = AFTER
                 else:
                     # Update interval to insert.
                     insertedInterval = merge(x, insertedInterval)
             else:
                 soln.append(x)
 
+        if state != AFTER:
+            # If we haven't inserted interval yet, do so now.
+            soln.append(insertedInterval)
         return soln
 
 
@@ -68,4 +76,25 @@ def test_3():
     intervals = []
     newInterval = [4,8]
     expected = [[4,8]]
+    assert Solution().insert(intervals, newInterval) == expected
+
+
+def test_4():
+    intervals = [[1,3],[6,9]]
+    newInterval = [10,11]
+    expected = [[1,3],[6,9], [10,11]]
+    assert Solution().insert(intervals, newInterval) == expected
+
+
+def test_5():
+    intervals = [[1,3],[6,9]]
+    newInterval = [0, 0]
+    expected = [[0,0],[1,3],[6,9]]
+    assert Solution().insert(intervals, newInterval) == expected
+
+
+def test_6():
+    intervals = [[1,3],[6,9]]
+    newInterval = [2, 2]
+    expected = [[1,3],[6,9]]
     assert Solution().insert(intervals, newInterval) == expected
