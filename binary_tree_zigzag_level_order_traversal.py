eb1b8142f33774d9317669cb2c1d898af34da1a9
Joseph Ramaswami <jramaswami@gmail.com>
2023-02-19 10:07:33 -0500

    AC. Different solution in Python for Feburary 2023 Challenge.

--- binary_tree_zigzag_level_order_traversal.py
+++ binary_tree_zigzag_level_order_traversal.py
@@ -11,19 +11,22 @@ from typing import *
 
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        soln = []
-        def inorder(node, level):
-            if node is None:
-                return
-            while len(soln) <= level:
-                soln.append([])
-
-            soln[level].append(node.val)
-            inorder(node.left, level+1)
-            inorder(node.right, level+1)
-
-        inorder(root, 0)
-        for i, _ in enumerate(soln):
-            if i % 2:
-                soln[i] = soln[i][::-1]
-        return soln
+        soln = [[root] if root else []]
+        level = 0
+        while soln[-1]:
+            T = []
+            for node in reversed(soln[-1]):
+                if level % 2:
+                    if node.left:
+                        T.append(node.left)
+                    if node.right:
+                        T.append(node.right)
+                else:
+                    if node.right:
+                        T.append(node.right)
+                    if node.left:
+                        T.append(node.left)
+            soln.append(T)
+            level += 1
+        soln.pop()
+        return [[node.val for node in row] for row in soln]
