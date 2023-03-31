bd117d2c765f5a71fe873f6afd16e1313d1c7a65
Joseph Ramaswami <jramaswami@gmail.com>
2023-03-31 18:16:59 -0400

    AC. Solved in Python with a little help from Larry for the March 2023 Challenge.

--- number_of_ways_of_cutting_a_pizza.py
+++ number_of_ways_of_cutting_a_pizza.py
@@ -8,6 +8,7 @@ Thank You Larry!
 """
 
 
+import functools
 from typing import *
 
 
@@ -15,56 +16,51 @@ class Solution:
     def ways(self, pizza: List[str], k: int) -> int:
         MOD = pow(10, 9) + 7
         soln = 0
+        R = len(pizza)
+        C = len(pizza[0])
 
-        mat_sum = [[0 for _ in row] for row in pizza]
-        for c, _ in enumerate(pizza[0]):
-            row_sum = 0
-            for r, _ in enumerate(pizza):
-                if pizza[r][c] == 'A':
-                    row_sum += 1
-                col_sum = mat_sum[r][c-1] if c > 0 else 0
-                mat_sum[r][c] = col_sum + row_sum
+        @functools.cache
+        def has_apple(r1, r2, c1, c2):
+            for r in range(r1, r2+1):
+                for c in range(c1, c2+1):
+                    if pizza[r][c] == 'A':
+                        return True
+            return False
 
-        def get_mat_sum_value(r, c):
-            if r < 0 or c < 0:
-                return 0
-            return mat_sum[r][c]
-
-        def get_mat_sum(r1, c1, r2, c2):
-            A = get_mat_sum_value(r2, c2)
-            B = get_mat_sum_value(r1-1, c2)
-            C = get_mat_sum_value(r2, c1-1)
-            D = get_mat_sum_value(r1-1, c1-1)
-            return A - B - C + D
-
-        def has_apple(r1, c1, r2, c2):
-            print(f'has_apple({r1}, {c1}, {r2}, {c2}) =', get_mat_sum(r1, c1, r2, c2) > 0)
-            return get_mat_sum(r1, c1, r2, c2) > 0
-
-        def rec(r1, c1, r2, c2, cuts_left):
-            print(f'rec', r1, c1, r2, c2, cuts_left)
+        @functools.cache
+        def rec(r, c, cuts_left):
+            "(r, c) is upper left of remaining piece; x is number of cuts left"
             if cuts_left == 0:
-                return 1
+                if has_apple(r, R-1, c, C-1):
+                    return 1
+                return 0
 
             result = 0
-            for c_cut in range(c1, c2-1):
-                if has_apple(r1, c1, r2, c_cut) and has_apple(r1, c_cut+1, r2, c2):
-                    result += rec(r1, c_cut+1, r2, c2, cuts_left - 1)
-                    result %= MOD
-            for r_cut in range(r1, r2-1):
-                if has_apple(r1, c1, r_cut, c2) and has_apple(r_cut+1, c1, r2, c2):
-                    result += rec(r_cut+1, c1, r2, c2, cuts_left - 1)
+            for r_cut in range(r, R-1):
+                if has_apple(r, r_cut, c, C-1) and has_apple(r_cut+1, R-1, c, C-1):
+                    result += rec(r_cut+1, c, cuts_left-1)
                     result %= MOD
 
-            return result %MOD
+            for c_cut in range(c, C-1):
+                if has_apple(r, R-1, c, c_cut) and has_apple(r, R-1, c_cut+1, C-1):
+                    result += rec(r, c_cut+1, cuts_left-1)
+                    result %= MOD
+            return result % MOD
 
-        soln = rec(0, 0, len(pizza)-1, len(pizza[-1])-1, k-1)
-        return soln % MOD
+        return rec(0, 0, k-1) % MOD
 
 
 
 def test_1():
     pizza = ["A..","AAA","..."]
     k = 3
+    expected = 3
+    assert Solution().ways(pizza, k) == expected
+
+
+def test_2():
+    pizza = ["A" * 50 for _ in range(49)]
+    print(pizza)
+    k = 10
     expected = 3
     assert Solution().ways(pizza, k) == expected
