447d89d3958b7111615d9c73dc894fb1eb87e0ec
Joseph Ramaswami <jramaswami@gmail.com>
2023-04-09 16:59:15 -0400

    AC. Solved in Python for April 2023 Challenge with help from Larry.

--- largest_color_value_in_a_directed_graph.py
+++ largest_color_value_in_a_directed_graph.py
@@ -3,6 +3,8 @@ LeetCode
 1857. Largest Color Value in a Directed Graph
 April 2023 Challenge
 jramaswami
+
+Thank You Larry!
 """
 
 
@@ -12,46 +14,58 @@ import collections
 
 class Solution:
     def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
-        in_degree = [0 for _ in colors]
-        graph = [[] for _ in colors]
+        N = len(colors)
+
+        adj = collections.defaultdict(list)
         for u, v in edges:
-            in_degree[v] += 1
-            graph[u].append(v)
+            adj[u].append(v)
 
-        soln = collections.Counter()
+        # I graph acyclic?
+        WHITE, GRAY, BLACK = 0, 1, 2
+        visited = [WHITE] * N
 
-        def dfs(u, visited, path_colors):
-            # Found a cycle.
-            if visited[u]:
-                return True
-            u_color = colors[u]
-            path_colors[u_color] += 1
-            soln[u_color] = max(soln[u_color], path_colors[u_color])
-            visited[u] = True
-            for v in graph[u]:
-                if dfs(v, visited, path_colors):
+        def has_cycle(u):
+            visited[u] = GRAY
+            for v in adj[u]:
+                if visited[v] == GRAY:
                     return True
-            path_colors[u_color] -= 1
-            visited[u] = False
+                if visited[v] == WHITE and has_cycle(v):
+                    return True
+            visited[u] = BLACK
             return False
 
-        # Start with nodes that have an in_degree of zero.
-        # If there aren't any there is a cycle.
-        if all(d > 0 for d in in_degree):
-            return -1
+        for root in range(N):
+            if visited[root] == WHITE and has_cycle(root):
+                return -1
 
-        for root, _ in enumerate(colors):
-            if in_degree[root] == 0:
-                # Root node for dfs.
-                path_colors = collections.Counter()
-                visited = [False for _ in colors]
-                cycle_exists = dfs(root, visited, path_colors)
-                if cycle_exists:
-                    return -1
+        def compute(color):
+            cache = [None] * N
+            has_cache = [False] * N
 
-        return max(soln.values())
+            def get_longest(u):
+                if has_cache[u]:
+                    return cache[u]
 
+                current = 0
+                if colors[u] == color:
+                    current = 1
+                result = current
+                for v in adj[u]:
+                    result = max(result, current + get_longest(v))
+                has_cache[u] = True
+                cache[u] = result
+                return cache[u]
 
+            result = 0
+            for u in range(N):
+                result = max(result, get_longest(u))
+            return result
+
+        # Acyclic graph!
+        soln = 0
+        for c in set(colors):
+            soln = max(soln, compute(c))
+        return soln
 
 
 def test_1():
