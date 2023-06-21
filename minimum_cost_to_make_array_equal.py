098610d9c3d3e100efd38a74615dfa106b7bba68
Joseph Ramaswami <jramaswami@gmail.com>
2023-06-20 20:51:42 -0400

    WA

---
+++ minimum_cost_to_make_array_equal.py
@@ -0,0 +1,51 @@
+"""
+LeetCode
+2448. Minimum Cost to Make Array Equal
+June 2023 Challenge
+jramaswami
+"""
+
+from typing import List
+
+
+class Solution:
+    def minCost(self, nums: List[int], cost: List[int]) -> int:
+        def compute_cost(x):
+            return sum(abs(n - x) * c for n, c in zip(nums, cost))
+
+        def ternary_search(f, left, right, eps):
+            while abs(right - left) >= eps:
+                left_third = left  + (right - left) / 3
+                right_third = right - (right - left) / 3
+                print(f"{left=} {left_third=} {f(left_third)=}, {right=} {right_third=} {f(right_third)=}")
+                if f(left_third) > f(right_third):
+                    left = left_third
+                else:
+                    right = right_third
+
+            return (left + right) / 2
+
+        best_x = int(ternary_search(compute_cost, min(nums), max(nums), 0.00000001))
+        return compute_cost(best_x)
+
+
+def test_1():
+    nums = [1,3,5,2]
+    cost = [2,3,1,14]
+    expected = 8
+    assert Solution().minCost(nums, cost) == expected
+
+
+def test_2():
+    nums = [2,2,2,2,2]
+    cost = [4,2,8,1,3]
+    expected = 0
+    assert Solution().minCost(nums, cost) == expected
+
+
+def test_3():
+    "WA"
+    nums = [576257,268115,512826,523563,927189,39253,720661,35147,552624,847824,354489,760949,734966,571013]
+    cost = [842872,273313,503060,139143,367612,217125,271272,407727,199063,120280,819193,935689,624116,453146]
+    expected = 1122145265809
+    assert Solution().minCost(nums, cost) == expected
