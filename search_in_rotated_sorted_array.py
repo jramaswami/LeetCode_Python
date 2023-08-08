faff3b9b3e2346418bbaf4738f3edfffd747f134
Joseph Ramaswami <jramaswami@gmail.com>
2023-08-08 13:55:45 -0400

    AC. Solved in Python for August 2023 Challenge.

--- search_in_rotated_sorted_array.py
+++ search_in_rotated_sorted_array.py
@@ -9,34 +9,91 @@ jramaswami
 from typing import *
 
 
+def find_pivot(nums: List[int]) -> int:
+    "Return the index of the pivot or len(nums) if there is no pivot."
+    # Index of left most item less than nums[0]
+    lo = 1
+    hi = len(nums) - 1
+    pivot = len(nums)
+    while lo <= hi:
+        mid = lo + ((hi - lo) // 2)
+        if nums[mid] <= nums[0]:
+            # Go left
+            hi = mid - 1
+            pivot = min(pivot, mid)
+        else:
+            # Go right
+            lo = mid + 1
+    return pivot
+
+
+def binsearch(nums, target, lo, hi):
+    while lo <= hi:
+        mid = lo + ((hi - lo) // 2)
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            lo = mid + 1
+        else:
+            hi = mid - 1
+    return -1
+
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        lo = 0
-        hi = len(nums) - 1
-        while lo <= hi:
-            mid = lo + ((hi - lo) // 2)
-            print(f"{lo=} {nums[lo]=} {hi=} {nums[hi]=} {mid=} {nums[mid]=} {target=}")
-            if nums[mid] == target:
-                return mid
-            elif target < nums[mid] and target < nums[lo]:
-                # If target is less than both nums[mid] and nums[lo] then it is
-                # to the right of mid.
-                lo = mid + 1
-            elif target > nums[mid] and target > nums[hi]:
-                # If target is greater than both nums[mid] and nums[hi] then it
-                # is to the left of mid.
-                hi = mid - 1
-            elif nums[lo] <= target < nums[mid]:
-                # If the target is between nums[lo] and nums[mid] then target
-                # is to the left of mid
-                hi = mid - 1
-            elif nums[mid] < target <= nums[hi]:
-                # If the target is between nums[mid] and nums[hi] then the target
-                # is to the right of mid.
-                lo = mid + 1
-            else:
-                break
-        return -1
+        p = find_pivot(nums)
+        if p == len(nums):
+            # Just a regular binary search.
+            return binsearch(nums, target, 0, len(nums)-1)
+        if target < nums[0]:
+            # Search the right half.
+            return binsearch(nums, target, p, len(nums)-1)
+        return binsearch(nums, target, 0, p-1)
+
+
+
+import collections
+
+
+def test_find_pivot_1():
+    nums = [4,5,6,7,0,1,2]
+    expected = 4
+    assert find_pivot(nums) == expected
+
+
+def test_find_pivot_2():
+    nums = [1]
+    expected = 1
+    assert find_pivot(nums) == expected
+
+
+def test_find_pivot_3():
+    nums = [1,2,3,4,5,6,7,8,9]
+    expected = len(nums)
+    assert find_pivot(nums) == expected
+
+
+def test_find_pivot_4():
+    nums = [2,3,4,5,6,7,8,1]
+    expected = len(nums) - 1
+    assert find_pivot(nums) == expected
+
+
+def test_find_pivot_5():
+    nums = [9,1,2,3,4,5,6,7,8]
+    expected = 1
+    assert find_pivot(nums) == expected
+
+
+def test_find_pivot_bf():
+    nums = collections.deque(range(20))
+    for r in range(20):
+        nums.rotate(-r)
+        nums0 = list(nums)
+        _, expected = min(((t, i) for i, t in enumerate(nums0)))
+        result = find_pivot(nums0)
+        print(nums0, expected, result)
+        assert result % len(nums) == expected
 
 
 def test_1():
