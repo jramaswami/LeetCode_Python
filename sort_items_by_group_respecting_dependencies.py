735eca7e110318722b885f0951d996ad4231463b
Joseph Ramaswami <jramaswami@gmail.com>
2023-08-20 12:34:42 -0400

    WA

--- sort_items_by_group_respecting_dependencies.py
+++ sort_items_by_group_respecting_dependencies.py
@@ -12,83 +12,83 @@ from typing import List
 
 class Solution:
     def sortItems(self, itemCount: int, groupCount: int, group: List[int], beforeItems: List[List[int]]) -> List[int]:
+
+        def khans_algorith(graph):
+            "Return the topological sort of the given graph."
+            # Compute indegree and number of edges
+            edgecount = 0
+            indegree = {i: 0 for i in graph}
+            for u in graph:
+                for v in graph[u]:
+                    indegree[v] += 1
+                    edgecount += 1
+
+            queue = collections.deque((u for u, d in indegree.items() if d == 0))
+            toposort = []
+            while queue:
+                u = queue.popleft()
+                toposort.append(u)
+                for v in graph[u]:
+                    # Remove edge
+                    edgecount -= 1
+                    indegree[v] -= 1
+                    if indegree[v] == 0:
+                        queue.append(v)
+            if edgecount == 0:
+                return toposort
+            return []
+
         # Assign each of the -1 groups a unique group number
         for i, g in enumerate(group):
             if g == -1:
                 group[i] = groupCount
                 groupCount += 1
 
+        # Create graph of group dependencies
+        group_graph = {i: [] for i in range(groupCount)}
+        for item in range(itemCount):
+            item_group = group[item]
+            for before_item in beforeItems[item]:
+                before_item_group = group[before_item]
+                if item_group != before_item_group:
+                    # before_item_group must come before item_group
+                    group_graph[before_item_group].append(item_group)
+
         # Topologically sort by group
-        # Get dependencies between groups
-        groupIndegree = [0 for _ in range(groupCount)]
-        groupEdges = [set() for _ in range(groupCount)]
-        groupEdgeCount = 0
-        for i, bis in enumerate(beforeItems):
-            iGroup = group[i]
-            for bi in bis:
-                biGroup = group[bi]
-                print(f'{i=} {group[i]=} {bi=} {group[bi]=}')
-                if iGroup != biGroup:
-                    # biGroup must come before iGroup
-                    groupIndegree[iGroup] += 1
-                    groupEdges[biGroup].add(iGroup)
-                    groupEdgeCount += 1
-        print(f"{groupIndegree=}\n{groupEdges=}\n{groupEdgeCount=}")
-        # Kahn's algorithm
-        queue = collections.deque([i for i, d in enumerate(groupIndegree) if d == 0])
-        groupTopo = []
-        while queue:
-            u = queue.popleft()
-            groupTopo.append(u)
-            for v in groupEdges[u]:
-                groupIndegree[v] -= 1
-                groupEdgeCount -= 1
-                if groupIndegree[v] == 0:
-                    queue.append(v)
-        if groupEdgeCount:
-            print('Unable to topo sort groups')
+        group_toposort = khans_algorith(group_graph)
+        if not group_toposort:
             return []
+        print('group toposort=', group_toposort)
 
-        print(f'{groupTopo=}')
+        # Place each item in its group
+        groups = [[] for _ in range(groupCount)]
+        for item, group_index in enumerate(group):
+            groups[group_index].append(item)
 
+        print('group', group)
+        print('groups', groups)
+        # Topologically sort each group
         soln = []
-        # Toplogically sort each group
-        # Determine groups membership and indegree
-        groupItems = [[] for _ in range(groupCount)]
-        for i, g in enumerate(group):
-            groupItems[g].append(i)
+        for group_index in group_toposort:
+            # Create graph of intergroup dependencies
+            item_graph = {i: [] for i in groups[group_index]}
+            for item in groups[group_index]:
+                for before_item in beforeItems[item]:
+                    before_item_group = group[before_item]
+                    if before_item_group == group_index:
+                        # before_item must come before item
+                        item_graph[before_item].append(item)
 
-        # Get intergroup indegree
-        itemIndegree = [0 for _ in group]
-        itemEdges = [[] for _ in group]
-        groupEdgeCount = [0 for _ in range(groupCount)]
-        for i, bis in enumerate(beforeItems):
-            for bi in bis:
-                print(f'{i=} {group[i]=} {bi=} {group[bi]=}')
-                if group[i] == group[bi]:
-                    itemIndegree[i] += 1
-                    itemEdges[bi].append(i)
-                    groupEdgeCount[group[bi]] += 1
-
-        for gi in groupTopo:
-            print('topo sort of', gi, groupItems[gi])
-            print('edges', [(i, itemEdges[i]) for i in groupItems[gi]])
-            print('indegree', [(i, itemIndegree[i]) for i in groupItems[gi]])
-            queue = collections.deque([i for i in groupItems[gi] if itemIndegree[i] == 0])
-            print('queue', queue)
-            while queue:
-                u = queue.popleft()
-                soln.append(u)
-                for v in itemEdges[u]:
-                    itemIndegree[v] -= 1
-                    groupEdgeCount[gi] -= 1
-                    if itemIndegree[v] == 0:
-                        queue.append(v)
-            if groupEdgeCount[gi]:
-                print('Unable to topo sort group', gi)
+            print('item graph for group', group_index, item_graph)
+            item_topo = khans_algorith(item_graph)
+            if item_topo == []:
                 return []
-        print(soln)
+            print('item topo for group', group_index, item_topo)
+            soln.extend(item_topo)
+        print('soln', soln)
         return soln
+
+
 
 
 
@@ -111,6 +111,7 @@ def test_1():
     expected = [6,3,4,1,5,2,0,7]
     result = Solution().sortItems(n, m, group, beforeItems)
     # assert result == expected
+    assert result != []
     assert check(result, beforeItems)
 
 
@@ -134,5 +135,18 @@ def test_3():
     assert check(expected, beforeItems)
     print(f'groups of solution:', [group[i] for i in expected])
     result = Solution().sortItems(n, m, group, beforeItems)
+    assert result != []
+    assert check(result, beforeItems)
+
+
+def test_4():
+    "WA"
+    n = 5
+    m = 5
+    group = [2,0,-1,3,0]
+    beforeItems = [[2,1,3],[2,4],[],[],[]]
+    result = Solution().sortItems(n, m, group, beforeItems)
+    expected = [3,2,4,1,0]
+    assert check(expected, beforeItems)
     assert result != []
     assert check(result, beforeItems)
