6d4c1983f1bd459c3d1b1a02130ae6592fe588c5
Joseph Ramaswami <jramaswami@gmail.com>
2023-11-04 13:14:54 -0400

    AC. Solved in Python for November 2023 Challenge.

--- last_moment_before_all_ants_fall_out_of_a_plank.py
+++ last_moment_before_all_ants_fall_out_of_a_plank.py
@@ -3,13 +3,23 @@ LeetCode
 1503. Last Moment Before All Ants Fall Out of a Plank
 November 2023 Challenge
 jramaswami
+
+Key insight: the ants striking each other and turning is equivalent to the
+ants just continuing in the same direction.
 """
 
 
+import math
 from typing import List
-import itertools
 
 
 class Solution:
     def getLastMoment(self, n: int, left: List[int], right: List[int]) -> int:
-        return max(itertools.chain(left, right))
+        soln = -math.inf
+        for i in left:
+            # Going towards zero, so distance = i
+            soln = max(soln, i)
+        for i in right:
+            # Goin towards n so distance is n - i
+            soln = max(soln, n - i)
+        return soln
