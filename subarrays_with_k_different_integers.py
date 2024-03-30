996d3241dc38013f0b246f41b5d8e80de9d89c1a
Joseph Ramaswami <jramaswami@gmail.com>
2024-03-30 11:47:28 -0400

    AC. Solved in Python for March 2024 Challenge.

--- subarrays_with_k_different_integers.py
+++ subarrays_with_k_different_integers.py
@@ -12,30 +12,25 @@ from typing import List
 
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        window = collections.deque()
+        window_freqs = collections.defaultdict(int)
         soln = 0
-        for length in range(1, len(nums) + 1):
-            # Initialize window
-            window = collections.deque()
-            window_freqs = collections.defaultdict(int)
-            i = 0
-            while len(window) < length:
-                n = nums[i]
-                window.append(n)
-                window_freqs[n] += 1
-                i += 1
-            if len(window_freqs) == k:
-                soln += 1
-            while i < len(nums):
+        for n in nums:
+            window.append(n)
+            window_freqs[n] += 1
+            while window and len(window_freqs) > k:
                 m = window.popleft()
                 window_freqs[m] -= 1
-                n = nums[i]
-                window.append(n)
-                window_freqs[n] += 1
-                if n != m and window_freqs[m] == 0:
+                if window_freqs[m] == 0:
                     del window_freqs[m]
-                if len(window_freqs) == k:
+            if len(window_freqs) == k:
+                window_freqs0 = collections.defaultdict(int)
+                for w in window:
                     soln += 1
-                i += 1
+                    window_freqs0[w] += 1
+                    if window_freqs[w] == window_freqs0[w]:
+                        break
+
         return soln
 
 
@@ -50,11 +45,4 @@ def test_2():
     nums = [1,2,1,3,4]
     k = 3
     expected = 3
-    assert Solution().subarraysWithKDistinct(nums, k) == expected
-
-
-def test_large():
-    nums = [1 for _ in range(2 * pow(10,4))]
-    k = 1
-    expected = len(nums)
     assert Solution().subarraysWithKDistinct(nums, k) == expected
