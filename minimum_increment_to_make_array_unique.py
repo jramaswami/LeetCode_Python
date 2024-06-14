c98471bdec58f7c7311fa3312794ea97d0d7afa6
Joseph Ramaswami <jramaswami@gmail.com>
2024-06-13 21:09:10 -0400

    AC. Solved in Python for June 2024 Challenge.

--- minimum_increment_to_make_array_unique.py
+++ minimum_increment_to_make_array_unique.py
@@ -5,20 +5,15 @@ jramaswami
 """
 
 
-import heapq
-from typing import List
-
-
 class Solution:
     def minIncrementForUnique(self, nums: List[int]) -> int:
+        nums.sort()
+        stack = [nums[0]]
         soln = 0
-        heapq.heapify(nums)
-        stack = [heapq.heappop(nums)]
-        while nums:
-            x = heapq.heappop(nums)
-            if x == stack[-1]:
-                heapq.heappush(nums, x + 1)
-                soln += 1
+        for n in nums[1:]:
+            if n <= stack[-1]:
+                soln += (stack[-1] + 1 - n)
+                stack.append(stack[-1]+1)
             else:
-                stack.append(x)
-        return soln 
+                stack.append(n)
+        return soln
