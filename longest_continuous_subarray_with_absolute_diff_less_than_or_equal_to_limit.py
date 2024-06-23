714e1d253b30b42bccc4761e8d21fc3643c6be01
Joseph Ramaswami <jramaswami@gmail.com>
2024-06-23 06:15:10 -0400

    AC. Solved in Python for June 2024 Challenge.

--- longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit.py
+++ longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit.py
@@ -6,16 +6,34 @@ jramaswami
 """
 
 
+import collections
+import heapq
+
+
+QItem = collections.namedtuple('QItem', ['value', 'index'])
+
+
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        INF = pow(10, 10)
         soln = 0
-        for i, _ in enumerate(nums):
-            curr_min = INF
-            curr_max = -INF
-            for j, n in enumerate(nums[i:], start=i):
-                curr_min = min(curr_min, n)
-                curr_max = max(curr_max, n)
-                if curr_max - curr_min <= limit:
-                    soln = max(soln, j - i + 1)
+        min_queue = []
+        max_queue = []
+        window = collections.deque()
+        for i, n in enumerate(nums):
+            # Add current item to window and min/max queues
+            window.append(QItem(n, i))
+            heapq.heappush(min_queue, QItem(n, i))
+            heapq.heappush(max_queue, QItem(-n, i))
+
+            # While the window has items that are two far from each other
+            while (-max_queue[0].value) - min_queue[0].value > limit:
+                # Remove leftmost item from window
+                window.popleft()
+                # Remove any items from min/max queues that are to the left of
+                # the current window
+                while min_queue[0].index < window[0].index:
+                    heapq.heappop(min_queue)
+                while max_queue[0].index < window[0].index:
+                    heapq.heappop(max_queue)
+            soln = max(soln, len(window)) 
         return soln
