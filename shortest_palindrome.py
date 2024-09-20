813ef0c517be58a3cf0a226e84fbc77e5d33f9cf
Joseph Ramaswami <jramaswami@gmail.com>
2024-09-20 10:56:58 -0400

    AC. Solved in Python for September 2024 Challenge with help from NeetCodeIO.

--- shortest_palindrome.py
+++ shortest_palindrome.py
@@ -7,22 +7,25 @@ Thank You NeetCodeIO!
 """
 
 
-import functools
-
-
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
         if s == "":
             return s
-            
-        @functools.cache
-        def is_palindrome(left, right):
-            if left >= right:
-                return True
 
-            return s[left] == s[right] and is_palindrome(left+1, right-1)
-        
-        for right in range(len(s)-1, -1, -1):
-            if is_palindrome(0, right):
-                t = s[right+1:][::-1]
-                return t + s
+        MOD = pow(10, 9) + 1
+        base = 29
+        suffix = 0
+        prefix = 0
+        m = 1
+        max_i = -1
+        for i, c in enumerate(s):
+            x = ord(c) - ord('a') + 1
+            prefix = (base * prefix) % MOD
+            prefix = (prefix + x) % MOD
+            
+            suffix = (suffix + x * m) % MOD
+            m = (m * base) % MOD
+
+            if suffix == prefix:
+                max_i = i
+        return s[max_i+1:][::-1] + s
