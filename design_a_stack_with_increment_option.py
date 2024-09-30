3683583e4250ab646fcc3439835114929ba146a8
Joseph Ramaswami <jramaswami@gmail.com>
2024-09-30 05:46:53 -0400

    AC. Solved in Python for September 2024 Challenge.

---
+++ design_a_stack_with_increment_option.py
@@ -0,0 +1,33 @@
+"""
+LeetCode
+1381. Design a Stack With Increment Operation
+September 2024 Challenge
+jramaswami
+"""
+
+
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.stack = []
+        self.max_size = maxSize
+
+    def push(self, x: int) -> None:
+        if len(self.stack) < self.max_size:
+            self.stack.append(x)
+
+    def pop(self) -> int:
+        if self.stack:
+            return self.stack.pop()
+        return -1
+        
+    def increment(self, k: int, val: int) -> None:
+        for i in range(min(k, len(self.stack))):
+            self.stack[i] += val
+
+
+# Your CustomStack object will be instantiated and called as such:
+# obj = CustomStack(maxSize)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)
