a7615f1250bbea84f0b73042a0c4f84b6b35fd14
Joseph Ramaswami <jramaswami@gmail.com>
2024-11-17 07:00:40 -0500

    AC. Solved in Python for November 2024 Challenge with help from NeetCode.IO.  Monotonic queue solution.

--- shortest_subarray_with_sum_at_least_k.py
+++ shortest_subarray_with_sum_at_least_k.py
@@ -15,16 +15,20 @@ import math
 
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
-        heap = []  # (prefix_sum, end_index)
+        queue = collections.deque() # (prefix_sum, end_index)
         soln = math.inf
         curr_sum = 0
         for i, n in enumerate(nums):
             curr_sum += n
             if curr_sum >= k:
                 soln = min(soln, i+1)
-            # Find the minimum window ending at i
-            while heap and curr_sum - heap[0][0] >= k:
-                x, j = heapq.heappop(heap)
+            # Find the minimum valid window ending at i
+            while queue and curr_sum - queue[0][0] >= k:
+                _, j = queue.popleft()
                 soln = min(soln, i - j)
-            heapq.heappush(heap, (curr_sum, i))
+            # Maintain monotonic queue
+            while queue and queue[-1][0] > curr_sum:
+                queue.pop()
+            queue.append((curr_sum, i))
+            
         return -1 if soln == math.inf else soln
