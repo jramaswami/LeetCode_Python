4396ac8361cd8d80d9f6d5f8e2c7f8e3a2322a7c
Joseph Ramaswami <jramaswami@gmail.com>
2024-11-29 07:14:44 -0500

    AC. Solved in Python for November 2024 Challenge.

--- minimum_time_to_visit_a_cell_in_a_grid.py
+++ minimum_time_to_visit_a_cell_in_a_grid.py
@@ -13,21 +13,42 @@ from typing import List
 
 class Solution:
     def minimumTime(self, grid: List[List[int]]) -> int:
-        N, M = len(grid), len(grid[0])
-        curr_queue = set([(0, 0)])
-        next_queue = set()
-        curr_time = 0
-        for _ in range(pow(10,5)):
-            curr_time += 1
-            for r, c in curr_queue:
-                for dr, dc in ((0, 1), (0, -1), (1, 0), (-1, 0)):
-                    r0, c0 = r + dr, c + dc
-                    if r0 >= 0 and r0 < len(grid) and c0 >= 0 and c0 < len(grid[r]):
-                        if curr_time >= grid[r0][c0]:
-                            if r0 == N - 1 and c0 == M - 1:
-                                return curr_time
-                            next_queue.add((r0, c0))
-            if not next_queue:
-                return -1
-            curr_queue, next_queue = next_queue, set()
-        return -1
+
+        def inbounds(r, c):
+            return r >= 0 and r < len(grid) and c >= 0 and c < len(grid[r])
+
+        def neighbors(r, c):
+            for dr, dc in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                r0, c0 = r + dr, c + dc
+                if inbounds(r0, c0):
+                    yield r0, c0
+
+        # Make sure that you can leave the origin
+        if all(grid[r0][c0] > 1 for r0, c0 in neighbors(0, 0)):
+            return -1
+
+        # Because we can backtrack once we have two cells we can visit,
+        # we can jitter back and forth until we can move to an unvisited
+        # cell.  This means that the parity of the current cells visit time
+        # determines when we can visit an adjacent cell.
+        max_time = [[math.inf for _ in row] for row in grid]
+        queue = []
+        heapq.heappush(queue, (0, 0, 0))
+        max_time[0][0] = 0
+        while queue:
+            t, r, c = heapq.heappop(queue)
+            if t == max_time[r][c]:
+                if r == len(grid) - 1 and c == len(grid[0]) - 1:
+                    return t
+                for r0, c0 in neighbors(r, c):
+                    if grid[r0][c0] > t:
+                        curr_cell_parity = t % 2
+                        neighbor_cell_parity = grid[r0][c0] % 2
+                        t0 = grid[r0][c0]
+                        if curr_cell_parity == neighbor_cell_parity:
+                            t0 = grid[r0][c0] + 1
+                    else:
+                        t0 = t + 1
+                    if t0 < max_time[r0][c0]:
+                        max_time[r0][c0] = t0
+                        heapq.heappush(queue, (t0, r0, c0))
