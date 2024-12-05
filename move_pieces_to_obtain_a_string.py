c7c6e1bd280926885e8aa39933df8bc787cde8f4
Joseph Ramaswami <jramaswami@gmail.com>
2024-12-05 07:45:37 -0500

    WA

--- move_pieces_to_obtain_a_string.py
+++ move_pieces_to_obtain_a_string.py
@@ -11,62 +11,57 @@ import collections
 
 class Solution:
     def canChange(self, start: str, target: str) -> bool:
-        # Character freqs must match
-        start_freqs = collections.Counter(start)
-        target_freqs = collections.Counter(target)
-        if start_freqs != target_freqs:
-            return False
+        start0 = collections.deque((i, x) for i, x in enumerate(start) if x in 'LR')
+        target0 = collections.deque((i, x) for i, x in enumerate(target) if x in 'LR')
 
-        # How many rights to the left of the current index
-        start_right = [0 for _ in start]
-        curr = 0
-        for i, x in enumerate(start):
-            if x == 'R':
-                curr += 1
-            elif x == 'L':
-                curr = 0
-            start_right[i] = curr
-
-        target_right = [0 for _ in target]
-        curr = 0
-        for i, x in enumerate(target):
-            if x == 'R':
-                curr += 1
-            elif x == 'L':
-                curr = 0
-            target_right[i] = curr
-
-        # How many lefts to the right of current index
-        start_left = [0 for _ in start]
-        curr = 0
-        for i in range(len(start) - 1, -1, -1):
-            x = start[i]
-            if x == 'R':
-                curr = 0
-            elif x == 'L':
-                curr += 1
-            start_left[i] = curr
-
-        target_left = [0 for _ in target]
-        curr = 0
-        for i in range(len(target) - 1, -1, -1):
-            x = target[i]
-            if x == 'R':
-                curr = 0
-            elif x == 'L':
-                curr += 1
-            target_left[i] = curr
-
-        print(start_left)
-        print(target_left)
-        print(start_right)
-        print(target_right)
-        # Do this for start and target.  For letter, appropriate freq must be gte target
-        for i, x in enumerate(target):
-            if x == 'L':
-                if start_left[i] < target_left[i]:
-                    return False
-            elif x == 'R':
-                if start_right[i] < target_right[i]:
-                    return False
+        for s, t in zip(start0, target0):
+            if s[1] != t[1]:
+                return False
+            if s[1] == 'L' and t[0] > s[0]:
+                return False
+            if s[1] == 'R' and t[0] < s[0]:
+                return False
         return True
+
+
+def test1():
+    start = "_L__R__R_"
+    target = "L______RR"
+    expected = True
+    assert Solution().canChange(start, target) == expected
+
+
+def test_2():
+    start = "R_L_"
+    target = "__LR"
+    expected = False
+    assert Solution().canChange(start, target) == expected
+
+
+def test_3():
+    start = "_R"
+    target = "R_"
+    expected = False
+    assert Solution().canChange(start, target) == expected
+
+
+def test_4():
+    start = "R_L__R__R_"
+    target = "_L______RR"
+    expected = False
+    assert Solution().canChange(start, target) == expected
+
+
+def test_5():
+    start = "R__L"
+    target = "_LR_"
+    expected = False
+    assert Solution().canChange(start, target) == expected
+
+
+def test_6():
+    "WA"
+    start = "_L__R__R_L"
+    target = "L______RR_"
+    expected = False
+    assert Solution().canChange(start, target) == expected
