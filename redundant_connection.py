fa4c60ccb1bd99c96fff5216f10b0f73897dac19
Joseph Ramaswami <jramaswami@gmail.com>
2025-01-29 15:45:51 -0500

    Resolved in Python for January 2025 Challenge.

--- redundant_connection.py
+++ redundant_connection.py
@@ -1,58 +1,40 @@
 """
-LeetCode :: June 2021 Challenge :: Redundant Connection
+LeetCode
+684. Redundant Connection
+January 2025 Challenge
 jramaswami
 """
-class Solution:
-    def findRedundantConnection(self, edges):
-        """
-        Use Disjoint Set Union to find the last edge that attempts to connect
-        vertices that are already connected.
 
-        This is a nearly O(n) solution as that is the time complexity of
-        find_set, which is the bottleneck operation.
-        """
 
-        parent = {}
-        size = {}
+from typing import List
 
-        def make_set(v):
-            parent[v] = v
-            size[v] = 1
 
-        def find_set(v):
-            if parent[v] == v:
-                return v
-            p = find_set(parent[v])
-            parent[v] = p
-            return p
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n+1))
+        self.size = [1 for _ in self.parent]
 
-        def union_set(a, b):
-            a = find_set(a)
-            b = find_set(b)
-            if size[a] < size[b]:
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, a, b):
+        a = self.find(a)
+        b = self.find(b)
+        if a != b:
+            if self.size[a] < self.size[b]:
                 a, b = b, a
-            parent[b] = a
-            size[a] += size[b]
+            self.size[a] += self.size[b]
+            self.parent[b] = a
+        
 
-        def connected(a, b):
-            return find_set(a) == find_set(b)
-
-        for v in range(1, len(edges) + 1):
-            make_set(v)
-
-        for a, b in edges:
-            if connected(a, b):
-                return (a, b)
-            union_set(a, b)
-
-
-def test_1():
-    edges = [[1,2],[1,3],[2,3]]
-    expected = [2,3]
-    assert Solution().findRedundantConnection(edges)
-
-
-def test_2():
-    edges = [[1,2],[2,3],[3,4],[1,4],[1,5]]
-    expected = [1,4]
-    assert Solution().findRedundantConnection(edges)
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        N = len(edges)
+        uf = UnionFind(N)
+        for u, v in edges:
+            if uf.find(u) == uf.find(v):
+                # This is a superfluous edge
+                return [u, v]
+            uf.union(u, v)
