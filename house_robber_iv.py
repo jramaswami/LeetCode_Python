5d72efbbf3cd1b319358f41bf5652703a843f1c0
Joseph Ramaswami <jramaswami@gmail.com>
2025-03-15 08:55:52 -0400

    AC. Solved in Python for March 2025 Challenge with help from NeetCode.IO.

--- house_robber_iv.py
+++ house_robber_iv.py
@@ -6,20 +6,38 @@ jramaswami
 """
 
 
+from typing import List
+
+
 class Solution:
     def minCapability(self, nums: List[int], k: int) -> int:
-        INF = pow(10, 10)
-        # dp[houses robbed: n][house index: i] = max value after robbing n houses at index i
-        dp = [[INF for _ in nums] for _ in range(k+1)]
-        dp[0] = [0 for _ in nums]
-        dp[1] = list(nums)
-        for n in range(1, k):
-            for i in range(len(nums)):
-                if dp[n][i] < INF:
-                    # I have robbed this house
-                    # I can rob another house and my capacity will be the min of this house
-                    # and the next.  I want to record the minimum capacity to have robbed
-                    # n+1 houses at house j.
-                    for j in range(i+2, len(nums)):
-                        dp[n+1][j] = min(dp[n+1][j], max(dp[n][i], nums[j]))
-        return min(dp[-1])
+        def check(capability):
+            i = 0
+            count = 0
+            while i < len(nums):
+                if nums[i] <= capability:
+                    i += 2
+                    count += 1
+                else:
+                    i += 1
+                if count == k:
+                    break
+            return count == k
+
+        soln = pow(10,10)
+        low = min(nums)
+        high = max(nums)
+        while low <= high:
+            mid = low + ((high - low) // 2)
+            if check(mid):
+                soln = min(soln, mid)
+                high = mid - 1
+            else:
+                low = mid + 1
+        return soln
+
+
+def test_1():
+    nums = [2,3,5,9]
+    k = 2
+    assert Solution().minCapability(nums, k) == 5
