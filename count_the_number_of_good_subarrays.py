7d093ffdac91943fe76e6f83abb72b33b1676465
Joseph Ramaswami <jramaswami@gmail.com>
2025-04-16 12:55:02 -0400

    Still TLE

--- count_the_number_of_good_subarrays.py
+++ count_the_number_of_good_subarrays.py
@@ -12,19 +12,14 @@ import math
 
 class Solution:
     def countGood(self, nums: List[int], k: int) -> int:
-        def pair_count(n):
-            return (n * (n-1)) // 2
-
         soln = 0
         for left, _ in enumerate(nums):
             freqs = collections.Counter()
             curr_pairs = 0
             for right, x in enumerate(nums[left:], start=left):
                 # Add x to the list of items
-                if x in freqs:
-                    curr_pairs -= pair_count(freqs[x])
                 freqs[x] += 1
-                curr_pairs += pair_count(freqs[x])
+                curr_pairs += freqs[x]
                 if curr_pairs >= k:
                     # From here on out, there will be enough values
                     # when we start from left and go at least to right
