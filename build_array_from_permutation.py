eeb26b869b76ff7aba956e4c1a5ff9a1e34742a3
Joseph Ramaswami <jramaswami@gmail.com>
2025-05-06 05:02:55 -0400

    AC. Solved in Python for May 2025 Challenge.

---
+++ build_array_from_permutation.py
@@ -0,0 +1,11 @@
+"""
+LeetCode
+1920. Build Array from Permutation
+May 2025 Challenge
+jramaswami
+"""
+
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        return [nums[i] for i in nums]
