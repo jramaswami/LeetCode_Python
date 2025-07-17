38a0be3ff81a2f42137bab959e4135de5ead6864
Joseph Ramaswami <jramaswami@gmail.com>
2025-07-17 04:52:32 -0400

    MLE

---
+++ find_the_maximum_length_of_valid_subsequence_ii.py
@@ -0,0 +1,29 @@
+"""
+LeetCode
+3202. Find the Maximum Length of Valid Subsequence II
+July 2025 Challenge
+jramaswami
+"""
+
+
+import functools
+
+
+class Solution:
+    def maximumLength(self, nums: List[int], k: int) -> int:
+
+        @functools.cache
+        def rec(i, prev, x):
+            if i >= len(nums):
+                return 0
+
+            if (prev + nums[i]) % k == x:
+                return 1 + rec(i+1, nums[i], x)
+            return rec(i+1, prev, x)
+
+        soln = 0
+        for i, a in enumerate(nums):
+            for j, b in enumerate(nums[i+1:], start=i+1):
+                x = (a + b) % k
+                soln = max(soln, 2 + rec(j+1, b, x))
+        return soln
