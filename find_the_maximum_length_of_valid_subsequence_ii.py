a28df9cc2536ebff28f62d6d42bfaf93a7dabad6
Joseph Ramaswami <jramaswami@gmail.com>
2025-07-17 05:21:19 -0400

    AC. Solved in Python for July 2025 Challenge.

--- find_the_maximum_length_of_valid_subsequence_ii.py
+++ find_the_maximum_length_of_valid_subsequence_ii.py
@@ -6,25 +6,20 @@ jramaswami
 """
 
 
-import functools
 from typing import List
 
 
 class Solution:
     def maximumLength(self, nums: List[int], k: int) -> int:
-
-        @functools.cache
-        def rec(i, prev, x):
-            if i >= len(nums):
-                return 0
-
-            if (prev + nums[i]) % k == x:
-                return 1 + rec(i+1, nums[i], x)
-            return rec(i+1, prev, x)
-
         soln = 0
+        # dp[i][x] = current length
+        dp = [[0 for _ in range(k)] for _ in nums]
         for i, a in enumerate(nums):
             for j, b in enumerate(nums[i+1:], start=i+1):
                 x = (a + b) % k
-                soln = max(soln, 2 + rec(j+1, b, x))
+                if dp[i][x] == 0:
+                    dp[j][x] = 2
+                else:
+                    dp[j][x] = 1 + dp[i][x]
+                soln = max(soln, dp[j][x])
         return soln
