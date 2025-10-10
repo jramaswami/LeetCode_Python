35d0034a08236bf95ad4fd896c4caaa1a4c57dd5
Joseph Ramaswami <jramaswami@gmail.com>
2025-10-10 05:31:21 -0400

    AC. Solved in Python for October 2025 Challenge.

--- taking_maximum_energy_from_the_mystic_dungeon.py
+++ taking_maximum_energy_from_the_mystic_dungeon.py
@@ -6,17 +6,14 @@ jramaswami
 """
 
 
-import itertools
 from typing import List
 
 
 class Solution:
     def maximumEnergy(self, energy: List[int], k: int) -> int:
-        return max(
-            max(
-                itertools.accumulate(
-                    itertools.islice(reversed(energy), start, len(energy), k)
-                )
-            )
-            for start in range(k)
-        )
+        energy0 = energy[::-1]
+        dp = [0 for _ in energy]
+        dp[:k] = list(energy0[:k])
+        for i, m in enumerate(energy0[k:], start=k):
+            dp[i] = m + dp[i-k]
+        return max(dp)
