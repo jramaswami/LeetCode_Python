88899c612c76f79cfb0a4e286a426cbbce7c992c
Joseph Ramaswami <jramaswami@gmail.com>
2025-11-05 07:53:04 -0500

    TLE

--- find_x_sum_of_all_k_long_subarrays_ii.py
+++ find_x_sum_of_all_k_long_subarrays_ii.py
@@ -8,6 +8,7 @@ jramaswami
 
 import collections
 from typing import List
+from sortedcontainers import SortedSet
 
 
 class Solution:
@@ -15,16 +16,26 @@ class Solution:
         soln = []
         freqs = collections.Counter()
         window = collections.deque()
+        window_freqs = SortedSet()
         for n in nums:
+            if freqs[n] > 0:
+                old_key = (freqs[n], n)
+                window_freqs.remove(old_key)
             freqs[n] += 1
             window.append(n)
+            new_key = (freqs[n], n)
+            window_freqs.add(new_key)
 
             while len(window) > k:
+                old_key = (freqs[window[0]], window[0])
+                window_freqs.remove(old_key)
                 freqs[window[0]] -= 1
+                if freqs[window[0]] > 0:
+                    new_key = (freqs[window[0]], window[0])
+                    window_freqs.add(new_key)
                 window.popleft()
 
             if len(window) == k:
-                A = list(sorted(((f, v) for v, f in freqs.items()), reverse=True))
-                soln.append(sum(f * v for f, v in A[:x]))
+                soln.append(sum(f * v for f, v in window_freqs[-x:]))
 
         return soln
