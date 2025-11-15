5ea00b4ac5e89e891338eca9b60e01c8f8baff3b
Joseph Ramaswami <jramaswami@gmail.com>
2025-11-15 15:52:52 -0500

    Still TLE.

--- count_the_number_of_substrings_with_dominant_ones.py
+++ count_the_number_of_substrings_with_dominant_ones.py
@@ -6,17 +6,29 @@ jramaswami
 """
 
 
+import itertools
+
+
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
+        ones_prefix = list(itertools.accumulate(int(x) for x in s))
+
+        def get(left, right):
+            if left == 0:
+                return ones_prefix[right]
+            return ones_prefix[right] - ones_prefix[left - 1]
+
         soln = 0
         for i, _ in enumerate(s):
-            curr_ones = 0
-            for j, val in enumerate(s[i:], start=i):
-                if val == '1':
-                    curr_ones += 1
+            j = i
+            while j < len(s):
+                curr_ones = get(i, j)
                 curr_zeros = (j - i + 1) - curr_ones
                 if curr_ones >= pow(curr_zeros, 2):
                     soln += 1
+                    j += 1
+                elif curr_zeros:
+                    j = max(j + 1, i + pow(curr_zeros, 2))
         return soln
 
 
