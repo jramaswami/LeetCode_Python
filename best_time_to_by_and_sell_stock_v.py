2e3023d815f22a5dc0ac8c809426f8aebeca5567
Joseph Ramaswami <jramaswami@gmail.com>
2025-12-17 13:35:53 -0500

    Solved in Python for December 2025 Challenge.

--- best_time_to_by_and_sell_stock_v.py
+++ best_time_to_by_and_sell_stock_v.py
@@ -15,31 +15,34 @@ class Solution:
     def maximumProfit(self, prices: List[int], k: int) -> int:
         LONG, SHORT, NONE = 0, 1, 2
 
-        @functools.cache
-        def rec(index, state, txs):
-            # Base Cases
-            # If we have run out of transactions
-            if txs == 0:
-                return 0
-            # If we have run out of days
-            if index >= len(prices):
-                if state == NONE:
-                    return 0
-                return -math.inf
+        # dp[day index][transactions][state]
+        dp = [[[-math.inf for _ in range(3)] for _ in range(k+1)] for _ in range(len(prices) + 1)]
+        dp[0][0][NONE] = 0
+        for today in range(len(prices)):
+            tomorrow = today + 1
+            todays_price = prices[today]
+            for tx in range(k+1):
+                # LONG
+                # Selling a stock I am holding
+                if tx+ 1 <= k:
+                    dp[tomorrow][tx+1][NONE] = max(dp[tomorrow][tx+1][NONE], dp[today][tx][LONG] + todays_price)
+                # Holding the stock
+                dp[tomorrow][tx][LONG] = max(dp[tomorrow][tx][LONG], dp[today][tx][LONG])
+                # SHORT
+                # Buying the stock that I shorted
+                if tx+ 1 <= k:
+                    dp[tomorrow][tx+1][NONE] = max(dp[tomorrow][tx+1][NONE], dp[today][tx][SHORT] - todays_price)
+                # Holding the short
+                dp[tomorrow][tx][SHORT] = max(dp[tomorrow][tx][SHORT], dp[today][tx][SHORT])
+                # NONE
+                # Buying a stock to sell later
+                dp[tomorrow][tx][LONG] = max(dp[tomorrow][tx][LONG], dp[today][tx][NONE] - todays_price)
+                # Selling a stock to short later
+                dp[tomorrow][tx][SHORT] = max(dp[tomorrow][tx][SHORT], dp[today][tx][NONE] + todays_price)
+                # Not buying or selling
+                dp[tomorrow][tx][NONE] = max(dp[tomorrow][tx][NONE], dp[today][tx][NONE])
 
-            # Recursive Cases
-            if state == LONG:
-                sell = rec(index+1, NONE, txs-1) + prices[index]
-                hold = rec(index+1, LONG, txs)
-                return max(sell, hold)
-            elif state == SHORT:
-                buy = rec(index+1, NONE, txs-1) - prices[index]
-                hold = rec(index+1, SHORT, txs)
-                return max(buy, hold)
-            # NONE
-            buy = rec(index+1, LONG, txs) - prices[index]
-            sell = rec(index+1, SHORT, txs) + prices[index]
-            hold = rec(index+1, NONE, txs)
-            return max((buy, sell, hold))
-
-        return rec(0, NONE, k)
+        soln = -math.inf
+        for j in range(k+1):
+            soln = max(dp[-1][j][NONE], soln)
+        return soln
