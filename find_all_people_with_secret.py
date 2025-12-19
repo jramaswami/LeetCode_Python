ac95c9f7e4c9c1c626e5d1bb800c9c9a051c903c
Joseph Ramaswami <jramaswami@gmail.com>
2025-12-19 08:28:53 -0500

    AC. Resolved in Python for December 2025 Challenge.  Previous solution was TLE for additional tests.

--- find_all_people_with_secret.py
+++ find_all_people_with_secret.py
@@ -1,87 +1,70 @@
 """
 LeetCode
 2092. Find All People With Secret
-February 2024
+December 2025 Challenge
 jramaswami
 """
 
 
 import collections
-import math
 from typing import List
 
 
-Edge = collections.namedtuple('Edge', ['dest', 'time'])
-QItem = collections.namedtuple('QItem', ['source', 'time'])
+class AugmentedUnionFind:
+    def __init__(self):
+        self.parent = dict()
+        self.size = dict()
+        self.knows_secret = dict()
+
+    def add(self, x, knows):
+        if x not in self.parent:
+            self.parent[x] = x
+            self.size[x] = 1
+            self.knows_secret[x] = knows
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+            self.knows_secret[x] |= self.parent[x]
+            return self.parent[x]
+        return x
+
+    def union(self, a, b):
+        a = self.find(a)
+        b = self.find(b)
+        if a != b:
+            if self.size[a] < self.size[b]:
+                a, b = b, a
+            self.parent[b] = a
+            self.knows_secret[a] |= self.knows_secret[b]
+            self.size[a] += self.size[b]
 
 
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        print(list(sorted(meetings, key=lambda t: t[2])))
-        # Transform meetings into graph with timed edges
-        graph = [[] for _ in range(n)]
-        for u, v, t in meetings:
-            graph[u].append(Edge(v,t))
-            graph[v].append(Edge(u,t))
+        knows_secret = set()
+        # Person 0 shares with firstPerson at time 0
+        knows_secret.add(0)
+        knows_secret.add(firstPerson)
 
-        # Zero tells firstPerson
-        graph[0].append(Edge(firstPerson, 0))
-        has_secret = [False for _ in range(n)]
-        secret_time = [math.inf for _ in range(n)]
-        secret_time[0] = 0
-        has_secret[0] = True
-        queue = collections.deque()
-        # Zero knows secret at time 0
-        queue.append(QItem(0,0))
-        while queue:
-            item = queue.popleft()
-            for edge in graph[item.source]:
-                if item.time <= edge.time and edge.time < secret_time[edge.dest]:
-                    # print(item, edge)
-                    has_secret[edge.dest] = True
-                    secret_time[edge.dest] = edge.time
-                    queue.append(QItem(edge.dest, edge.time))
+        meetings_by_time = collections.defaultdict(list)
+        for x, y, t in meetings:
+            meetings_by_time[t].append((x, y))
 
-        return [i for i, s in enumerate(has_secret) if s]
+        for t in sorted(meetings_by_time):
+            # Augmented union find with knows secret.
+            uf = AugmentedUnionFind()
+            for x, y in meetings_by_time[t]:
+                uf.add(x, x in knows_secret)
+                uf.add(y, y in knows_secret)
+                uf.union(x, y)
 
-
-def test_1():
-    n = 6
-    meetings = [[1,2,5],[2,3,8],[1,5,10]]
-    firstPerson = 1
-    expected = [0,1,2,3,5]
-    assert Solution().findAllPeople(n, meetings, firstPerson) == expected
-
-
-def test_2():
-    n = 4
-    meetings = [[3,1,3],[1,2,2],[0,3,3]]
-    firstPerson = 3
-    expected = [0,1,3]
-    assert Solution().findAllPeople(n, meetings, firstPerson) == expected
-
-
-def test_3():
-    n = 5
-    meetings = [[3,4,2],[1,2,1],[2,3,1]]
-    firstPerson = 1
-    expected = [0,1,2,3,4]
-    assert Solution().findAllPeople(n, meetings, firstPerson) == expected
-
-
-def test_4():
-    "WA"
-    n = 6
-    meetings = [[0,2,1],[1,3,1],[4,5,1]]
-    firstPerson = 1
-    expected = [0,1,2,3]
-    assert Solution().findAllPeople(n, meetings, firstPerson) == expected
-
-
-def test_5():
-    "WA"
-    n = 12
-    meetings = [[10,8,6],[9,5,11],[0,5,18],[4,5,13],[11,6,17],[0,11,10],[10,11,7],[5,8,3],[7,6,16],[3,6,10],[3,11,1],[8,3,2],[5,0,7],[3,8,20],[11,0,20],[8,3,4],[1,9,4],[10,7,11],[8,10,18]]
-    firstPerson = 9
-    expected = [0,1,4,5,6,9,11]
-    assert Solution().findAllPeople(n, meetings, firstPerson) == expected
+            # Record if they know the secret
+            for x, y in meetings_by_time[t]:
+                z = uf.find(x)
+                if uf.knows_secret[z]:
+                    knows_secret.add(x)
+                z = uf.find(y)
+                if uf.knows_secret[z]:
+                    knows_secret.add(y)
+        return list(knows_secret)
