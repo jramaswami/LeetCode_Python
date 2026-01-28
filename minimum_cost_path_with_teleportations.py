df0845422742fe7bba043d45bff3030e52c69780
Joseph Ramaswami <jramaswami@gmail.com>
2026-01-28 12:10:08 -0500

    Different solution, but still TLE

--- minimum_cost_path_with_teleportations.py
+++ minimum_cost_path_with_teleportations.py
@@ -6,41 +6,48 @@ jramaswami
 """
 
 
+import heapq
 import math
 
 
 class Solution:
     def minCost(self, grid: list[list[int]], k: int) -> int:
-        # teleporations[row][col] = list of destinations
-        teleportations = [[[] for _ in row] for row in grid]
+        teleportations = [[set() for _ in row] for row in grid]
         for i, row0 in enumerate(grid):
             for j, _ in enumerate(row0):
                 for x, row1 in enumerate(grid):
                     for y, _ in enumerate(row1):
                         if grid[x][y] <= grid[i][j]:
-                            teleportations[i][j].append((x, y))
+                            teleportations[i][j].add((x, y))
 
-        # dp[number of teleports][row][column] = min distance
-        dp = [[[math.inf for _ in row] for row in grid] for _ in range(k+1)]
-        dp[0][0][0] = 0
+        # distance[number of teleports][row][column] = min distance
+        distance = [[[math.inf for _ in row] for row in grid] for _ in range(k+1)]
+        processed = [[[False for _ in row] for row in grid] for _ in range(k+1)]
+        distance[0][0][0] = 0
+        queue = [(0, 0, 0, 0)]
+        while queue:
+            d, t, r, c = heapq.heappop(queue)
+            if processed[t][r][c]:
+                continue
+            processed[t][r][c] = True
+            if r == len(grid)-1 and c == len(grid[0])-1:
+                return d
 
-        for t in range(k+1):
-            for r, row in enumerate(grid):
-                for c, _ in enumerate(row):
-                    # Normal moves
-                    if r + 1 < len(grid):
-                        dp[t][r+1][c] = min(dp[t][r+1][c], dp[t][r][c] + grid[r+1][c])
-                    if c + 1 < len(row):
-                        dp[t][r][c+1] = min(dp[t][r][c+1], dp[t][r][c] + grid[r][c+1])
-                    # Teleportations
-                    if t + 1 <= k:
-                        for r0, c0 in teleportations[r][c]:
-                            dp[t+1][r0][c0] = min(dp[t+1][r0][c0], dp[t][r][c])
-
-        soln = math.inf
-        for t in range(k+1):
-            soln = min(soln, dp[t][-1][-1])
-        return soln
+            # Normal moves
+            if r + 1 < len(grid):
+                r0 = r + 1
+                c0 = c
+                d0 = d + grid[r0][c0]
+                heapq.heappush(queue, (d0, t, r0, c0))
+            if c + 1 < len(grid[0]):
+                r0 = r
+                c0 = c + 1
+                d0 = d + grid[r0][c0]
+                heapq.heappush(queue, (d0, t, r0, c0))
+            # Teleportations
+            if t + 1 <= k:
+                for r0, c0 in teleportations[r][c]:
+                    heapq.heappush(queue, (d, t+1, r0, c0))
 
 
 def test_1():
