26335df29e52b5f4a31d6a4469eb610bdd393092
Joseph Ramaswami <jramaswami@gmail.com>
2026-03-15 09:22:57 -0400

    AC. Solved in Python for March 2026 Challenge.

--- fancy_sequence.py
+++ fancy_sequence.py
@@ -21,16 +21,19 @@ class Fancy:
         self.values = []
         self.MOD = pow(10, 9) + 7
 
+    def inverse(self, a):
+        return pow(a, self.MOD-2, self.MOD)
+
     def append(self, val: int) -> None:
         self.values.append(Value(len(self.additions), val))
 
     def addAll(self, inc: int) -> None:
-        self.additions.append(self.additions[-1] + inc)
+        self.additions.append((self.additions[-1] + inc) % self.MOD)
         self.multiplications.append(self.multiplications[-1])
 
     def multAll(self, m: int) -> None:
-        self.additions.append(self.additions[-1] * m)
-        self.multiplications.append(self.multiplications[-1] * m)
+        self.additions.append((self.additions[-1] * m) % self.MOD)
+        self.multiplications.append((self.multiplications[-1] * m) % self.MOD)
 
     def getIndex(self, idx: int) -> int:
         if idx >= len(self.values):
@@ -40,7 +43,8 @@ class Fancy:
         if value.clock == 1:
             x = (value.val * self.multiplications[-1]) + self.additions[-1]
         else:
-            m = self.multiplications[-1] // self.multiplications[value.clock-1]
-            a = self.additions[-1] - (self.additions[value.clock-1] * m)
+            m = self.multiplications[-1] * self.inverse(self.multiplications[value.clock-1])
+            p = (self.additions[value.clock-1] * m) % self.MOD
+            a = (self.additions[-1] - p + self.MOD) % self.MOD
             x = (value.val * m) + a
         return x % self.MOD
