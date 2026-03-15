3bcfd01540a48cc4c13692b5aed46583fa77db5e
Joseph Ramaswami <jramaswami@gmail.com>
2026-03-15 09:13:18 -0400

    MLE

--- fancy_sequence.py
+++ fancy_sequence.py
@@ -15,30 +15,25 @@ Value = collections.namedtuple('Value', ('clock', 'val'))
 class Fancy:
 
     def __init__(self):
-        self.operations = []
-        self.clock = 0
+        self.additions = [0]
+        self.multiplications = [1]
         self.values = []
-        self.MOD = pow(10, 9) + 7
 
     def append(self, val: int) -> None:
-        self.values.append(Value(self.clock, val))
-        self.clock += 1
+        self.values.append(Value(len(self.additions), val))
 
     def addAll(self, inc: int) -> None:
-        self.operations.append(Operation(self.clock, lambda x: x + inc))
-        self.clock += 1
+        self.additions.append(inc)
+        self.multiplications.append(self.multiplications[-1])
 
     def multAll(self, m: int) -> None:
-        self.operations.append(Operation(self.clock, lambda x: x * m))
-        self.clock += 1
+        self.additions.append(self.additions[-1] * m)
+        self.multiplications.append(self.multiplications[-1] * m)
 
     def getIndex(self, idx: int) -> int:
         if idx >= len(self.values):
             return -1
-        x = self.values[idx].val
-        t = self.values[idx].clock
-        for op in self.operations:
-            if op.clock > t:
-                x = op.fn(x)
-                x %= self.MOD
+        m = self.multiplications[-1] // self.multiplications[idx-1]
+        a = self.additions[idx-1] * m
+        x = (self.additions[-1] - a) // m
         return x
