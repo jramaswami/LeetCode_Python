23549bb61634bfa7ee086005dec2bd5bf9859a94
Joseph Ramaswami <jramaswami@gmail.com>
2026-04-07 12:11:07 -0400

    AC. Solved in Python for April 2026 Challenge.

--- walking_robot_simulation_ii.py
+++ walking_robot_simulation_ii.py
@@ -6,51 +6,36 @@ jramaswami
 """
 
 
-EAST, NORTH, WEST, SOUTH = 0, 1, 2, 3
-
-
 class Robot:
 
     def __init__(self, width: int, height: int):
+        self.cells = []
+        for x in range(width):
+            self.cells.append((x, 0))
+        for y in range(1, height):
+            self.cells.append((width-1, y))
+        for x in range(width-2,-1,-1):
+            self.cells.append((x, height-1))
+        for y in range(height-2, 0, -1):
+            self.cells.append((0, y))
+        self.steps = 0
         self.width, self.height = width, height
-        self.dirn = 0
-        self.directions = ('East', 'North', 'West', 'South')
-        self.x = self.y = 0
 
     def step(self, num: int) -> None:
-        # Base case
-        if num == 0:
-            return
-
-        steps = 0
-        next_x, next_y = self.x, self.y
-        if self.dirn == EAST:
-            next_x = min(self.x + num, self.width - 1)
-            steps = next_x - self.x
-        elif self.dirn == WEST:
-            next_x = max(self.x - num, 0)
-            steps = self.x - next_x
-        elif self.dirn == NORTH:
-            next_y = min(self.y + num, self.height - 1)
-            steps = next_y - self.y
-        elif self.dirn == WEST:
-            next_y = max(self.y - num, 0)
-            steps = self.y - next_y
-
-        if steps == 0:
-                self.dirn = (self.dirn + 1) % len(directions)
-        self.x, self.y = next_x, next_y
-        self.step(self, num - steps)
+        self.steps += num
 
     def getPos(self) -> List[int]:
-        return [self.x, self.y]
+        return self.cells[self.steps % len(self.cells)]
 
     def getDir(self) -> str:
-        return self.directions[self.dirn]
-
-
-# Your Robot object will be instantiated and called as such:
-# obj = Robot(width, height)
-# obj.step(num)
-# param_2 = obj.getPos()
-# param_3 = obj.getDir()
+        x, y = self.getPos()
+        if self.steps == 0:
+            return 'East'
+        if x > 0 and y == 0:
+            return 'East'
+        if x == self.width - 1:
+            return 'North'
+        if y == self.height - 1:
+            return 'West'
+        if x == 0:
+            return 'South'
