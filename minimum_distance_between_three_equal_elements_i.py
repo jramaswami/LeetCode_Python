ff450cf300670334dc4804fbc7fc204a8bff6dbb
Joseph Ramaswami <jramaswami@gmail.com>
2026-04-10 13:24:57 -0400

    AC. Solved in Python for April 2026 Challenge.

---
+++ minimum_distance_between_three_equal_elements_i.py
@@ -0,0 +1,28 @@
+"""
+LeetCode
+3740. Minimum Distance Between Three Equal Elements I
+April 2026 Challenge
+jramaswami
+"""
+
+
+import collections
+import math
+from typing import List
+
+
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        values = collections.defaultdict(list)
+        for i, n in enumerate(nums):
+            values[n].append(i)
+        soln = math.inf
+        for n in values:
+            if len(values[n]) >= 3:
+                p = 0
+                while p + 2 < len(values[n]):
+                    i = values[n][p]
+                    k = values[n][p+2]
+                    soln = min(soln, 2*k - 2*i)
+                    p += 1
+        return soln if soln < math.inf else -1
