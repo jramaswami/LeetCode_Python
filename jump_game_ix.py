2eb12dd3fce9471eea0ba4833353e93f675b629a
Joseph Ramaswami <jramaswami@gmail.com>
2026-05-07 14:53:13 -0400

    AC. Solved in Python for May 2026 Challenge with help from AlgoMonster.

--- jump_game_ix.py
+++ jump_game_ix.py
@@ -3,6 +3,8 @@ LeetCode
 3660. Jump Game IX
 May 2026 Challenge
 jramaswami
+
+REF: https://algo.monster/liteproblems/3660
 """
 
 
@@ -11,32 +13,19 @@ from typing import List
 
 class Solution:
     def maxValue(self, nums: List[int]) -> List[int]:
-        parent = list(range(len(nums)))
-        size = [1 for _ in nums]
-        comp_max = list(nums)
-
-        def find(a):
-            if parent[a] != a:
-                parent[a] = find(parent[a])
-            return parent[a]
-
-        def union(a, b):
-            a = find(a)
-            b = find(b)
-            if a != b:
-                if size[a] < size[b]:
-                    a,b  = b,a
-                size[a] += size[b]
-                comp_max[a] = max(comp_max[b], comp_max[a])
-                parent[b] = a
-
-        for i, n in enumerate(nums):
-            # Backwards only allowed for smaller jumps
-            for j, x in enumerate(nums[:i]):
-                if x > n:
-                    union(i, j)
-
-        return [comp_max[find(i)] for i, _ in enumerate(nums)]
+        INF = pow(10, 10)
+        soln = [0 for _ in nums]
+        pre_max =[nums[0] for _ in nums]
+        for i, n in enumerate(nums[1:], start=1):
+            pre_max[i] = max(pre_max[i-1], n)
+        suf_min = INF
+        for i in range(len(nums)-1, -1, -1):
+            if i+1 < len(nums):
+                soln[i] = soln[i+1] if pre_max[i] > suf_min else pre_max[i]
+            else:
+                soln[i] = pre_max[i] if pre_max[i] <= suf_min else 0
+            suf_min = min(suf_min, nums[i])
+        return soln
 
 
 def test_1():
