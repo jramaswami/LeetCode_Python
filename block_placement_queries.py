46d61c73f48b7a551db4126b4ad6fda3c36dc984
Joseph Ramaswami <jramaswami@gmail.com>
2026-05-30 14:18:24 -0400

    AC. Solved in Python for May 2026 Challenge with help from Impoldev.

--- block_placement_queries.py
+++ block_placement_queries.py
@@ -3,6 +3,8 @@ LeetCode
 3161. Block Placement Queries
 May 2026 Challenge
 jramaswami
+
+REF: https://www.youtube.com/watch?v=rHlO09AkA_g
 """
 
 
@@ -10,34 +12,58 @@ from typing import List
 import sortedcontainers
 
 
+MX = pow(10, 5)
+
+
+class SegmentTree:
+    def __init__(self):
+        self.st = [0] * (4 * (MX+1))
+
+    def insert(self, idx, val, node=1, left=0, right=MX):
+        if left == right:
+            self.st[node] = val
+        else:
+            mid = left + (right - left) // 2
+            if idx <= mid:
+                self.insert(idx, val, node*2, left, mid)
+            else:
+                self.insert(idx, val, node*2 + 1, mid+1, right)
+            self.st[node] = max(self.st[node*2], self.st[node*2+1])
+
+    def check(self, query_left, query_right, node = 1, left = 0, right = MX):
+        if right < query_left or left > query_right:
+            return 0
+        if query_left <= left and query_right >= right:
+            return self.st[node]
+        mid = left + (right - left) // 2
+        return max(
+            self.check(query_left, query_right, node*2, left, mid),
+            self.check(query_left, query_right, node*2+1, mid+1, right)
+        )
+
+
 class Solution:
     def getResults(self, queries: List[List[int]]) -> List[bool]:
         soln = []
-        obstacles = sortedcontainers.SortedList()
+        obstacles = sortedcontainers.SortedList([0, MX])
+        st = SegmentTree()
+        st.insert(0, MX)
         for q in queries:
             if q[0] == 1:
-                obstacles.add(q[1])
+                _, x = q
+                i = obstacles.bisect_left(x)
+                left, right = obstacles[i-1], obstacles[i]
+                st.insert(left, x-left)
+                st.insert(x, right - x)
+                obstacles.add(x)
             else:
-                soln.append(False)
-                x = q[1]
-                sz = q[2]
-                if obstacles:
-                    prev = 0
-                    for curr in obstacles:
-                        if prev >= x:
-                            break
-                        curr = min(curr, x)
-                        dist = curr - prev
-                        prev = curr
-                        if dist >= sz:
-                            soln[-1] = True
-                            break
-                    dist = x - prev
-                    if dist >= sz:
-                        soln[-1] = True
-                else:
-                    if sz <= x:
-                        soln[-1] = True
+                _, x, sz = q
+                i = obstacles.bisect_left(x)
+                left, right = 0, obstacles[i-1]
+                prev = st.check(left, right-1)
+                tail = x - right
+                mx = max(prev, tail)
+                soln.append(mx >= sz)
         return soln
 
 
