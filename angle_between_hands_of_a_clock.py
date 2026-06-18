4c87ce6017a375e26dbdf58809e77b6a558affd8
Joseph Ramaswami <jramaswami@gmail.com>
2026-06-18 09:57:45 -0400

    WA

---
+++ angle_between_hands_of_a_clock.py
@@ -0,0 +1,29 @@
+"""
+LeetCode
+1344. Angle Between Hands of a Clock
+June 2026 Challenge
+jramaswami
+"""
+
+
+class Solution:
+    def angleClock(self, hour: int, minutes: int) -> float:
+        hour %= 12
+        degrees_per_hour = 360 / 12
+        degrees_per_minute = 360 / 60
+        hour_angle = hour * degrees_per_hour
+        minutes_angle = minutes * degrees_per_minute
+        # Adjust hour hand
+        hour_angle += (degrees_per_hour * minutes / 60)
+        if hour_angle > minutes_angle:
+            return hour_angle - minutes_angle
+        return minutes_angle - hour_angle
+
+
+EPS = pow(10, -5)
+
+
+def test_79():
+    "WA"
+    hour, minutes, expected = 1, 57, 76.5
+    assert abs(Solution().angleClock(hour, minutes) - expected) < EPS
