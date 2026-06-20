3e4f2ff9377b9c9415aedb2712c940a913144487
Joseph Ramaswami <jramaswami@gmail.com>
2026-06-20 15:31:29 -0400

    AC. Solved in Python for June 2026 Challenge.

--- maximum_building_height.py
+++ maximum_building_height.py
@@ -11,14 +11,30 @@ from typing import List
 
 class Solution:
     def maxBuilding(self, n: int, restrictions: List[List[int]]) -> int:
-        limit = list(range(n))
-        for i, k in restrictions:
-            limit[i-1] = k
-        for i, curr in enumerate(limit[1:], start=1):
-            prev = limit[i-1]
-            limit[i] = min(curr, prev+1)
-        for i in range(n-2, -1, -1):
-            prev = limit[i+1]
-            curr = limit[i]
-            limit[i] = min(curr, prev+1)
-        return max(limit)
+        INDEX, LIMIT = 0, 1
+        restrictions.append([1, 0])
+        restrictions.sort()
+        if restrictions[-1][INDEX] != n:
+            restrictions.append([n, n])
+
+        for i in range(1, len(restrictions)):
+            x, prev = restrictions[i-1]
+            y, curr = restrictions[i]
+            delta = y - x
+            restrictions[i][LIMIT] = min(prev + delta, curr)
+
+        for i in range(len(restrictions)-2, -1, -1):
+            x, prev = restrictions[i+1]
+            y, curr = restrictions[i]
+            delta = x - y
+            restrictions[i][LIMIT] = min(prev + delta, curr)
+
+        soln = 0
+        for i, _ in enumerate(restrictions[:-1]):
+            x, prev = restrictions[i]
+            y, curr = restrictions[i+1]
+            dist = y - x
+            delta = abs(curr - prev)
+            k = max(prev, curr) + ((dist - delta) // 2)
+            soln = max(soln, k)
+        return soln
